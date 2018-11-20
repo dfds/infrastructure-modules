6ab7511775db984cbf4635bd91306602ff3757c5
Rasmus Rask <raras@dfds.com>
2018-11-20 15:25:20 +0100

    Child zone name

--- network/route53-delegate-zone/main.tf
+++ network/route53-delegate-zone/main.tf
@@ -14,7 +14,7 @@ terraform {
 resource "aws_route53_record" "ns" {
     # This will create the record of type NS that enables a sub domain to be queried
     zone_id = "${var.dns_zone_id}"
-    name    = "${var.dns_zone_name}"
+    name    = "${var.dns_child_zone_name}"
     type    = "NS"
     ttl     = "300"
 
