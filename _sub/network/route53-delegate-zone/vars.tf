2814fc567fb4fa09841485038a1ed220d5b5cc7e
Rasmus Rask <raras@dfds.com>
2018-11-20 16:02:09 +0100

    Removed region from vars

--- _sub/network/route53-delegate-zone/vars.tf
+++ _sub/network/route53-delegate-zone/vars.tf
@@ -1,9 +1,6 @@
 #Initializes the variables needed to generate a new account
 #The values vill be propagated via a tfvars file
 #By default route53 generates 4 nameservers for a zone which is why there is 4 (zeroindexed) zone-ns variables
-variable "aws_region" {
-  type = "string"
-}
 
 variable "dns_child_zone_name" {
   type = "string"
