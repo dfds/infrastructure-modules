3b967330ca2879087834008b4be44c0b6a050b3a
Rasmus Rask <raras@dfds.com>
2018-12-12 13:50:02 +0100

    CloudTrail: Moved Removed s3_key_prefix

--- _sub/security/cloudtrail-config/main.tf
+++ _sub/security/cloudtrail-config/main.tf
@@ -3,7 +3,6 @@ resource "aws_cloudtrail" "cloudtrail" {
   count = "${var.s3_bucket != "" ? 1 : 0}"
   name                  = "${var.trail_name}"
   s3_bucket_name        = "${var.s3_bucket}"
-  # s3_key_prefix         = "${var.trail_name}"
   is_multi_region_trail = true
   is_organization_trail = "${var.is_organization_trail}"
   include_global_service_events = true
