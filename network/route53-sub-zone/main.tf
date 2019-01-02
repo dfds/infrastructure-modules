d5c342f95cf8013f64cfead6770111a4d46983ef
Rasmus Rask <raras@dfds.com>
2019-01-02 11:39:59 +0100

    Added zone_id to reference (R53)

--- network/route53-sub-zone/main.tf
+++ network/route53-sub-zone/main.tf
@@ -37,7 +37,7 @@ module "route53_zone" {
 module "route53_delegate_zone" {
     source = "../../_sub/network/route53-delegate-zone"
     dns_child_zone_name = "${var.dns_zone_name}"
-    dns_zone_id = "${data.aws_route53_zone.parent}"
+    dns_zone_id = "${data.aws_route53_zone.parent.zone_id}"
     dns_zone_ns = "${module.route53_zone.dns_zone_ns}"
 
   providers = {
