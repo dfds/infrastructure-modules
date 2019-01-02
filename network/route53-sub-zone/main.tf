86dc3017db7ca8db8e952546d8acc29930adb5cb
Rasmus Rask <raras@dfds.com>
2019-01-02 11:26:17 +0100

    Fix duplicate module names in R53 main module

--- network/route53-sub-zone/main.tf
+++ network/route53-sub-zone/main.tf
@@ -25,7 +25,7 @@ terraform {
 
 
 # Create zone in workload account
-module "route53_create_zone" {
+module "route53_zone" {
     source = "../../_sub/network/route53-zone"
     dns_zone_name = "${var.dns_zone_name}"
   providers = {
@@ -34,7 +34,7 @@ module "route53_create_zone" {
 }
 
 # Delegate zone in core account
-module "route53_create_zone" {
+module "route53_delegate_zone" {
     source = "../../_sub/network/route53-delegate-zone"
     dns_child_zone_name = "${var.dns_zone_name}"
     dns_zone_ns = "${module.route53_create_zone.dns_zone_ns}"
