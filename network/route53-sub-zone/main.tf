09a9a5f85595bd074859343ead89265f13e4079c
Rasmus Rask <raras@dfds.com>
2019-01-02 13:09:50 +0100

    Removed alias from default provider (Route53)

--- network/route53-sub-zone/main.tf
+++ network/route53-sub-zone/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region  = "${var.aws_region}"
   version = "~> 1.40"
-  alias   = "core"
 }
 
 provider "aws" {
@@ -16,18 +15,17 @@ provider "aws" {
   }
 }
 
-
 terraform {
-    # The configuration for this backend will be filled in by Terragrunt
-    backend "s3" {}
-    required_version = "~> 0.11.7"
+  # The configuration for this backend will be filled in by Terragrunt
+  backend          "s3"             {}
+  required_version = "~> 0.11.7"
 }
-
 
 # Create zone in workload account
 module "route53_zone" {
-    source = "../../_sub/network/route53-zone"
-    dns_zone_name = "${var.dns_zone_name}"
+  source        = "../../_sub/network/route53-zone"
+  dns_zone_name = "${var.dns_zone_name}"
+
   providers = {
     aws = "aws.workload"
   }
@@ -35,12 +33,8 @@ module "route53_zone" {
 
 # Delegate zone in core account
 module "route53_delegate_zone" {
-    source = "../../_sub/network/route53-delegate-zone"
-    dns_child_zone_name = "${var.dns_zone_name}"
-    dns_zone_id = "${data.aws_route53_zone.parent.zone_id}"
-    dns_zone_ns = "${module.route53_zone.dns_zone_ns}"
-
-  providers = {
-    aws = "aws.core"
-  }
+  source              = "../../_sub/network/route53-delegate-zone"
+  dns_child_zone_name = "${var.dns_zone_name}"
+  dns_zone_id         = "${data.aws_route53_zone.parent.zone_id}"
+  dns_zone_ns         = "${module.route53_zone.dns_zone_ns}"
 }
