9cd841cb69364c1292c9276b762594bbcc4334cd
Rasmus Rask <raras@dfds.com>
2019-01-02 11:35:35 +0100

    3rd time's the charm...

--- _sub/network/route53-zone/vars.tf
+++ _sub/network/route53-zone/vars.tf
@@ -1,9 +1,5 @@
 #Initializes the variables needed to generate a new account
 #The values vill be propagated via a tfvars file
-variable "aws_region" {
-  type = "string"
-}
-
 variable "dns_zone_name" {
   type = "string"
 }
--- network/route53-sub-zone/dependencies.tf
+++ network/route53-sub-zone/dependencies.tf
@@ -1,4 +1,4 @@
-data "aws_route53_zone" "selected" {
+data "aws_route53_zone" "parent" {
   name         = "${var.dns_parent_zone_name}"
   private_zone = false
 }
