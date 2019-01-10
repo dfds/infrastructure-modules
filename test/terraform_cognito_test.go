87716c21fe26667f5f5d9c64fc1aae07a8de02ac
Rasmus Rask <raras@dfds.com>
2019-01-10 14:36:52 +0100

    Disable Cognito and CloudTracker test

--- test/terraform_cognito_test.go
+++ test/terraform_cognito_test.go
@@ -1,10 +1,14 @@
+// Only run test of 'cognito' tag specified
+// +build cognito
+
 package test
 
 import (
 	"fmt"
-	"testing"
 	"io/ioutil"
 	"path/filepath"
+	"testing"
+
 	//"strings"
 	"os"
 
@@ -21,19 +25,19 @@ func TestCognitoModules(t *testing.T) {
 	t.Parallel()
 
 	stateDirectory, err := ioutil.TempDir("", t.Name())
- 	if err != nil {
- 		t.Fatal(err)
-	 }
-	 remoteStateFile := filepath.Join(stateDirectory, "backend.tfstate")
+	if err != nil {
+		t.Fatal(err)
+	}
+	remoteStateFile := filepath.Join(stateDirectory, "backend.tfstate")
 
 	// Give this EC2 Instance a unique ID for a name tag so we can distinguish it from any other EC2 Instance running
 	// in your AWS account
-	randomInt := random.Random(1,500000)
+	randomInt := random.Random(1, 500000)
 	expectedName := fmt.Sprintf("dfdsuserpool%v", randomInt)
 	expectedIdentityPoolName := fmt.Sprintf("dfdsidpool%v", randomInt)
 
 	// Fixed to EU-Central-1, to avoid picking non-compliant region
-	awsRegion := "eu-central-1" 
+	awsRegion := "eu-central-1"
 	terraformDir := "../identity/cognito"
 	overridePath := filepath.Join(terraformDir, "override.tf")
 
@@ -41,7 +45,6 @@ func TestCognitoModules(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
@@ -52,30 +55,28 @@ func TestCognitoModules(t *testing.T) {
 
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
-			"user_pool_name": expectedName,
-			"identity_pool_name": expectedIdentityPoolName,
-			"user_pool_domain_name": expectedName,
-			"user_pool_client_name": "blaster",
+			"user_pool_name":                   expectedName,
+			"identity_pool_name":               expectedIdentityPoolName,
+			"user_pool_domain_name":            expectedName,
+			"user_pool_client_name":            "blaster",
 			"user_pool_identity_provider_name": "DFDSAzureAD",
-			"azure_ad_tenant_id": "73a99466-ad05-4221-9f90-e7142aa2f6c1",
-			"build_callback_url": "https://build.dfds.com/sigin-oidc",
-			"build_logout_url": "https://build.dfds.com/logout",
-			"aws_region": awsRegion,
+			"azure_ad_tenant_id":               "73a99466-ad05-4221-9f90-e7142aa2f6c1",
+			"build_callback_url":               "https://build.dfds.com/sigin-oidc",
+			"build_logout_url":                 "https://build.dfds.com/logout",
+			"aws_region":                       awsRegion,
 		},
 		// Environment variables to set when running Terraform
 		EnvVars: map[string]string{
 			"AWS_DEFAULT_REGION": awsRegion,
-			"AWS_PROFILE": "dfds-playground",
+			"AWS_PROFILE":        "dfds-playground",
 		},
 	}
-
 
 	// Remove override as last thing
 	defer os.Remove(overridePath)
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
-	
 
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
