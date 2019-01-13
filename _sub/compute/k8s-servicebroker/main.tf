ce9cb90adbf6db64b8bebf881a87c46fd7310b40
JacobHeidelbachDFDS <jfhei@dfds.com>
2019-01-13 17:40:22 +0100

    Added dependencies between Helm repos, Service Catalog CRDs

--- _sub/compute/k8s-servicebroker/main.tf
+++ _sub/compute/k8s-servicebroker/main.tf
@@ -1,90 +1,91 @@
 provider "helm" {
-    kubernetes {
-        config_path = "${pathexpand("~/.kube/config_${var.cluster_name}")}"
-    }
+  kubernetes {
+    config_path = "${pathexpand("~/.kube/config_${var.cluster_name}")}"
+  }
 }
 
 resource "null_resource" "repo_init_helm" {
-    triggers {
-        build_number = "${timestamp()}"
-    }
+  triggers {
+    build_number = "${timestamp()}"
+  }
 
   provisioner "local-exec" {
-        command = "helm init --client-only"
-    }
-  
-}
-resource "helm_repository" "servicecatalog" {
-    name = "servicecatalog"
-    url  = "https://svc-catalog-charts.storage.googleapis.com"
+    command = "helm init --client-only"
+  }
 
-    depends_on = 
-        [
-            "null_resource.repo_init_helm"
-        ]
+  provisioner "local-exec" {
+    command = "helm repo add servicecatalog https://svc-catalog-charts.storage.googleapis.com"
+  }
+
+  provisioner "local-exec" {
+    command = "helm repo add aws-sb https://awsservicebroker.s3.amazonaws.com/charts"
+  }
+}
+
+resource "helm_repository" "servicecatalog" {
+  triggers {
+    build_number = "${timestamp()}"
+  }
+
+  name = "servicecatalog"
+  url  = "https://svc-catalog-charts.storage.googleapis.com"
+
+  depends_on = [
+    "null_resource.repo_init_helm",
+  ]
 }
 
 resource "helm_repository" "aws-sb" {
-    name = "aws-sb"
-    url  = "https://awsservicebroker.s3.amazonaws.com/charts"
+  triggers {
+    build_number = "${timestamp()}"
+  }
 
-    depends_on = 
-        [
-            "null_resource.repo_init_helm"
-        ]
+  name = "aws-sb"
+  url  = "https://awsservicebroker.s3.amazonaws.com/charts"
+
+  depends_on = [
+    "null_resource.repo_init_helm",
+  ]
 }
 
 resource "helm_release" "service-catalog" {
-    name        = "catalog"
-    repository  = "${helm_repository.servicecatalog.metadata.0.name}"
-    namespace   = "catalog"
-    chart       = "catalog"
+  name       = "catalog"
+  repository = "${helm_repository.servicecatalog.metadata.0.name}"
+  namespace  = "catalog"
+  chart      = "catalog"
+
+  depends_on = ["helm_repository.servicecatalog"]
 }
 
 resource "helm_release" "service-broker" {
-    name        = "aws-servicebroker"
-    repository  = "${helm_repository.aws-sb.metadata.0.name}"
-    namespace   = "aws-sb"
-    chart       = "aws-servicebroker"
-    version     = "1.0.0-beta.3"
-    set {
-        name    = "aws.region"
-        value   = "${var.aws_region}"
-    }
-    set {
-        name    = "aws.tablename"
-        value   = "${var.table_name}-${var.cluster_name}"
-    }
-    set {
-        name    = "brokerconfig.brokerid"
-        value   = "${var.cluster_name}sb"
-    }
-    set_string {
-        name    = "aws.targetaccountid"
-        value   = "${var.workload_account_id}"
-    }
+  name       = "aws-servicebroker"
+  repository = "${helm_repository.aws-sb.metadata.0.name}"
+  namespace  = "aws-sb"
+  chart      = "aws-servicebroker"
+  version    = "1.0.0-beta.3"
+
+  set {
+    name  = "aws.region"
+    value = "${var.aws_region}"
+  }
+
+  set {
+    name  = "aws.tablename"
+    value = "${var.table_name}-${var.cluster_name}"
+  }
+
+  set {
+    name  = "brokerconfig.brokerid"
+    value = "${var.cluster_name}sb"
+  }
+
+  set_string {
+    name  = "aws.targetaccountid"
+    value = "${var.workload_account_id}"
+  }
+
+  depends_on = [
+    "helm_repository.aws-sb",
+    "helm_release.service-catalog"
+  ]
 }
-
-
-# image: awsservicebroker/aws-servicebroker:beta
-# imagePullPolicy: Always
-# authenticate: true
-# tls:
-#   cert:
-#   key:
-# deployClusterServiceBroker: true
-# aws:
-#   region: us-east-1
-#   bucket: awsservicebroker
-#   key: templates/latest
-#   s3region: us-east-1
-#   tablename: awssb
-#   accesskeyid: ""
-#   secretkey: ""
-#   targetaccountid: ""
-#   targetrolename: ""
-#   vpcid: ""
-# brokerconfig:
-#   verbosity: 10
-#   brokerid: awsservicebroker
-#   prescribeoverrides: true
