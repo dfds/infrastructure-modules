8cd9f4e949245a6a9faba18c3acd280389642a1c
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2019-02-14 08:06:06 +0100

    uploading a user config file to parameter store

--- _sub/compute/eks-heptio/configmap.tf
+++ _sub/compute/eks-heptio/configmap.tf
@@ -32,6 +32,37 @@ KUBECONFIG
 }
 
 locals {
+  kubeconfig_users = <<KUBECONFIG
+
+
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${var.eks_endpoint}
+    certificate-authority-data: ${var.eks_certificate_authority}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster_name}"
+KUBECONFIG
+}
+
+locals {
   config-map-aws-auth = <<CONFIGMAPAWSAUTH
 
 
@@ -50,9 +81,16 @@ data:
 CONFIGMAPAWSAUTH
 }
 
-resource "aws_ssm_parameter" "kubeconfig" {
+resource "aws_ssm_parameter" "kubeconfig_admin" {
   name        = "/eks/${var.cluster_name}/admin"
   description = "The initial config file for eks ${var.cluster_name}"
+  type        = "SecureString"
+  value       = "${local.kubeconfig}"
+}
+
+resource "aws_ssm_parameter" "kubeconfig_users" {
+  name        = "/eks/${var.cluster_name}/default"
+  description = "The default user config file for eks ${var.cluster_name_users}"
   type        = "SecureString"
   value       = "${local.kubeconfig}"
 }
