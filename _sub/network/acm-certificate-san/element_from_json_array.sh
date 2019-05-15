14dfe0784579966f9063ee21acca5940b940aa6e
Rasmus Rask <raras@dfds.com>
2019-05-15 23:35:00 +0200

    Fixes #34 (ACM certificate validation orderering)

--- _sub/network/acm-certificate-san/element_from_json_array.sh
+++ _sub/network/acm-certificate-san/element_from_json_array.sh
@@ -1,1 +1,11 @@
-cat ${1} | jq --arg index ${2} '.[$index|tonumber]'
+if [ -z "$4" ]; then
+    echo "Need 4 arguments"
+    exit 1
+fi
+
+
+if [[ ${2} == "==" ]]; then
+    cat ${1} | jq  --arg domain_name ${3} --arg index ${4} 'map(select(.domain_name == $domain_name))[$index|tonumber]'
+else
+    cat ${1} | jq  --arg domain_name ${3} --arg index ${4} 'map(select(.domain_name != $domain_name))[$index|tonumber]'
+fi
