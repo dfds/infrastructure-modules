ebad5913637f1e7046de3858024bee74b1e4925f
runeabrahamsson <5913288+runeabrahamsson@users.noreply.github.com>
2019-05-24 14:09:50 +0200

    Enabled basic audit logging for the cluster

--- _sub/compute/eks-cluster/eks.tf
+++ _sub/compute/eks-cluster/eks.tf
@@ -3,6 +3,9 @@ resource "aws_eks_cluster" "eks" {
   role_arn        = "${aws_iam_role.eks.arn}"
   version         = "1.11"
   
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+  #enabled_cluster_log_types = ["api", "audit", "authenticator","controllerManager","scheduler"]
+  
   vpc_config {
     security_group_ids = ["${aws_security_group.eks-cluster.id}"]
     subnet_ids         = ["${aws_subnet.eks.*.id}"]
