ec6527b6a00e33337758764409d53278a4a3388c
Willi Carlsen <carlsenwilli@gmail.com>
2019-05-27 12:08:56 +0200

    Fix typo

--- _sub/security/org-account/main.tf
+++ _sub/security/org-account/main.tf
@@ -1,5 +1,5 @@
 resource "aws_organizations_account" "org_account" {
-  name                       = "${lower(var.name})"
+  name                       = "${lowe(var.name)}"
   email                      = "${var.email}"
   iam_user_access_to_billing = "ALLOW"
   role_name                  = "${var.org_role_name}"
