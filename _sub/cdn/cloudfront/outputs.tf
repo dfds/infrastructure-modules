8ca4129a1580e239e33cd9f232ef050b04d0612d
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-07-24 11:17:46 +0200

    cloudfront oai is now optional

--- _sub/cdn/cloudfront/outputs.tf
+++ _sub/cdn/cloudfront/outputs.tf
@@ -1,7 +1,3 @@
-output "oai_arn" {
-  value = "${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"  
-}
-
 output "distribution_domain_name" {
   value = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
 }
--- _sub/cdn/cloudfront_oai/outputs.tf
+++ _sub/cdn/cloudfront_oai/outputs.tf
@@ -1,3 +1,7 @@
 output "origin_access_identity" {
   value = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
 }
+
+output "oai_arn" {
+  value = "${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"  
+}
