d702df7464fbdaa38165e42723d7d01f09a5c74b
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-08-09 14:34:01 +0200

    fix sub cf

--- cdn/cloudfront-sub/outputs.tf
+++ cdn/cloudfront-sub/outputs.tf
@@ -7,5 +7,5 @@ output "aws_cloudfront_distribution_domain_name" {
 }
 
 output "aws_cloudfront_distribution_domain_name_commented" {
-  value = "${var.enable_output_comments ? module.aws_cf_dist.distribution_domain_name(var.cf_dist_comment) : module.aws_cf_dist.distribution_domain_name}"
+  value = "${var.enable_output_comments ? "${module.aws_cf_dist.distribution_domain_name(var.cf_dist_comment)}" : module.aws_cf_dist.distribution_domain_name}"
 }
