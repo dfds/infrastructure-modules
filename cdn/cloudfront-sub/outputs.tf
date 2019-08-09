82a607d1349e86f45e2b34a9554397519919edf8
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-08-09 14:45:58 +0200

    fix sub cf

--- cdn/cloudfront-sub/outputs.tf
+++ cdn/cloudfront-sub/outputs.tf
@@ -1,17 +1,18 @@
+# output "aws_cloudfront_oai_arn" {
+#   value = "${module.aws_cf_oai.oai_arn} (${var.cf_dist_comment})"
+# }
+
 output "aws_cloudfront_oai_arn" {
-  value = "${module.aws_cf_oai.oai_arn} (${var.cf_dist_comment})"
+  # value = "${module.aws_cf_oai.oai_arn} (${var.cf_dist_comment})"
+
+  value = "${var.enable_output_comments ? "${module.aws_cf_oai.oai_arn} (${var.cf_dist_comment})" : module.aws_cf_oai.oai_arn }" 
 }
+
+# output "aws_cloudfront_distribution_domain_name" {
+#   value = "${module.aws_cf_dist.distribution_domain_name} (${var.cf_dist_comment})"
+# }
 
 output "aws_cloudfront_distribution_domain_name" {
-  value = "${module.aws_cf_dist.distribution_domain_name} (${var.cf_dist_comment})"
-}
-
-output "aws_cloudfront_distribution_domain_name_commented" {
-  # value = "${var.enable_output_comments ? "${module.aws_cf_dist.distribution_domain_name(var.cf_dist_comment)}" : "${module.aws_cf_dist.distribution_domain_name}" }"
-  # env_name = "${var.env == "prod"? "" : "-${var.env}"}"
-
-  # value = "${var.enable_output_comments ? module.aws_cf_dist.distribution_domain_name : ""}"
-
   value = "${var.enable_output_comments ? "${module.aws_cf_dist.distribution_domain_name} (${var.cf_dist_comment})" : module.aws_cf_dist.distribution_domain_name }" 
 }
 
