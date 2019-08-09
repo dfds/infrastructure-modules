2514670addf84a3bff181b2dac8c62c8cdfebb89
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-08-09 13:27:20 +0200

    fix sub cf

--- static-content-hosting/s3-app/outputs.tf
+++ static-content-hosting/s3-app/outputs.tf
@@ -2,6 +2,6 @@
 # 
 # --------------------------------------------------
 
-output "aws_s3_webapp_domain_name" {
-  value = "${module.s3_app.bucket_domain_name} (${var.s3_app_bucket})"
+output "aws_s3_bucket_domain_name" {
+  value = "${module.s3_app.bucket_domain_name}" # (${var.s3_app_bucket})"  
 }
