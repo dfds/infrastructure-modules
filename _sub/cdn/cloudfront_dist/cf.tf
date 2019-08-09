d5c3a01d5ca8a5fff467c4a4edbb228417d820a6
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-08-09 15:49:59 +0200

    fix sub cf

--- _sub/cdn/cloudfront_dist/cf.tf
+++ _sub/cdn/cloudfront_dist/cf.tf
@@ -5,7 +5,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   viewer_certificate{
     cloudfront_default_certificate = "${var.acm_certificate_arn == "" ? true: false}"
     acm_certificate_arn = "${var.acm_certificate_arn}"
-    ssl_support_method = "sni-only"
+    # ssl_support_method = "sni-only"
     minimum_protocol_version = "TLSv1" # TLSv1.2_2018 ?    
   }
 
