c8e741db540f5e2895ae79d36e0113ce48baffc5
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-08-13 10:25:17 +0200

    fix sub cf

--- cdn/cloudfront-sub/dependencies.tf
+++ cdn/cloudfront-sub/dependencies.tf
@@ -1,5 +1,5 @@
 locals {
-  cf_dist_comment_lower = "${lower(var.cf_dist_comment)}"
+#   cf_dist_comment_lower = "${lower(var.cf_dist_comment)}"
 
-  lamda_edge_prefix = "${replace(local.cf_dist_comment_lower, " ", "-")}"
+  lamda_edge_prefix = "${replace(lower(var.cf_dist_comment), " ", "-")}"
 }
