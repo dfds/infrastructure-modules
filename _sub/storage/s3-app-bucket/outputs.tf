427fb4b0d1e1740d2e621dab8bcf750662c795b8
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-08-14 09:31:13 +0200

    removed unnecessary bucket name output

--- _sub/storage/s3-app-bucket/outputs.tf
+++ _sub/storage/s3-app-bucket/outputs.tf
@@ -1,7 +1,3 @@
 output "bucket_domain_name" {
   value = aws_s3_bucket.bucket.bucket_regional_domain_name
 }
-
-output "bucket_name" {
-  value = "${element(concat(aws_s3_bucket.bucket.*.id, list("")), 0)}"
-}
