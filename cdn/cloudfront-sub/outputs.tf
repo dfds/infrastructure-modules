89ec47b11aa7e119f8fa53193d0a5467cceeba83
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-08-14 09:36:20 +0200

    minor fix

--- cdn/cloudfront-sub/outputs.tf
+++ cdn/cloudfront-sub/outputs.tf
@@ -1,4 +1,9 @@
 output "aws_cloudfront_oai_arn" {
+  # value = "${module.aws_cf_oai.oai_arn} (${var.cf_dist_comment})"
+
   value = "${var.enable_output_comments ? "${module.aws_cf_oai.oai_arn} (${var.cf_dist_comment})" : module.aws_cf_oai.oai_arn }" 
 }
 
+output "aws_cloudfront_distribution_domain_name" {
+  value = "${var.enable_output_comments ? "${module.aws_cf_dist.distribution_domain_name} (${var.cf_dist_comment})" : module.aws_cf_dist.distribution_domain_name }" 
+}
