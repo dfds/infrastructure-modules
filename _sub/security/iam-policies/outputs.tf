daef7374707ff84e713d2fca143a856f9ca5f882
Rasmus Rask <raras@dfds.com>
2019-09-06 11:48:07 +0200

    Added missing iam-policies output for ecr-push

--- _sub/security/iam-policies/outputs.tf
+++ _sub/security/iam-policies/outputs.tf
@@ -2,6 +2,10 @@ output "admin" {
     value = "${data.aws_iam_policy_document.admin.json}"
 }
 
+output "push_to_ecr" {
+    value = "${data.aws_iam_policy_document.admin.json}"
+}
+
 output "create_org_account" {
     value = "${data.aws_iam_policy_document.create_org_account.json}"
 }
