b9275078c736f54bd37945c7eb369bc9b93f42d9
Rasmus Rask <raras@dfds.com>
2019-09-06 13:17:37 +0200

    Fixed iam-policies output for ecr_push

--- _sub/security/iam-policies/outputs.tf
+++ _sub/security/iam-policies/outputs.tf
@@ -3,7 +3,7 @@ output "admin" {
 }
 
 output "push_to_ecr" {
-    value = "${data.aws_iam_policy_document.admin.json}"
+    value = "${data.aws_iam_policy_document.push_to_ecr.json}"
 }
 
 output "create_org_account" {
