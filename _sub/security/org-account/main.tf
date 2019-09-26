eb3671ff8f31d2566e90e18d35422441483f4e05
Rasmus Rask <raras@dfds.com>
2019-09-26 17:24:16 +0200

    Specified dependency for unsubscribe AWS spam

--- _sub/security/org-account/main.tf
+++ _sub/security/org-account/main.tf
@@ -12,6 +12,8 @@ resource "aws_organizations_account" "org_account" {
 
 resource "null_resource" "ubsubscribe_spam" {
 
+  depends_on = ["aws_organizations_account.org_account"]
+
   provisioner "local-exec" {
     command = "curl -v 'https://pages.awscloud.com/index.php/leadCapture/save2' --data 'Email=${replace(var.email, "@", "%40")}&preferenceCenterCategory=no&preferenceCenterGettingStarted=no&preferenceCenterOnlineInPersonEvents=no&preferenceCenterMonthlyAWSNewsletter=no&preferenceCenterTrainingandBestPracticeContent=no&preferenceCenterProductandServiceAnnoucements=no&preferenceCenterSurveys=no&PreferenceCenter_AWS_Partner_Events_Co__c=no&preferenceCenterOtherAWSCommunications=no&formVid=19260'"
   }
