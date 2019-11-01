62884e95bcd77300308ff5d2af53246e0c17ab36
Rasmus Rask <raras@dfds.com>
2019-11-01 10:05:21 +0100

    Kubeconfig path fix 2

--- compute/eks-ec2/dependencies.tf
+++ compute/eks-ec2/dependencies.tf
@@ -1,3 +1,12 @@
-locals {
-    kubeconfig_path = "${pathexpand("~/.kube/config_${var.eks_cluster_name}")}"
+data "null_data_source" "paths" {
+
+    inputs = {
+        kubeconfig = "${pathexpand("~/.kube/config_${var.eks_cluster_name}")}"
+    }
+
 }
+
+# locals {
+#     # kubeconfig_path = "${pathexpand("~/.kube/config_${var.eks_cluster_name}")}"
+#     # kubeconfig_path = "${path.root}/${var.eks_cluster_name}.config"
+# }
