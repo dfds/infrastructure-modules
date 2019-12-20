492bd2a489b9b03845bddb8614d9ffa434ec850c
Rasmus Rask <raras@dfds.com>
2019-12-20 11:17:17 +0100

    security-group-eks-node

--- _sub/network/security-group-eks-node/main.tf
+++ _sub/network/security-group-eks-node/main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "eks-node" {
   name        = "eks-${var.cluster_name}-node"
   description = "Security group for all nodes in the cluster"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   egress {
     from_port   = 0
@@ -10,20 +10,18 @@ resource "aws_security_group" "eks-node" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = "${
-    map(
-     "Name", "eks-${var.cluster_name}-node",
-     "kubernetes.io/cluster/${var.cluster_name}", "owned",
-    )
-  }"
+  tags = {
+    "Name"                                      = "eks-${var.cluster_name}-node"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
 }
 
 resource "aws_security_group_rule" "eks-node-ingress-self" {
   description              = "Allow node to communicate with each other"
   from_port                = 0
   protocol                 = "-1"
-  security_group_id        = "${aws_security_group.eks-node.id}"
-  source_security_group_id = "${aws_security_group.eks-node.id}"
+  security_group_id        = aws_security_group.eks-node.id
+  source_security_group_id = aws_security_group.eks-node.id
   to_port                  = 65535
   type                     = "ingress"
 }
@@ -32,8 +30,8 @@ resource "aws_security_group_rule" "eks-node-ingress-cluster" {
   description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
   from_port                = 1025
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.eks-node.id}"
-  source_security_group_id = "${var.autoscale_security_group}"
+  security_group_id        = aws_security_group.eks-node.id
+  source_security_group_id = var.autoscale_security_group
   to_port                  = 65535
   type                     = "ingress"
 }
@@ -42,19 +40,20 @@ resource "aws_security_group_rule" "eks-cluster-ingress-node-https" {
   description              = "Allow pods to communicate with the cluster API Server"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = "${var.autoscale_security_group}"
-  source_security_group_id = "${aws_security_group.eks-node.id}"
+  security_group_id        = var.autoscale_security_group
+  source_security_group_id = aws_security_group.eks-node.id
   to_port                  = 443
   type                     = "ingress"
 }
 
 #Enable SSH access to nodes by tfvars set to 1
 resource "aws_security_group_rule" "ssh-access-to-worker-nodes" {
-  description              = "Allow SSH access to worker nodes"
-  cidr_blocks              = "${var.ssh_ip_whitelist}"
-  from_port                = 22
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.eks-node.id}"
-  to_port                  = 22
-  type                     = "ingress"
+  description       = "Allow SSH access to worker nodes"
+  cidr_blocks       = var.ssh_ip_whitelist
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks-node.id
+  to_port           = 22
+  type              = "ingress"
 }
+
