1eb3e6dd9bc13edae38f8450b99e20e80adbfdc7
Rasmus Rask <raras@dfds.com>
2019-12-21 21:33:54 +0100

    rds-postgres-harbor

--- _sub/database/rds-postgres-harbor/db-server-instance.tf
+++ _sub/database/rds-postgres-harbor/db-server-instance.tf
@@ -1,20 +1,21 @@
 resource "aws_db_instance" "instance" {
-  count             = "${var.deploy}"
-  allocated_storage = "${var.db_storage_size}"
+  count             = var.deploy
+  allocated_storage = var.db_storage_size
   storage_type      = "gp2"
   engine            = "postgres"
-  engine_version    = "${var.postgresdb_engine_version}"
-  instance_class    = "${var.db_instance_size}"
-  identifier_prefix         = "${var.ressource_name_prefix}"
-  name              = "${var.db_name}"
-  username          = "${var.db_username}"
-  password          = "${var.db_password}"
-  port              = "${var.port}"
+  engine_version    = var.postgresdb_engine_version
+  instance_class    = var.db_instance_size
+  identifier_prefix = var.ressource_name_prefix
+  name              = var.db_name
+  username          = var.db_username
+  password          = var.db_password
+  port              = var.port
 
-  vpc_security_group_ids      = ["${aws_security_group.db.id}"]
-  db_subnet_group_name        = "${aws_db_subnet_group.harbor-db-sg.id}"
+  vpc_security_group_ids      = [aws_security_group.db[0].id]
+  db_subnet_group_name        = aws_db_subnet_group.harbor-db-sg[0].id
   multi_az                    = true
   allow_major_version_upgrade = false
   deletion_protection         = false
   skip_final_snapshot         = true
 }
+
--- _sub/database/rds-postgres-harbor/securitygroup.tf
+++ _sub/database/rds-postgres-harbor/securitygroup.tf
@@ -3,10 +3,10 @@
 # ------------------------------------------------------------------------------
 
 resource "aws_db_subnet_group" "harbor-db-sg" {
-  count       = "${var.deploy}"
-  name_prefix = "${var.ressource_name_prefix}"
+  count       = var.deploy
+  name_prefix = var.ressource_name_prefix
   description = "Database subnet group for harbor"
-  subnet_ids  = ["${var.subnet_ids}"]
+  subnet_ids  = var.subnet_ids
 
   tags = {
     Name = "Harbor-rds subnet group"
@@ -14,39 +14,38 @@ resource "aws_db_subnet_group" "harbor-db-sg" {
 
   lifecycle {
     create_before_destroy = true
-  }  
+  }
 }
 
 # ------------------------------------------------------------------------------
 # CREATE THE SECURITY GROUP THAT CONTROLS WHAT TRAFFIC CAN CONNECT TO THE DB
 # ------------------------------------------------------------------------------
 resource "aws_security_group" "db" {
-  count       = "${var.deploy}"
-  name_prefix = "${var.ressource_name_prefix}"
+  count       = var.deploy
+  name_prefix = var.ressource_name_prefix
   description = "Security group for Harbor Postgres Db"
-  vpc_id      = "${var.vpc_id}"
-  tags = "${
-    map(
-     "Name", "Harbor-db"
-    )
-  }"
+  vpc_id      = var.vpc_id
+  tags = {
+    "Name" = "Harbor-db"
+  }
 
   lifecycle {
     create_before_destroy = true
-  }  
+  }
 }
 
 resource "aws_security_group_rule" "allow_connections_from_security_group" {
-  count        = "${var.deploy && length(var.allow_connections_from_security_groups) >= 0 ? length(var.allow_connections_from_security_groups) : 0}"
+  count                    = var.deploy && length(var.allow_connections_from_security_groups) >= 0 ? length(var.allow_connections_from_security_groups) : 0
   type                     = "ingress"
-  from_port                = "${var.port}"
-  to_port                  = "${var.port}"
+  from_port                = var.port
+  to_port                  = var.port
   protocol                 = "tcp"
-  source_security_group_id = "${element(var.allow_connections_from_security_groups, count.index)}"
+  source_security_group_id = element(var.allow_connections_from_security_groups, count.index)
 
-  security_group_id = "${element(concat(aws_security_group.db.*.id, list("")), 0)}"  
+  security_group_id = element(concat(aws_security_group.db.*.id, [""]), 0)
 
   lifecycle {
     create_before_destroy = true
-  }  
+  }
 }
+
