d565e2082b494f9aa067665bb58ac3356fa7d9ec
Rasmus Rask <raras@dfds.com>
2020-01-06 10:55:26 +0100

    Default value of eks_worker_inotify_max_user_watches as power of two

--- compute/eks-ec2/vars.tf
+++ compute/eks-ec2/vars.tf
@@ -57,7 +57,7 @@ variable "eks_worker_ssh_public_key" {
 }
 
 variable "eks_worker_inotify_max_user_watches" {
-  default = 100000 # default t3.large is 8192 which is too low
+  default = 131072 # default t3.large is 8192 which is too low
 }
 
 variable "eks_public_s3_bucket" {
