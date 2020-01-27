dbf131bf5dbfc42297e2060a3ed6a1cf6487cf1b
Willi Carlsen <carlsenwilli@gmail.com>
2020-01-27 14:35:15 +0100

    Added required variables from EKS pipeline to module vars
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- storage/s3-eks-public/vars.tf
+++ storage/s3-eks-public/vars.tf
@@ -16,3 +16,30 @@ variable "eks_public_s3_bucket" {
   default     = ""
 }
 
+# --------------------------------------------------
+# Unused variables - to provent TF warning/error:
+# Using a variables file to set an undeclared variable is deprecated and will
+# become an error in a future release. If you wish to provide certain "global"
+# settings to all configurations in your organization, use TF_VAR_...
+# environment variables to set these instead.
+# --------------------------------------------------
+
+variable "terraform_state_region" {
+  type    = string
+  default = ""
+}
+
+variable "aws_workload_account_id" {
+  type    = string
+  default = ""
+}
+
+variable "workload_dns_zone_name" {
+  type    = string
+  default = ""
+}
+
+variable "terraform_state_s3_bucket" {
+  type    = string
+  default = ""
+}
