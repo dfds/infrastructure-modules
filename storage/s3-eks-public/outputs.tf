6685ac10ed8a547c34316877560b0f4a40cf95fa
Willi Carlsen <carlsenwilli@gmail.com>
2020-01-27 10:59:39 +0100

    Converted s3-eks-public to terraform 0.12
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- storage/s3-eks-public/outputs.tf
+++ storage/s3-eks-public/outputs.tf
@@ -1,3 +1,4 @@
 output "bucket_name" {
-  value = "${module.s3_bucket.bucket_name}"
+  value = module.s3_bucket.bucket_name
 }
+
--- storage/s3-eks-public/vars.tf
+++ storage/s3-eks-public/vars.tf
@@ -3,15 +3,16 @@
 # --------------------------------------------------
 
 variable "aws_region" {
-  type = "string"
+  type = string
 }
 
 variable "aws_assume_role_arn" {
-  type = "string"
+  type = string
 }
 
 variable "eks_public_s3_bucket" {
   description = "The name of the public S3 bucket, where non-sensitive Kubeconfig will be copied to."
-  type = "string"
-  default = ""
+  type        = string
+  default     = ""
 }
+
