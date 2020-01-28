3591711658c9e6af9731af7bb934ab3994ee0e04
Rasmus Rask <raras@dfds.com>
2020-01-28 13:48:40 +0100

    QA pipeline update

--- src/qa-test-eks.sh
+++ src/qa-test-eks.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -eu -o pipefail
-
 ACTION=$1
 
-# Init
-helm init --client-only
-helm repo add servicecatalog "https://svc-catalog-charts.storage.googleapis.com"
-helm repo add aws-sb "https://awsservicebroker.s3.amazonaws.com/charts"
-az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+
+if [ "$ACTION" = "init" ]; then
+    helm init --client-only
+    helm repo add servicecatalog "https://svc-catalog-charts.storage.googleapis.com"
+    helm repo add aws-sb "https://awsservicebroker.s3.amazonaws.com/charts"
+    az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+fi
 
 
 if [ "$ACTION" = "plan" ]; then
     # Show the plan of what will be applied
     terragrunt plan-all --terragrunt-working-dir ./test/integration --terragrunt-source-update --terragrunt-non-interactive -input=false
+
 fi
 
 
