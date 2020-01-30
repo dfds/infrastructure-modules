0ba401cf000a6f2ab4d2c02052678cc8197a905e
Rasmus Rask <raras@dfds.com>
2020-01-30 14:22:45 +0100

    Updated default EKS component versions according to https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html

--- _sub/compute/eks-addons/dependencies.tf
+++ _sub/compute/eks-addons/dependencies.tf
@@ -2,32 +2,26 @@
 # https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html 
 
 locals {
-    kubeproxy_version_map = "${map(
-        "1.11", "1.11.8",
-        "1.12", "1.12.6",
-        "1.13", "1.13.10",
-        "1.14", "1.14.6"
-    )}"
-    coredns_version_map = "${map(
-        "1.11", "1.1.3",
-        "1.12", "1.2.2",
-        "1.13", "1.2.6",
-        "1.14", "1.3.1"
-    )}"
-    vpccni_version_map = "${map(
-        "1.11", "1.5.3",
-        "1.12", "1.5.3",
-        "1.13", "1.5.3",
-        "1.14", "1.5.3"
-    )}"
+  kubeproxy_version_map = "${map(
+    "1.13", "1.13.12",
+    "1.14", "1.14.9"
+  )}"
+  coredns_version_map = "${map(
+    "1.13", "1.6.6",
+    "1.14", "1.6.6"
+  )}"
+  vpccni_version_map = "${map(
+    "1.13", "1.5.5",
+    "1.14", "1.5.5"
+  )}"
 }
 
 
 # Lookup actual add-on versions
 locals {
-    kubeproxy_version = "${var.kubeproxy_version_override == "" ? local.kubeproxy_version_map[var.cluster_version] : var.kubeproxy_version_override}"
-    coredns_version = "${var.coredns_version_override == "" ? local.coredns_version_map[var.cluster_version] : var.coredns_version_override}"
-    vpccni_version = "${var.vpccni_version_override == "" ? local.vpccni_version_map[var.cluster_version] : var.vpccni_version_override}"
-    vpccni_minorversion = "${join(".", slice(split(".", local.vpccni_version), 0, 2))}"
+  kubeproxy_version   = "${var.kubeproxy_version_override == "" ? local.kubeproxy_version_map[var.cluster_version] : var.kubeproxy_version_override}"
+  coredns_version     = "${var.coredns_version_override == "" ? local.coredns_version_map[var.cluster_version] : var.coredns_version_override}"
+  vpccni_version      = "${var.vpccni_version_override == "" ? local.vpccni_version_map[var.cluster_version] : var.vpccni_version_override}"
+  vpccni_minorversion = "${join(".", slice(split(".", local.vpccni_version), 0, 2))}"
 
 }
