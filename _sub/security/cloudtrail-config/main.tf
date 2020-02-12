8422999adbbb7ceea0aabec04b78db11e1ebe518
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-02-12 08:45:54 +0100

    Upgraded _sub/security/cloudtrail-config

--- _sub/security/cloudtrail-config/main.tf
+++ _sub/security/cloudtrail-config/main.tf
@@ -1,11 +1,11 @@
-
 resource "aws_cloudtrail" "cloudtrail" {
-  count                         = "${var.deploy}"
-  name                          = "${var.trail_name}"
-  s3_bucket_name                = "${var.s3_bucket}"
+  count                         = var.deploy
+  name                          = var.trail_name
+  s3_bucket_name                = var.s3_bucket
   is_multi_region_trail         = true
-  is_organization_trail         = "${var.is_organization_trail}"
+  is_organization_trail         = var.is_organization_trail
   include_global_service_events = true
   enable_logging                = true
   enable_log_file_validation    = true
 }
+
