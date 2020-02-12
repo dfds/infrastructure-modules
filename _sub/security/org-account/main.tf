d290182d1747d57b20d39e4f5636ee48f63ed803
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-02-12 10:24:40 +0100

    Upgraded _sub/security/org-account

--- _sub/security/org-account/main.tf
+++ _sub/security/org-account/main.tf
@@ -1,9 +1,9 @@
 resource "aws_organizations_account" "org_account" {
-  name                       = "${lower(var.name)}"
-  email                      = "${var.email}"
+  name                       = lower(var.name)
+  email                      = var.email
   iam_user_access_to_billing = "ALLOW"
-  role_name                  = "${var.org_role_name}"
-  parent_id                  = "${var.parent_id}"
+  role_name                  = var.org_role_name
+  parent_id                  = var.parent_id
 
   provisioner "local-exec" {
     command = "sleep ${var.sleep_after}"
@@ -11,10 +11,10 @@ resource "aws_organizations_account" "org_account" {
 }
 
 resource "null_resource" "ubsubscribe_spam" {
-
-  depends_on = ["aws_organizations_account.org_account"]
+  depends_on = [aws_organizations_account.org_account]
 
   provisioner "local-exec" {
     command = "curl -v 'https://pages.awscloud.com/index.php/leadCapture/save2' --data 'Email=${urlencode(var.email)}&preferenceCenterCategory=no&preferenceCenterGettingStarted=no&preferenceCenterOnlineInPersonEvents=no&preferenceCenterMonthlyAWSNewsletter=no&preferenceCenterTrainingandBestPracticeContent=no&preferenceCenterProductandServiceAnnoucements=no&preferenceCenterSurveys=no&PreferenceCenter_AWS_Partner_Events_Co__c=no&preferenceCenterOtherAWSCommunications=no&formVid=19260'"
   }
 }
+
--- _sub/security/org-account/outputs.tf
+++ _sub/security/org-account/outputs.tf
@@ -1,19 +1,20 @@
 output "name" {
-  value = "${aws_organizations_account.org_account.name}"
+  value = aws_organizations_account.org_account.name
 }
 
 output "id" {
-  value = "${aws_organizations_account.org_account.id}"
+  value = aws_organizations_account.org_account.id
 }
 
 output "email" {
-  value = "${aws_organizations_account.org_account.email}"
+  value = aws_organizations_account.org_account.email
 }
 
 output "org_role_name" {
-  value = "${aws_organizations_account.org_account.role_name}"
+  value = aws_organizations_account.org_account.role_name
 }
 
 output "org_role_arn" {
   value = "arn:aws:iam::${aws_organizations_account.org_account.id}:role/${aws_organizations_account.org_account.role_name}"
 }
+
