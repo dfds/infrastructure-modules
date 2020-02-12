b43fad8790ad5d40363c20e9683ff5b207455729
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-02-12 08:42:04 +0100

    Upgraded security/cloudtrail-config

--- security/cloudtrail-master/main.tf
+++ security/cloudtrail-master/main.tf
@@ -1,17 +1,18 @@
 provider "aws" {
-    region = "${var.aws_region}"
-    version = "~> 1.40"
+  region  = var.aws_region
+  version = "~> 2.43"
 }
 
 terraform {
-    # The configuration for this backend will be filled in by Terragrunt
-    backend "s3" {}
-    required_version = "~> 0.11.7"
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {
+  }
 }
 
 module "cloudtrail_central" {
-  source = "../../_sub/security/cloudtrail-config"
-  s3_bucket = "${var.cloudtrail_central_s3_bucket}"
-  trail_name = "org-audit"
+  source                = "../../_sub/security/cloudtrail-config"
+  s3_bucket             = var.cloudtrail_central_s3_bucket
+  trail_name            = "org-audit"
   is_organization_trail = true
 }
+
