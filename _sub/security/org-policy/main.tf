deaf6b5b08d1998221c0111a8022be0007a51469
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-02-12 11:31:35 +0100

    _sub/security/org-policy

--- _sub/security/org-policy/main.tf
+++ _sub/security/org-policy/main.tf
@@ -1,11 +1,12 @@
 resource "aws_organizations_policy" "policy" {
-  name        = "${var.name}"
-  description = "${var.description}"
-  content     = "${var.policy}"
+  name        = var.name
+  description = var.description
+  content     = var.policy
 }
 
 resource "aws_organizations_policy_attachment" "attach" {
-  count     = "${var.attach_target_id != "" ? 1 : 0}"
-  policy_id = "${aws_organizations_policy.policy.id}"
-  target_id = "${var.attach_target_id}"
+  count     = var.attach_target_id != "" ? 1 : 0
+  policy_id = aws_organizations_policy.policy.id
+  target_id = var.attach_target_id
 }
+
