cec5ab24575c3bb29362b4bf1e9e45537d95fa26
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-02-12 09:07:45 +0100

    Upgraded _sub/security/iam-policies

--- _sub/security/iam-policies/outputs.tf
+++ _sub/security/iam-policies/outputs.tf
@@ -1,19 +1,23 @@
 output "admin" {
-    value = "${data.aws_iam_policy_document.admin.json}"
+  value = data.aws_iam_policy_document.admin.json
 }
 
 output "push_to_ecr" {
-    value = "${data.aws_iam_policy_document.push_to_ecr.json}"
+  value = data.aws_iam_policy_document.push_to_ecr.json
 }
 
 output "create_org_account" {
-    value = "${data.aws_iam_policy_document.create_org_account.json}"
+  value = data.aws_iam_policy_document.create_org_account.json
 }
 
 output "trusted_account" {
-    value = "${element(concat(data.aws_iam_policy_document.trusted_account.*.json, list("")), 0)}"
+  value = element(
+    concat(data.aws_iam_policy_document.trusted_account.*.json, [""]),
+    0,
+  )
 }
 
 output "assume_noncore_accounts" {
-    value = "${data.aws_iam_policy_document.assume_noncore_accounts.json}"
+  value = data.aws_iam_policy_document.assume_noncore_accounts.json
 }
+
