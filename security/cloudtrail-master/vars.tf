d0dd439100c2a6001cfb237ac5c8e53f8cda8b48
Henrik Stanley Mortensen <hemor@dfds.com>
2020-02-14 12:58:56 +0100

    upgraded security/cloudtrail-master to tf 0.12 [ch958]

--- security/cloudtrail-master/vars.tf
+++ security/cloudtrail-master/vars.tf
@@ -1,10 +1,10 @@
 #Initializes the variables needed to generate a new account
 #The values vill be propagated via a tfvars file
 variable "aws_region" {
-  type = "string"
+  type = string
 }
 
 variable "cloudtrail_central_s3_bucket" {
-  type = "string"
+  type = string
 }
 
