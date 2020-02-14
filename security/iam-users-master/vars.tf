93a04c57699d31d084cc50641fae50f8efb16f12
Henrik Stanley Mortensen <hemor@dfds.com>
2020-02-14 13:04:26 +0100

    upgraded security/iam-users-master to tf 0.12 [ch958]

--- security/iam-users-master/vars.tf
+++ security/iam-users-master/vars.tf
@@ -1,23 +1,24 @@
 variable "aws_region" {
-  type = "string"
+  type = string
 }
 
 variable "iam_user_name" {
-    description = "Name of IAM user"
-    type = "string"
+  description = "Name of IAM user"
+  type        = string
 }
 
 variable "create_org_account_iam_policy_name" {
-    description = "Policy granting access to create new AWS Organization accounts"
-    type = "string"
+  description = "Policy granting access to create new AWS Organization accounts"
+  type        = string
 }
 
 variable "assume_noncore_accounts_iam_policy_name" {
-    description = "Policy granting access to create new AWS Organization accounts"
-    type = "string"
+  description = "Policy granting access to create new AWS Organization accounts"
+  type        = string
 }
 
 variable "core_account_role_arns" {
-    description = "List of core account (e.g. Core, QA, Master, Security) root ARNs"
-    type = "list"
+  description = "List of core account (e.g. Core, QA, Master, Security) root ARNs"
+  type        = list(string)
 }
+
