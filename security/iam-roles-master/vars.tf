87c47e62ce3e7cc1e7bb04d28c5669922cbfea20
Henrik Stanley Mortensen <hemor@dfds.com>
2020-02-14 13:02:01 +0100

    upgraded security/iam-roles-master to tf 0.12 [ch958]

--- security/iam-roles-master/vars.tf
+++ security/iam-roles-master/vars.tf
@@ -1,23 +1,24 @@
 variable "aws_region" {
-  type = "string"
+  type = string
 }
 
 variable "iam_role_name" {
-    description = "Name of IAM role"
-    type = "string"
+  description = "Name of IAM role"
+  type        = string
 }
 
 variable "iam_role_description" {
-    description = "Description for IAM role"
-    type = "string"
+  description = "Description for IAM role"
+  type        = string
 }
 
 variable "iam_role_trusted_account_root_arn" {
-    description = "The ARN of the account trusted to assume the role"
-    type = "string"
+  description = "The ARN of the account trusted to assume the role"
+  type        = string
 }
 
 variable "create_org_account_iam_policy_name" {
-    description = "Policy granting access to create new AWS Organization accounts"
-    type = "string"
+  description = "Policy granting access to create new AWS Organization accounts"
+  type        = string
 }
+
