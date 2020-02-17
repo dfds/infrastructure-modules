1a92000fc634cbea46becdac1c3ba478e73492e7
Henrik Stanley Mortensen <hemor@dfds.com>
2020-02-17 14:14:27 +0100

    upgraded security/iam-roles-workload [ch1368]

--- security/iam-roles-workload/vars.tf
+++ security/iam-roles-workload/vars.tf
@@ -1,23 +1,24 @@
 variable "aws_region" {
-  type = "string"
+  type = string
 }
 
 variable "iam_role_name" {
-    description = "Name of IAM role"
-    type = "string"
+  description = "Name of IAM role"
+  type        = string
 }
 
 variable "iam_role_description" {
-    description = "Description for IAM role"
-    type = "string"
+  description = "Description for IAM role"
+  type        = string
 }
 
 variable "account_id_workload" {
-    description = "The ID of the account trusted to assume the role"
-    type = "string"
+  description = "The ID of the account trusted to assume the role"
+  type        = string
 }
 
 variable "admin_iam_policy_name" {
-    description = "Policy granting access to create new AWS Organization accounts"
-    type = "string"
+  description = "Policy granting access to create new AWS Organization accounts"
+  type        = string
 }
+
