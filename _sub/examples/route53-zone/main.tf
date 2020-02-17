9697d5ce7c525f0c8876bf269634e960e0fab6bc
Henrik Stanley Mortensen <hemor@dfds.com>
2020-02-17 14:33:32 +0100

    upgraded _sub/examples/route53-zone to tf 0.12 [ch1368]

--- _sub/examples/route53-zone/main.tf
+++ _sub/examples/route53-zone/main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  version = "~> 1.60.0"
-  region  = "${var.aws_region}"
+  version = "~> 2.43"
+  region  = var.aws_region
 }
 
 module "route53_zone" {
   source        = "../..//network/route53-zone"
-  dns_zone_name = "${var.dns_zone_name}"
+  dns_zone_name = var.dns_zone_name
 }
+
--- _sub/examples/route53-zone/outputs.tf
+++ _sub/examples/route53-zone/outputs.tf
@@ -1,3 +1,4 @@
 output "dns_zone_id" {
-  value = "${module.route53_zone.dns_zone_id}"
+  value = module.route53_zone.dns_zone_id
 }
+
