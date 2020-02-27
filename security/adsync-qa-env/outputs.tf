b09be6ace406e03cf2ccf65ff860405adb767144
Rasmus Rask <raras@dfds.com>
2020-02-27 14:10:45 +0100

    Rename activedirectory-qa to adsync-qa-env

--- security/adsync-qa-env/outputs.tf
+++ security/adsync-qa-env/outputs.tf
@@ -2,10 +2,6 @@ output "ec2_public_dns" {
   value = module.ec2_instance.public_dns
 }
 
-output "ec2_password" {
-  value = module.ec2_instance.password
-}
-
 output "ec2_password_data" {
   value = module.ec2_instance.password_data
 }
