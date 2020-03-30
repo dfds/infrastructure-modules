c855cca032c36e7bf21ddca227c97e55c24db3ee
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2020-03-30 15:08:13 +0200

    added output for traefik alb to use with alerts

--- _sub/compute/eks-alb/outputs.tf
+++ _sub/compute/eks-alb/outputs.tf
@@ -2,6 +2,10 @@ output "alb_fqdn" {
   value = element(concat(aws_lb.traefik.*.dns_name, [""]), 0)
 }
 
+output "alb_arn_suffix" {
+  value = "${aws_lb.traefik.*.arn_suffix}"
+}
+
 # output "dbg_subnet_ids" {
 #   value = "${data.terraform_remote_state.cluster.eks_cluster_subnet_ids}"
 # }
