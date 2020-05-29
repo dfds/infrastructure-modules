34a8f922a3d6fe14ecff7e118b53c0e26a7a0d9f
Johannes Hansen <jhans@dfds.com>
2020-05-29 07:15:43 +0200

    Added defaults back in
    Signed-off-by: Johannes Hansen <jhans@dfds.com>

--- database/postgres/vars.tf
+++ database/postgres/vars.tf
@@ -1,57 +1,65 @@
 variable "aws_region" {
-  type = string
+  type    = string
 }
 
 variable "application" {
-  type = string
+  type    = string
 }
 
 variable "environment" {
   type    = string
+  default = "prod"
 }
 
 variable "db_storage_type" {
-  type = string
+  type    = string
+  default = "gp2"
 }
 
 variable "db_instance_class" {
-  type = string
+  type    = string
+  default = "db.t2.micro"
 }
 
 variable "db_allocated_storage" {
-  type = string
+  type    = number
+  default = 20
 }
 
 variable "db_engine_major_version" {
-  type = string
+  type    = string
+  default = "10"
 }
 
 variable "db_engine_minor_version" {
-  type = string
+  type    = string
+  default = "9"
 }
 
 variable "db_name" {
-  type = string
+  type    = string
 }
 
 variable "db_master_username" {
-  type = string
+  type    = string
 }
 
 variable "db_master_password" {
-  type = string
+  type    = string
 }
 
 variable "db_port" {
-  type = number
+  type    = number
+  default = 5432
 }
 
 variable "db_deletion_protection" {
-  type = bool
+  type    = bool
   default = true
 }
 
 variable "skip_final_snapshot" {
-  type = bool
+  type    = bool
+  default = false
 }
 
