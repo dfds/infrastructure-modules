64e0d41efdb7e2baf993e05bcd68bf98076775da
Willi Carlsen <carlsenwilli@gmail.com>
2020-06-09 13:00:32 +0200

    Fixed bug with outputting asg ids for the ALB's to pick up

--- _sub/compute/eks-nodegroup-unmanaged/outputs.tf
+++ _sub/compute/eks-nodegroup-unmanaged/outputs.tf
@@ -1,4 +1,5 @@
 output "autoscaling_group_id" {
-  value = [for asg in aws_autoscaling_group.eks : asg.id]
+  # value = [for asg in aws_autoscaling_group.eks : asg.id]
+  value = try(aws_autoscaling_group.eks.*.id, [])
 }
 
