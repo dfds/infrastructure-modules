590a59809233a8bafde1fc03457a7e138078bee3
Willi Carlsen <carlsenwilli@gmail.com>
2020-09-22 11:26:49 +0200

    Allow sts for deploy users

--- security/org-capability-root/dependencies.tf
+++ security/org-capability-root/dependencies.tf
@@ -16,6 +16,7 @@ locals {
                 "cloudfront:*",
                 "globalaccelerator:*",
                 "iam:*",
+				"sts:*",
                 "importexport:*",
                 "kms:*",
                 "lambda:*",
