761c17b60fb74b9df6cabb7025fd575c7256e5b7
Willi Carlsen <carlsenwilli@gmail.com>
2020-09-23 14:19:21 +0200

    Created Kubernetes clusterrole submodule

---
+++ _sub/compute/k8s-clusterrole/main.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_cluster_role" "cloud_engineer" {
+  metadata {
+    name = var.name
+  }
+
+  dynamic "rule" {
+    for_each = [for r in var.rules: {
+      api_groups = r.api_groups
+      resources = r.resources
+      verbs = r.verbs
+    }]
+
+    content {
+      api_groups = rule.value.api_groups
+      resources = rule.value.resources
+      verbs = rule.value.verbs
+    }
+  }
+}
