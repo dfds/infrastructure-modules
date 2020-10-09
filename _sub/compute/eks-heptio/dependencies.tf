76ce7f6d817574a86e04860feedbe62119ad419c
Rasmus Rask <raras@dfds.com>
2020-10-09 17:43:45 +0200

    Replace inline templates with TF templates, fix usernames, cleanup etc.

---
+++ _sub/compute/eks-heptio/dependencies.tf
@@ -0,0 +1,25 @@
+data "template_file" "kubeconfig_admin" {
+  template = file("${path.module}/kubeconfig-admin.yaml")
+  vars = {
+    cluster_name = var.cluster_name
+    endpoint     = var.eks_endpoint
+    ca           = var.eks_certificate_authority
+    role_arn     = var.aws_assume_role_arn
+  }
+}
+
+data "template_file" "kubeconfig_saml" {
+  template = file("${path.module}/kubeconfig-saml.yaml")
+  vars = {
+    cluster_name = var.cluster_name
+    endpoint     = var.eks_endpoint
+    ca           = var.eks_certificate_authority
+  }
+}
+
+data "template_file" "default_auth_configmap" {
+  template = file("${path.module}/default-auth-cm.yaml")
+  vars = {
+    role_arn = var.eks_role_arn
+  }
+}
