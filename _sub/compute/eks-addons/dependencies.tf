1c2dd9fba79543b33db3c6f36205753a1354b1ab
Rasmus Rask <raras@dfds.com>
2020-10-15 15:04:25 +0200

    Update EKS addon versions for 1.16 and upwards

--- _sub/compute/eks-addons/dependencies.tf
+++ _sub/compute/eks-addons/dependencies.tf
@@ -2,37 +2,37 @@
 # https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html
 
 locals {
-  kubeproxy_version_map = {
-    "1.13" = "1.13.12"
-    "1.14" = "1.14.9"
-    "1.15" = "1.15.11"
-    "1.16" = "1.16.12"
-    "1.17" = "1.17.7"
+  vpccni_version_map = {
+    "1.14" = "1.5.5"
+    "1.15" = "1.6.1"
+    "1.16" = "1.7.5"
+    "1.17" = "1.7.5"
+    "1.18" = "1.7.5"
   }
 
   coredns_version_map = {
-    "1.13" = "1.6.6"
     "1.14" = "1.6.6"
     "1.15" = "1.6.6"
     "1.16" = "1.6.6"
     "1.17" = "1.6.6"
+    "1.18" = "1.7.0"
   }
 
-  vpccni_version_map = {
-    "1.13" = "1.5.5"
-    "1.14" = "1.5.5"
-    "1.15" = "1.6.1"
-    "1.16" = "1.6.3"
-    "1.17" = "1.6.3"
+  kubeproxy_version_map = {
+    "1.14" = "1.14.9"
+    "1.15" = "1.15.11"
+    "1.16" = "1.16.13"
+    "1.17" = "1.17.9"
+    "1.18" = "1.18.8"
   }
 }
 
 # Lookup actual add-on versions
 locals {
-  kubeproxy_version   = var.kubeproxy_version_override == "" ? local.kubeproxy_version_map[var.cluster_version] : var.kubeproxy_version_override
-  coredns_version     = var.coredns_version_override == "" ? local.coredns_version_map[var.cluster_version] : var.coredns_version_override
   vpccni_version      = var.vpccni_version_override == "" ? local.vpccni_version_map[var.cluster_version] : var.vpccni_version_override
   vpccni_minorversion = join(".", slice(split(".", local.vpccni_version), 0, 2))
+  coredns_version     = var.coredns_version_override == "" ? local.coredns_version_map[var.cluster_version] : var.coredns_version_override
+  kubeproxy_version   = var.kubeproxy_version_override == "" ? local.kubeproxy_version_map[var.cluster_version] : var.kubeproxy_version_override
 }
 
 # Get current AWS region
