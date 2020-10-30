5c1031b1cf163fd8bba75eee81982eda38fd09b2
Fisher Richard <rifis@DFDS.COM>
2020-10-30 15:19:58 +0000

    WIP

--- _sub/compute/helm-goldpinger/main.tf
+++ _sub/compute/helm-goldpinger/main.tf
@@ -2,8 +2,7 @@ resource "helm_release" "goldpinger" {
   count = var.deploy ? 1 : 0
   name = "goldpinger"
   chart = "stable/goldpinger"
-  repository = "https://kubernetes-charts.storage.googleapis.com"
-  version = var.chart_version ? var.chart_version : null
+  version = var.chart_version != null ? var.chart_version : null
   namespace = var.namespace
 
   set {
@@ -15,4 +14,10 @@ resource "helm_release" "goldpinger" {
     name = "serviceMonitor.enabled"
     value = "true"
   }
+
+  set {
+    name  = "serviceMonitor.selector.release"
+    value = "monitoring"
+  }
+
 }
--- _sub/compute/helm-goldpinger/outputs.tf
+++ _sub/compute/helm-goldpinger/outputs.tf
@@ -1,11 +1,11 @@
 output "goldpinger_chart_name" {
-  value = helm_release.goldpinger.name
+  value = length(helm_release.goldpinger) > 0 ? helm_release.goldpinger[0].name : ""
 }
 
 output "goldpinger_chart_version" {
-  value = helm_release.goldpinger.chart_version
+  value = length(helm_release.goldpinger) > 0 ? helm_release.goldpinger[0].version : ""
 }
 
 output "goldpinger_chart_namespace" {
-  value = helm_release.goldpinger.namespace
+  value = length(helm_release.goldpinger) > 0 ? helm_release.goldpinger[0].namespace : ""
 }
