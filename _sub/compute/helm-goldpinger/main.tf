f96003e4d74bfa0faa8a108ab6945725a708c7d6
Willi Carlsen <carlsenwilli@gmail.com>
2020-11-05 21:08:34 +0100

    WIP goldpinger

--- _sub/compute/helm-goldpinger/main.tf
+++ _sub/compute/helm-goldpinger/main.tf
@@ -1,9 +1,13 @@
 resource "helm_release" "goldpinger" {
   count = var.deploy ? 1 : 0
   name = "goldpinger"
-  chart = "stable/goldpinger"
+  chart = "goldpinger"
+  repository = "https://kubernetes-charts.storage.googleapis.com"
   version = var.chart_version != null ? var.chart_version : null
   namespace = var.namespace
+  recreate_pods = true
+  create_namespace = true
+  force_update = true
 
   set {
     name = "priorityClassName"
