db8962b705c69518ad74b8eb776e8ebe28e195b6
Rasmus Rask <raras@dfds.com>
2020-11-11 09:04:05 +0100

    Added comments re: ASG min and max size

--- _sub/compute/eks-nodegroup-unmanaged/main.tf
+++ _sub/compute/eks-nodegroup-unmanaged/main.tf
@@ -25,13 +25,20 @@ resource "aws_placement_group" "cluster" {
   strategy = "cluster"
 }
 
+
+locals {
+  asg_min_size = 0 # Allow scaling an ASG to zero, e.g. for maintenance og dialing down sandbox clusters at night
+  asg_max_size = 2 * var.desired_size_per_subnet # Allows node roll-over script to double the instance count, to roll-over all nodes in an ASG in one operation
+}
+
+
 resource "aws_autoscaling_group" "eks" {
   count                = length(var.instance_types) > 0 ? length(var.subnet_ids) : 0
   name                 = "eks-${var.cluster_name}-${var.nodegroup_name}_${data.aws_subnet.subnet[count.index].availability_zone}"
   launch_configuration = try(aws_launch_configuration.eks[0].id, ["NA"])
   availability_zones   = toset([data.aws_subnet.subnet[count.index].availability_zone])
-  min_size             = 0
-  max_size             = 2 * var.desired_size_per_subnet
+  min_size             = local.asg_min_size
+  max_size             = local.asg_max_size
   desired_capacity     = var.desired_size_per_subnet
   vpc_zone_identifier  = toset([data.aws_subnet.subnet[count.index].id])
 
@@ -58,8 +65,8 @@ resource "aws_autoscaling_schedule" "eks" {
   count                  = var.is_sandbox && length(var.instance_types) > 0 ? 1 : 0
   autoscaling_group_name = aws_autoscaling_group.eks[0].name
   scheduled_action_name  = "Scale to zero"
-  desired_capacity       = 0
-  min_size               = 0
-  max_size               = 2 * var.desired_size_per_subnet
   recurrence             = var.scale_to_zero_cron
+  min_size               = local.asg_min_size
+  max_size               = local.asg_max_size
+  desired_capacity       = 0
 }
