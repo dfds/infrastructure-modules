85ef58bcef0ca5d34a2eadf807d0ba9c96f268a7
Fisher Richard <rifis@DFDS.COM>
2020-11-18 13:29:22 +0000

    added kubelet-preferred-address-types arg

--- _sub/compute/helm-metrics-server/main.tf
+++ _sub/compute/helm-metrics-server/main.tf
@@ -4,4 +4,8 @@ resource "helm_release" "metrics_server" {
     chart = "metrics-server"
     version = var.chart_version != null ? var.chart_version : null
     namespace = var.namespace != null ? var.namespace : null
+
+    values = [
+    "${file("${path.module}/values/values.yaml")}"
+  ]
 }
