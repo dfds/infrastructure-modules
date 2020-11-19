e1900d731376ec86a5c545303031738df5fa1e68
Rasmus Rask <raras@dfds.com>
2020-11-19 15:26:34 +0100

    Toggle Grafana notifiers and Alertmanager enable based on Slack hook defined

--- _sub/compute/helm-kube-prometheus-stack/main.tf
+++ _sub/compute/helm-kube-prometheus-stack/main.tf
@@ -10,36 +10,46 @@ resource "helm_release" "kube_prometheus_stack" {
   values = [
     templatefile("${path.module}/values/components.yaml", {
     }),
+
     templatefile("${path.module}/values/grafana.yaml", {
-      grafana_admin_password         = var.grafana_admin_password
-      grafana_priorityclass          = var.priority_class
-      grafana_ingress_path           = var.grafana_ingress_path
-      grafana_host                   = var.grafana_host
-      grafana_root_url               = "https://%(domain)s${var.grafana_ingress_path}"
+      grafana_admin_password = var.grafana_admin_password
+      grafana_priorityclass  = var.priority_class
+      grafana_ingress_path   = var.grafana_ingress_path
+      grafana_host           = var.grafana_host
+      grafana_root_url       = "https://%(domain)s${var.grafana_ingress_path}"
+    }),
+
+    length(var.slack_webhook) > 0 ? templatefile("${path.module}/values/grafana-notifiers.yaml", {
       grafana_notifier_name          = var.grafana_notifier_name
       grafana_slack_notifier_channel = var.slack_channel
       grafana_slack_webhook          = var.slack_webhook
-    }),
+    }) : "",
+
     templatefile("${path.module}/values/prometheus.yaml", {
       prometheus_priorityclass = var.priority_class
       prometheus_storageclass  = var.prometheus_storageclass
       prometheus_storage_size  = var.prometheus_storage_size
       prometheus_retention     = var.prometheus_retention
     }),
-    templatefile("${path.module}/values/alertmanager.yaml", {
-      alertmanager_priorityclass = var.priority_class
-      alertmanager_slack_channel = var.slack_channel
-      alertmanager_slack_webhook = var.slack_webhook
+
+    length(var.slack_webhook) > 0 ? templatefile("${path.module}/values/alertmanager-slack.yaml", {
+      alertmanager_priorityclass      = var.priority_class
+      alertmanager_slack_channel      = var.slack_channel
+      alertmanager_slack_webhook      = var.slack_webhook
       alertmanager_silence_namespaces = var.alertmanager_silence_namespaces
-    }),
+    }) : file("${path.module}/values/alertmanager-disabled.yaml"),
+
     templatefile("${path.module}/values/rules.yaml", {
       target_namespaces = var.target_namespaces
     }),
+
     templatefile("${path.module}/values/prometheus-operator.yaml", {
     }),
+
     templatefile("${path.module}/values/node-exporter.yaml", {
       prometheus_node_exporter_priorityclass = var.priority_class
     }),
+
     templatefile("${path.module}/values/kube-state-metrics.yaml", {
       kube_state_metrics_priorityclass = var.priority_class
     })
