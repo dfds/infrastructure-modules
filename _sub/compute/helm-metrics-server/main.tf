aa2879e2b34cb02b82caace7b6263ba886d6a0df
Rasmus Rask <raras@dfds.com>
2020-11-19 15:26:51 +0100

    Change deprecated reference in helm-metrics-server

--- _sub/compute/helm-metrics-server/main.tf
+++ _sub/compute/helm-metrics-server/main.tf
@@ -1,12 +1,12 @@
 resource "helm_release" "metrics_server" {
-    name = "metrics-server"
-    repository = "https://charts.helm.sh/stable"
-    chart = "metrics-server"
-    version = var.chart_version != null ? var.chart_version : null
-    namespace = var.namespace != null ? var.namespace : null
-    recreate_pods = true
+  name          = "metrics-server"
+  repository    = "https://charts.helm.sh/stable"
+  chart         = "metrics-server"
+  version       = var.chart_version != null ? var.chart_version : null
+  namespace     = var.namespace != null ? var.namespace : null
+  recreate_pods = true
 
-    values = [
-    "${file("${path.module}/values/values.yaml")}"
-    ]
+  values = [
+    file("${path.module}/values/values.yaml")
+  ]
 }
