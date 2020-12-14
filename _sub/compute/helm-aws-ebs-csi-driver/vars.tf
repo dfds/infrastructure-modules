1c0b227fa962cd7d8094a44165379f6c0553095e
abstrask <rask.misc@gmail.com>
2020-12-14 17:13:54 +0100

    Align vars descriptions

--- _sub/compute/helm-aws-ebs-csi-driver/vars.tf
+++ _sub/compute/helm-aws-ebs-csi-driver/vars.tf
@@ -1,20 +1,20 @@
 variable "kubeconfig_path" {
   type        = string
-  description = "The path to the kubeconfig file."
+  description = "The path to the kubeconfig file"
   default     = null
 }
 
 variable "chart_version" {
   type        = string
-  description = "aws-ebs-csi-driver helm chart version."
+  description = "The Helm chart version to deploy"
 }
 
 variable "cluster_name" {
   type        = string
-  description = "The cluster name."
+  description = "The cluster name"
 }
 
 variable "kiam_server_role_arn" {
   type        = string
-  description = "The role or entity to provide trust for when creating roles to use with annotations in kubernetes."
+  description = "The role or entity to provide trust for when creating roles to use with annotations in kubernetes"
 }
