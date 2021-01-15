0fd4d7b9d4c18129d091acbbb9bb6634f220314a
rifisdfds <40063756+rifisdfds@users.noreply.github.com>
2021-01-15 10:03:53 +0000

    Add atlantis (#154)
    * create atlantis module

---
+++ _sub/compute/helm-atlantis/main.tf
@@ -0,0 +1,75 @@
+## Atlantis ##
+
+locals {
+  full_github_repo_names = [
+    for repo in var.github_repositories:
+      "github.com/${repo}"
+  ]
+}
+  
+resource "helm_release" "atlantis" {
+  name          = "atlantis"
+  chart         = "atlantis"
+  repository    = "https://runatlantis.github.io/helm-charts"
+  version       = var.chart_version != null ? var.chart_version : null
+  namespace     = var.namespace
+  create_namespace = true
+  recreate_pods = true
+  force_update  = false
+
+  values = [
+    templatefile("${path.module}/values/values.yaml", {
+      atlantis_ingress = var.atlantis_ingress,
+      atlantis_image = var.atlantis_image,
+      atlantis_image_tag = var.atlantis_image_tag,
+      github_username = var.github_username,
+      github_token = var.github_token,
+      github_secret = var.webhook_secret,
+      github_repos = join(",", local.full_github_repo_names)
+      arm_tenant_id = var.arm_tenant_id,
+      arm_subscription_id = var.arm_subscription_id,
+      arm_client_id = var.arm_client_id,
+      arm_client_secret = var.arm_client_secret
+  })]
+}
+
+## Github ##
+
+data "github_user" "github_user" {
+    username = var.github_username
+}
+
+data "github_repository" "repo" {
+    count = length(var.github_repositories)
+    full_name = var.github_repositories[count.index]
+}
+
+resource "github_repository_webhook" "hook" {
+    count = length(data.github_repository.repo)
+    repository = data.github_repository.repo[count.index].name
+
+    configuration {
+        url = "https://${var.webhook_url}/events"
+        content_type = var.webhook_content_type
+        secret = var.webhook_secret
+        insecure_ssl = false
+    }
+
+    events = var.webhook_events
+}
+
+## Kubernetes ##
+
+resource "kubernetes_secret" "aws" {
+    metadata {
+        name = "aws-credentials"
+        namespace = var.namespace
+    }
+
+    data = {
+        aws_access_key = var.aws_access_key
+        aws_secret = var.aws_secret
+        access_key_master = var.access_key_master
+        secret_key_master = var.secret_key_master
+    }
+}
--- compute/k8s-services/vars.tf
+++ compute/k8s-services/vars.tf
@@ -372,3 +372,134 @@ variable "ebs_csi_driver_chart_version" {
   description = "The version of the AWS EBS CSI driver Helm chart to deploy (defaults to latest)"
   default     = null
 }
+
+# --------------------------------------------------
+# Atlantis
+# --------------------------------------------------
+
+variable "atlantis_deploy" {
+  type        = bool
+  description = "Deploy Atlantis"
+  default     = false
+}
+
+variable "github_token" {
+    description = "Github token that the provider uses to perform Github operations. Leaving unset will fall back to GITHUB_TOKEN environment variable"
+    default = null
+}
+
+variable "github_organization" {
+    description = "Github organization name. Conflicts with github_owner. Leaving unset will use GITHUB_ORGANIZATION environment variable if exists"
+    default = null
+}
+
+variable "github_owner" {
+    description = "Github owner(username). Conflicts with github_organization. Leaving unset will use GITHUB_OWNER environment variable if exists"
+    default = null
+}
+
+variable "github_username" {
+    description = "Github username of the account that will post Atlantis comments on PR's"
+    default = null
+}
+
+variable "github_repositories" {
+    description = "List of repositories to whitelist for Atlantis"
+    type = list(string)
+    default = []
+}
+
+variable "webhook_secret" {
+    description = "Secret used by the Webhook to speak to Atlantis"
+    default = null
+}
+
+variable "webhook_content_type" {
+    default = "application/json"
+}
+
+variable "webhook_insecure_ssl" {
+    default = false
+}
+
+variable "webhook_events" {
+    description = "A list of events that should trigger the webhook"
+    default = []
+    type = list(string)
+}
+
+variable "namespace" {
+  type = string
+  description = ""
+  default = "atlantis"
+}
+
+variable "chart_version" {
+  type = string
+  description = ""
+  default = null
+}
+
+variable "atlantis_ingress" {
+  type = string
+  description = ""
+  default = null
+}
+
+variable "atlantis_image" {
+  type = string
+  description = ""
+  default = "dfdsdk/atlantis-prime-pipeline"
+}
+
+variable "atlantis_image_tag" {
+  type = string
+  description = "Tag of the Atlantis image to use"
+  default = "latest"
+}
+
+variable "arm_tenant_id" {
+  type = string
+  description = ""
+  default = null
+}
+
+variable "arm_subscription_id" {
+  type = string
+  description = ""
+  default = null
+}
+
+variable "arm_client_id" {
+  type = string
+  description = ""
+  default = null
+}
+
+variable "arm_client_secret" {
+  type = string
+  description = ""
+  default = null
+}
+
+variable "aws_access_key" {
+    description = "AWS Access Key"
+    default = null
+}
+
+variable "aws_secret" {
+    description = "AWS Secret"
+    default = null
+}
+
+variable "access_key_master" {
+  type = string
+  description = "Access Key for Core account"
+  default = null
+}
+
+variable "secret_key_master" {
+  type = string
+  description = "Secret for Core account"
+  default = null
+}
