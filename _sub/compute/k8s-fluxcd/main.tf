831e9cd4e819e3fde23908680e8cdb1d3ced11cc
Peter West <pwes@dfds.com>
2021-01-18 13:06:27 +0000

    Changed resource for repository to data instead and updated references.

--- _sub/compute/k8s-fluxcd/main.tf
+++ _sub/compute/k8s-fluxcd/main.tf
@@ -89,42 +89,46 @@ resource "kubernetes_secret" "main" {
 }
 
 # GitHub
-resource "github_repository" "main" {
-  name        = var.repo_name
-  description = "Manifests for deploying essentials services on our Kubernetes clusters using Flux-CD"
-  visibility  = var.repo_visibility
-  auto_init   = true
-}
+# resource "github_repository" "main" {
+#   name        = var.repo_name
+#   description = "Manifests for deploying essentials services on our Kubernetes clusters using Flux-CD"
+#   visibility  = var.repo_visibility
+#   auto_init   = true
+# }
 
-resource "github_branch_default" "main" {
-  repository = github_repository.main.name
-  branch     = var.repo_branch
+# resource "github_branch_default" "main" {
+#   repository = github_repository.main.name
+#   branch     = var.repo_branch
+# }
+
+data "github_repository" "main" {
+  name = var.repo_name
 }
 
 resource "github_repository_deploy_key" "main" {
   title      = "staging-cluster"
-  repository = github_repository.main.name
+  repository = data.github_repository.main.name
   key        = tls_private_key.main.public_key_openssh
   read_only  = true
 }
 
 resource "github_repository_file" "install" {
-  repository = github_repository.main.name
+  repository = data.github_repository.main.name
   file       = data.flux_install.main.path
   content    = data.flux_install.main.content
-  branch     = var.repo_branch
+  branch     = data.github_repository.main.default_branch
 }
 
 resource "github_repository_file" "sync" {
-  repository = github_repository.main.name
+  repository = data.github_repository.main.name
   file       = data.flux_sync.main.path
   content    = data.flux_sync.main.content
-  branch     = var.repo_branch
+  branch     = data.github_repository.main.default_branch
 }
 
 resource "github_repository_file" "kustomize" {
-  repository = github_repository.main.name
+  repository = data.github_repository.main.name
   file       = data.flux_sync.main.kustomize_path
   content    = data.flux_sync.main.kustomize_content
-  branch     = var.repo_branch
+  branch     = data.github_repository.main.default_branch
 }
