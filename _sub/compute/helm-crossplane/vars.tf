dbbdf6555a2b28eb5a8cc766632af244ecf0b062
Fisher Richard <rifis@DFDS.COM>
2021-01-28 11:16:11 +0000

    move defaults from submodule to k8s-services

--- _sub/compute/helm-crossplane/vars.tf
+++ _sub/compute/helm-crossplane/vars.tf
@@ -1,35 +1,29 @@
 variable "namespace" {
   type = string
   description = "Namespace in which to install Crossplane"
-  default = "crossplane-system"
 }
 
 variable "chart_version" {
   type = string
   description = "Specify a version of the Helm chart"
-  default = null
 }
 
 variable "release_name" {
   type = string
   description = "Name of the chart release"
-  default = "crossplane"
 }
 
 variable "recreate_pods" {
     type = bool
     description = "Recreate pods on deployment"
-    default = true
 }
 
 variable "force_update" {
     type = bool
     description = "Force resource updates through replacement"
-    default = false
 }
 
 variable "crossplane_providers" {
   type = list(string)
   description = "List of Crossplane providers to install"
-  default = []
 }
--- compute/k8s-services/vars.tf
+++ compute/k8s-services/vars.tf
@@ -588,29 +588,35 @@ variable "crossplane_deploy" {
 variable "crossplane_namespace" {
   type = string
   description = "Namespace in which to install Crossplane"
+  default = "crossplane-system"
 }
 
 variable "crossplane_release_name" {
   type = string
   description = "Name of the chart release"
+  default = "crossplane"
 }
 
 variable "crossplane_chart_version" {
   type = string
   description = "Specify a version of the Helm chart"
+  default = null
 }
 
 variable "crossplane_recreate_pods" {
     type = bool
     description = "Recreate pods on deployment"
+    default = true
 }
 
 variable "crossplane_force_update" {
     type = bool
     description = "Force resource updates through replacement"
+    default = false
 }
 
 variable "crossplane_providers" {
   type = list(string)
   description = "List of Crossplane providers to install"
+  default = []
 }
