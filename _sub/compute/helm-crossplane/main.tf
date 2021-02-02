a8044d9095d8b5b4ee19b2f4d24c16d14d8d1123
abstrask <rask.misc@gmail.com>
2021-02-02 20:19:18 +0100

    Auto-format Crossplane module

--- _sub/compute/helm-crossplane/main.tf
+++ _sub/compute/helm-crossplane/main.tf
@@ -1,16 +1,16 @@
 locals {
-  packages_list = replace(yamlencode({provider:{packages:var.crossplane_providers}}), "\"", "")
+  packages_list = replace(yamlencode({ provider : { packages : var.crossplane_providers } }), "\"", "")
 }
 resource "helm_release" "crossplane" {
-    name = var.release_name
-    chart = "crossplane"
-    repository    = "https://charts.crossplane.io/stable"
-    version       = var.chart_version
-    namespace     = var.namespace
-    recreate_pods = var.recreate_pods
-    force_update  = var.force_update
+  name          = var.release_name
+  chart         = "crossplane"
+  repository    = "https://charts.crossplane.io/stable"
+  version       = var.chart_version
+  namespace     = var.namespace
+  recreate_pods = var.recreate_pods
+  force_update  = var.force_update
 
-    values = [
+  values = [
     templatefile("${path.module}/values/values.yaml", {
       crossplane_providers = local.packages_list
   })]
--- _sub/compute/helm-crossplane/vars.tf
+++ _sub/compute/helm-crossplane/vars.tf
@@ -1,29 +1,29 @@
 variable "namespace" {
-  type = string
+  type        = string
   description = "Namespace in which to install Crossplane"
 }
 
 variable "chart_version" {
-  type = string
+  type        = string
   description = "Specify a version of the Helm chart"
 }
 
 variable "release_name" {
-  type = string
+  type        = string
   description = "Name of the chart release"
 }
 
 variable "recreate_pods" {
-    type = bool
-    description = "Recreate pods on deployment"
+  type        = bool
+  description = "Recreate pods on deployment"
 }
 
 variable "force_update" {
-    type = bool
-    description = "Force resource updates through replacement"
+  type        = bool
+  description = "Force resource updates through replacement"
 }
 
 variable "crossplane_providers" {
-  type = list(string)
+  type        = list(string)
   description = "List of Crossplane providers to install"
 }
