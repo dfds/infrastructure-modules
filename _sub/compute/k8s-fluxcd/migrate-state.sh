1bbf604a39ef393164f36810cee7c0b0f2518eff
Rasmus Rask <raras@dfds.com>
2021-02-23 14:17:18 +0100

    Migrate command updated

--- _sub/compute/k8s-fluxcd/migrate-state.sh
+++ _sub/compute/k8s-fluxcd/migrate-state.sh
@@ -2,26 +2,43 @@ terragrunt state pull \
 | jq -r '.resources[] | select(.module == "module.platform_fluxcd[0]") | select(.type == "kubectl_manifest") | select(.name == "install") | .instances[] | "\(.index_key) \(.attributes.kind) \(.attributes.name)"' \
 | xargs -n3 sh -c 'terragrunt state mv module.platform_fluxcd[0].kubectl_manifest.install[\"$0\"] module.platform_fluxcd[0].kubectl_manifest.install[\"$1/$2\"]'
 
-"apps/v1/deployment/flux-system/notification-controller"
-"apiextensions.k8s.io/v1/customresourcedefinition/providers.notification.toolkit.fluxcd.io"
-"v1/namespace/flux-system"
-"apps/v1/deployment/flux-system/notification-controller"
-"apiextensions.k8s.io/v1/customresourcedefinition/providers.notification.toolkit.fluxcd.io"
-"v1/namespace/flux-system"
-"rbac.authorization.k8s.io/v1/clusterrolebinding/cluster-reconciler-flux-system"
-"rbac.authorization.k8s.io/v1/clusterrolebinding/cluster-reconciler-flux-system"
-"apiextensions.k8s.io/v1/customresourcedefinition/kustomizations.kustomize.toolkit.fluxcd.io"
-"apps/v1/deployment/flux-system/notification-controller"
-"v1/namespace/flux-system"
-"networking.k8s.io/v1/networkpolicy/flux-system/allow-webhooks"
-"apiextensions.k8s.io/v1/customresourcedefinition/helmreleases.helm.toolkit.fluxcd.io"
-"apiextensions.k8s.io/v1/customresourcedefinition/kustomizations.kustomize.toolkit.fluxcd.io"
-"apps/v1/deployment/flux-system/helm-controller"
 
-lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name])))
+cat state.json \
+| jq -r '.resources[] | select(.module == "module.platform_fluxcd[0]") | select(.type == "kubectl_manifest") | select(.name == "install") | .instances[] | "\(.index_key) \(.attributes.api_version) \(.attributes.kind) \(.attributes.namespace) \(.attributes.name)"' | sed -e 's/\(.*\)/\L\1/' \
+| xargs -n5 sh -c 'echo $0 $1/$2/$3/$4' | sed 's/null\///g' | xargs -n2 sh -c 'echo terragrunt state mv module.platform_fluxcd[0].kubectl_manifest.install[\"$0\"] module.platform_fluxcd[0].kubectl_manifest.install[\"$1\"]'
 
-terragrunt state pull \
-| jq -r '.resources[] | select(.module == "module.platform_fluxcd[0]") | select(.type == "kubectl_manifest") | select(.name == "install") | .instances[] | "\(.index_key) \(.attributes.kind) \(.attributes.name)"' \
-| xargs -n3 sh -c 'terragrunt state mv module.platform_fluxcd[0].kubectl_manifest.install[\"$0\"] module.platform_fluxcd[0].kubectl_manifest.install[\"$1/$2\"]'
 
-sed -e 's/\(.*\)/\L\1/'
+# Copy-paste from terminal:
+
+# apiextensions.k8s.io/v1/customresourcedefinition/helmreleases.helm.toolkit.fluxcd.io
+# apiextensions.k8s.io/v1/customresourcedefinition/kustomizations.kustomize.toolkit.fluxcd.io
+# apiextensions.k8s.io/v1/customresourcedefinition/providers.notification.toolkit.fluxcd.io
+# apps/v1/deployment/flux-system/helm-controller
+# apps/v1/deployment/flux-system/notification-controller
+# networking.k8s.io/v1/networkpolicy/flux-system/allow-webhooks
+# rbac.authorization.k8s.io/v1/clusterrolebinding/cluster-reconciler-flux-system
+# v1/namespace/flux-system
+
+
+# Output by migration command:
+
+# apiextensions.k8s.io/v1/customresourcedefinition/gitrepositories.source.toolkit.fluxcd.io
+# rbac.authorization.k8s.io/v1/clusterrolebinding/cluster-reconciler-flux-system
+# v1/service/flux-system/notification-controller
+# v1/serviceaccount/flux-system/kustomize-controller
+# v1/service/flux-system/source-controller
+# v1/serviceaccount/flux-system/helm-controller
+# v1/service/flux-system/webhook-receiver
+# apiextensions.k8s.io/v1/customresourcedefinition/helmrepositories.source.toolkit.fluxcd.io
+# rbac.authorization.k8s.io/v1/clusterrole/crd-controller-flux-system
+# networking.k8s.io/v1/networkpolicy/flux-system/deny-ingress
+# apiextensions.k8s.io/v1/customresourcedefinition/receivers.notification.toolkit.fluxcd.io
+# networking.k8s.io/v1/networkpolicy/flux-system/allow-webhooks
+# v1/namespace/flux-system
+# v1/serviceaccount/flux-system/source-controller
+# rbac.authorization.k8s.io/v1/clusterrolebinding/crd-controller-flux-system
+# v1/serviceaccount/flux-system/notification-controller
+# networking.k8s.io/v1/networkpolicy/flux-system/allow-scraping
+# apiextensions.k8s.io/v1/customresourcedefinition/buckets.source.toolkit.fluxcd.io
+# apiextensions.k8s.io/v1/customresourcedefinition/helmcharts.source.toolkit.fluxcd.io
+# apiextensions.k8s.io/v1/customresourcedefinition/providers.notification.toolkit.fluxcd.io
