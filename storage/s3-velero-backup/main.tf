324feb0b22e3ad1634885bed67c20f5ff39fa8a3
Willi Carlsen <carlsenwilli@gmail.com>
2021-03-31 13:34:09 +0200

    Velero (#194)
    * Velero bucket and IAM role plus repo_branch variable applied to k8s-services main module
    
    * velero WIP
    
    * Velero s3 storage module

---
+++ storage/s3-velero-backup/main.tf
@@ -0,0 +1,107 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  version = "~> 2.43"
+  
+  assume_role {
+    role_arn = var.aws_assume_role_arn
+  }
+}
+
+resource "aws_s3_bucket" "velero_storage" {
+  bucket = var.bucket_name
+  acl = "private"
+  force_destroy = var.force_bucket_destroy
+
+  versioning {
+    enabled = var.versioning
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "veloro_storage_block_public_access" {
+  bucket = aws_s3_bucket.velero_storage.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_iam_policy" "velero_policy" {
+  name        = "VeleroPolicy"
+  description = "Policy for Velero backups"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeVolumes",
+                "ec2:DescribeSnapshots",
+                "ec2:CreateTags",
+                "ec2:CreateVolume",
+                "ec2:CreateSnapshot",
+                "ec2:DeleteSnapshot"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:DeleteObject",
+                "s3:PutObject",
+                "s3:AbortMultipartUpload",
+                "s3:ListMultipartUploadParts"
+            ],
+            "Resource": [
+                "${aws_s3_bucket.velero_storage.arn}/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "${aws_s3_bucket.velero_storage.arn}"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "velero_role" {
+  name        = var.velero_iam_role_name
+  description = ""
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Condition": {
+        "StringLike": {
+          "aws:PrincipalArn": "${var.kiam_server_role_arn}"
+        }
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "velero_policy_attach" {
+  role       = aws_iam_role.velero_role.name
+  policy_arn = aws_iam_policy.velero_policy.arn
+}
---
+++ storage/s3-velero-backup/vars.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_assume_role_arn" {
+  type = string
+}
+
+variable bucket_name {
+  type        = string
+  description = ""
+}
+
+variable kiam_server_role_arn {
+  type        = string
+  default     = ""
+  description = ""
+}
+
+variable versioning {
+  type        = bool
+  default     = true
+  description = ""
+}
+
+variable "velero_iam_role_name" {
+  type = string
+  default = "VeleroBackup"
+  description = ""
+}
+
+variable force_bucket_destroy {
+  type        = bool
+  default     = true
+  description = "Destroy bucket without error"
+}
