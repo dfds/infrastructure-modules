6e2d940001ea643a200669bc7fcc0750f30b46f2
Audun Nes <aunes@dfds.com>
2021-04-27 13:29:07 +0200

    Allow kiam chart_version to be null

--- _sub/compute/k8s-kiam/vars.tf
+++ _sub/compute/k8s-kiam/vars.tf
@@ -91,7 +91,9 @@ variable "chart_version" {
   default     = null
 
   validation {
-    condition     = tonumber(element(split(".", var.chart_version), 0)) >= 6
+    condition     = var.chart_version == null ? true : (
+      tonumber(element(split(".", var.chart_version), 0)) >= 6
+    )
     error_message = "The KIAM Helm chart_version must be greater than 6.0.0."
   }
 }
