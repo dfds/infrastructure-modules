36f8bdc119e4f99e465188af8b18f8c7921321d0
Rasmus Rask <raras@dfds.com>
2021-05-03 12:22:21 +0200

    QA: Additional cleanup, simplified script/pipeline (#212)

--- src/qa-test-eks.sh
+++ src/qa-test-eks.sh
@@ -3,28 +3,53 @@ set -eu -o pipefail
 
 BASEPATH=./test/integration
 ACTION=$1
+# $AWS_DEFAULT_REGION
+
+extra_cleanup () {
+    CLUSTERNAME=$1
+
+    # Remove specific resources that sometimes get left behind (always return true, as resource may have been successfully been cleaned up)
+    
+    aws iam list-roles --output json | jq -r --arg ROLEPREFIX "eks-${CLUSTERNAME}-" '.Roles[] | select( .RoleName | contains($ROLEPREFIX) ) | .RoleName' | xargs aws iam delete-role --role-name || true
+    aws --region eu-west-1 ec2 delete-network-interface --network-interface-id "$(aws --region eu-west-1 ec2 describe-network-interfaces --filters "Name=group-name,Values=eks-${CLUSTERNAME}-node" --query "NetworkInterfaces[].NetworkInterfaceId" --output text)" || true
+}
 
 
 if [ "$ACTION" = "init" ]; then
-    az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+    az login --service-principal --username "$ARM_CLIENT_ID" --password "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID"
 fi
 
 
-if [ "$ACTION" = "plan" ]; then
-    SUBPATH=$2
-    WORKDIR="${BASEPATH}/${SUBPATH}"
+if [ "$ACTION" = "plan-cluster" ]; then
+    REGION=$2
+    CLUSTERNAME=$3
+    WORKDIR="${BASEPATH}/${REGION}/k8s-${CLUSTERNAME}/cluster"
     # Show the plan of what will be applied
     # Can't run plan all, because later stages depend on data from Terraform state (which is empty)
     # terragrunt plan-all --terragrunt-working-dir ./test/integration --terragrunt-source-update --terragrunt-non-interactive -input=false
-    terragrunt plan --terragrunt-working-dir $WORKDIR --terragrunt-source-update --terragrunt-non-interactive -input=false
+    terragrunt plan --terragrunt-working-dir "$WORKDIR" --terragrunt-source-update --terragrunt-non-interactive -input=false
 fi
 
 
-if [ "$ACTION" = "apply-all" ]; then
+if [ "$ACTION" = "apply-shared" ]; then
     SUBPATH=$2
     WORKDIR="${BASEPATH}/${SUBPATH}"
+        
     # Apply the configuration
-    terragrunt apply-all --terragrunt-working-dir $WORKDIR --terragrunt-source-update --terragrunt-non-interactive -input=false -auto-approve
+    terragrunt apply-all --terragrunt-working-dir "$WORKDIR" --terragrunt-source-update --terragrunt-non-interactive -input=false -auto-approve
+fi
+
+
+if [ "$ACTION" = "apply-cluster" ]; then
+    REGION=$2
+    CLUSTERNAME=$3
+    WORKDIR="${BASEPATH}/${REGION}/k8s-${CLUSTERNAME}"
+    
+    # Remove specific resources that sometimes get left behind (always return true, as resource may have been successfully been cleaned up)
+    extra_cleanup "$CLUSTERNAME"
+    
+    # Apply the configuration
+    terragrunt apply-all --terragrunt-working-dir "$WORKDIR" --terragrunt-source-update --terragrunt-non-interactive -input=false -auto-approve
 fi
 
 
@@ -33,9 +58,10 @@ if [ "$ACTION" = "test" ]; then
     # Get kubeconfig path
     # --------------------------------------------------
 
-    SUBPATH=$2
-    WORKDIR="${BASEPATH}/${SUBPATH}/cluster"
-    export KUBECONFIG=$(terragrunt output kubeconfig_path --terragrunt-working-dir $WORKDIR)
+    REGION=$2
+    CLUSTERNAME=$3
+    WORKDIR="${BASEPATH}/${REGION}/k8s-${CLUSTERNAME}/cluster"
+    export KUBECONFIG=$(terragrunt output kubeconfig_path --terragrunt-working-dir "$WORKDIR")
 
     # Sleep before test?
 
@@ -86,26 +112,22 @@ if [ "$ACTION" = "test" ]; then
 fi
 
 
-if [ "$ACTION" = "disable-cluster-logging" ]; then
-    REGION=$2
-    CLUSTERNAME=$3
-    aws --region $REGION eks update-cluster-config --name $CLUSTERNAME --logging '{"clusterLogging":[{"types":["api","audit","authenticator","controllerManager","scheduler"],"enabled":false}]}' || true
-    sleep 60
-fi
-
-
 if [ "$ACTION" = "destroy-cluster" ]; then
     RETURN=0
-    SUBPATH=$2
+    REGION=$2
     CLUSTERNAME=$3
-    WORKDIR="${BASEPATH}/${SUBPATH}"
-    
+    WORKDIR="${BASEPATH}/${REGION}/k8s-${CLUSTERNAME}"
+
+    # Disable cluster logging
+    echo Disabling cluster logging
+    aws --region "$REGION" eks update-cluster-config --name "$CLUSTERNAME" --logging '{"clusterLogging":[{"types":["api","audit","authenticator","controllerManager","scheduler"],"enabled":false}]}' || true
+    sleep 60
+
     # Essential cleanup commands (set RETURN=1 if fails)
-    terragrunt destroy-all --terragrunt-working-dir $WORKDIR --terragrunt-source-update --terragrunt-non-interactive -input=false -auto-approve || RETURN=1
+    terragrunt destroy-all --terragrunt-working-dir "$WORKDIR" --terragrunt-source-update --terragrunt-non-interactive -input=false -auto-approve || RETURN=1
     
     # Remove specific resources that sometimes get left behind (always return true, as resource may have been successfully been cleaned up)
-    aws iam list-roles --output json | jq -r --arg ROLEPREFIX "eks-${CLUSTERNAME}-" '.Roles[] | select( .RoleName | contains($ROLEPREFIX) ) | .RoleName' | xargs aws iam delete-role --role-name || true
-    aws --region eu-west-1 ec2 delete-network-interface --network-interface-id "$(aws --region eu-west-1 ec2 describe-network-interfaces --filters "Name=group-name,Values=eks-${CLUSTERNAME}-node" --query "NetworkInterfaces[].NetworkInterfaceId" --output text)" || true
+    extra_cleanup "$CLUSTERNAME"
 
     # Return false, if any *eseential* commands failed
     if [ $RETURN -ne 0 ]; then
@@ -119,5 +141,5 @@ if [ "$ACTION" = "destroy-shared" ]; then
     WORKDIR="${BASEPATH}/${SUBPATH}"
     
     # Cleanup
-    terragrunt destroy-all --terragrunt-working-dir $WORKDIR --terragrunt-source-update --terragrunt-non-interactive -input=false -auto-approve || true
+    terragrunt destroy-all --terragrunt-working-dir "$WORKDIR" --terragrunt-source-update --terragrunt-non-interactive -input=false -auto-approve || true
 fi
