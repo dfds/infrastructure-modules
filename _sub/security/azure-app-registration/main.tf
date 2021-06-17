175ab3de50519514bf5f6f613852dae8d5c06e2c
Peter West <pwes@dfds.com>
2021-06-17 15:04:14 +0100

    Changed homepage attribute due to deprecation.

--- _sub/security/azure-app-registration/main.tf
+++ _sub/security/azure-app-registration/main.tf
@@ -1,6 +1,6 @@
 resource "azuread_application" "app" {
   display_name    = var.name
-  homepage        = var.homepage
+  homepage_url    = var.homepage
   identifier_uris = var.identifier_uris
   reply_urls      = var.reply_urls
 }
