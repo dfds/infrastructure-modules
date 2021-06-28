110654b5b183ab30df1fc246c3a7eefc33369c9c
Audun Nes <aunes@dfds.com>
2021-06-28 13:47:25 +0200

    Add fallback ingressroute

--- _sub/compute/k8s-traefik-flux/main.tf
+++ _sub/compute/k8s-traefik-flux/main.tf
@@ -11,3 +11,10 @@ resource "github_repository_file" "traefik_patch" {
   file       = "${local.base_repo_path}/patch.yaml"
   content    = jsonencode(local.patch)
 }
+
+resource "github_repository_file" "traefik_fallback" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.base_repo_path}/fallback.yaml"
+  content    = jsonencode(local.patch)
+}
