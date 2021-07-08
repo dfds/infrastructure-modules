92ceb2f5f94ba73e79e6e7695c0801d4c614905b
Rasmus Rask <raras@dfds.com>
2021-07-08 13:28:57 +0200

    Set Grafana password output as sensitive

--- _sub/compute/helm-kube-prometheus-stack/outputs.tf
+++ _sub/compute/helm-kube-prometheus-stack/outputs.tf
@@ -1,3 +1,4 @@
 output "grafana_admin_password" {
-  value = var.grafana_admin_password != "" ? "You already know it..." : random_password.grafana_password.result
+  value     = var.grafana_admin_password != "" ? "<as supplied in terragrunt.hcl>" : random_password.grafana_password.result
+  sensitive = true
 }
