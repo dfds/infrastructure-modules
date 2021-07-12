adcda3679a40dc6ec2e44875edb4d32f5d457836
Rasmus Rask <raras@dfds.com>
2021-07-12 13:15:21 +0200

    Fix for TF linter being so helpful, it removed an curly end-brace

--- database/postgres-restore/versions.tf
+++ database/postgres-restore/versions.tf
@@ -6,4 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 2.0.0"
     }
+  }
 }
--- network/route53-sub-zone/versions.tf
+++ network/route53-sub-zone/versions.tf
@@ -6,4 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 2.43.0"
     }
+  }
 }
