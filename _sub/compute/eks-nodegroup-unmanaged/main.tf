a3731cf7dfd7667f31bc0ac493b5bb8c8c04a0fa
Peter West <pwes@dfds.com>
2021-08-18 12:26:00 +0100

    Added metadata options to enforce IMDSv2.

--- _sub/compute/eks-nodegroup-unmanaged/main.tf
+++ _sub/compute/eks-nodegroup-unmanaged/main.tf
@@ -9,6 +9,11 @@ resource "aws_launch_configuration" "eks" {
   user_data_base64            = var.cloudwatch_agent_enabled ? base64encode(local.worker-node-userdata-cw-agent) : base64encode(local.worker-node-userdata)
   key_name                    = var.ec2_ssh_key
 
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
   root_block_device {
     volume_size = var.disk_size
     volume_type = "gp2"
