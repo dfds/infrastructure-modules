00a6dca67af0359057cdc450e769d207389fe365
Audun Nes <aunes@dfds.com>
2021-09-10 10:26:32 +0200

    Ingress is allowed to received traffic from any source

--- _sub/compute/eks-alb-auth/main.tf
+++ _sub/compute/eks-alb-auth/main.tf
@@ -93,14 +93,14 @@ resource "aws_security_group" "traefik_auth" {
     from_port   = 80
     to_port     = 80
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sg
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sg
   }
 
   ingress {
