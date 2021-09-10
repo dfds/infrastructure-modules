2b17a1679c7577df78358f5da5f9b1284f14f1c7
Audun Nes <aunes@dfds.com>
2021-09-10 13:28:30 +0200

    Allow default encryption. aws-cloudwatch-log-group-customer-key

--- _sub/compute/eks-cluster/eks.tf
+++ _sub/compute/eks-cluster/eks.tf
@@ -1,3 +1,4 @@
+#tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "eks" {
   name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = var.log_retention_days
