abe93d81e541d6692aeb8eaccbbc8c3e9cfa737c
Audun V. Nes <aunes@dfds.com>
2021-09-15 09:02:27 +0200

    Update kubernetes provider without change in functionality (#312)

--- compute/eks-ec2/main.tf
+++ compute/eks-ec2/main.tf
@@ -18,7 +18,6 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks_cluster.eks_endpoint
   cluster_ca_certificate = base64decode(module.eks_cluster.eks_certificate_authority)
-  load_config_file       = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
