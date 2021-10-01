32e81a01f9dc09ce93830848e5c595b17e7d8875
Rasmus Rask <raras@dfds.com>
2021-10-01 14:07:36 +0200

    Bump required Terraform version to 1.0 for all modules

--- _sub/compute/k8s-traefik/versions.tf
+++ _sub/compute/k8s-traefik/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 1.0"
 
-    required_providers {
-      htpasswd = {
-        source  = "loafoe/htpasswd"
-        version = "~> 0.9.0"
-      }
+  required_providers {
+    htpasswd = {
+      source  = "loafoe/htpasswd"
+      version = "~> 0.9.0"
+    }
   }
 }
