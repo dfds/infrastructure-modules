8e3a879de7fe83fda60d3b85749fce0b9f3fef9e
Peter West <pwes@dfds.com>
2021-10-13 12:59:47 +0100

    Variable to allow SSM parameter to be tagged.

--- security/org-account-context/main.tf
+++ security/org-account-context/main.tf
@@ -164,6 +164,7 @@ module "iam_user_deploy_store_credentials" {
   source          = "../../_sub/security/ssm-parameter-store"
   key_name        = "/managed/deploy/aws-creds"
   key_description = "AWS credentials for the IAM 'Deploy' user"
+  tag_createdby   = var.ssm_param_createdby
 
   key_value = <<EOF
 AWS_ACCESS_KEY_ID=${module.iam_user_deploy.access_key}
