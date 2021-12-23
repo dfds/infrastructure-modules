17b0a87cad7fd34d11fcb6fc8d3a4f4f775c3836
Peter West <pwes@dfds.com>
2021-12-23 11:28:03 +0000

    Defined var for OpenID Connect URL.

--- _sub/compute/helm-ebs-csi-driver/vars.tf
+++ _sub/compute/helm-ebs-csi-driver/vars.tf
@@ -1,3 +1,9 @@
+variable "eks_openid_connect_provider_url" {
+  type        = string
+  description = "The OpenID Connect provider URL for the EKS cluster"
+  default     = null
+}
+
 variable "kubeconfig_path" {
   type        = string
   description = "The path to the kubeconfig file"
