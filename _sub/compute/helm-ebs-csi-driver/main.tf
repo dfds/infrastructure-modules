12ae6748116c48ef97174627f37091633456ae61
Peter West <39488932+wiredworm@users.noreply.github.com>
2022-02-17 14:21:15 +0000

    OIDC provider isolation (#424)
    * Removed OIDC provider from CSI Module
    
    * Variable changes.
    
    * Created new module for OIDC Provider.
    
    * Added module reference for OIDC Provider.
    
    * Ensure service account name is honoured by Helm.
    
    * Removed non-required variable in k8s-services

--- _sub/compute/helm-ebs-csi-driver/main.tf
+++ _sub/compute/helm-ebs-csi-driver/main.tf
@@ -159,21 +159,6 @@ EOF
 
 }
 
-# required TLS Certificate which is then used for the openid connect provider thumprint list
-data "tls_certificate" "eks" {
-  url = "${var.eks_openid_connect_provider_url}"
-}
-
-# define openid connect provider that is bound to the provider URL for the EKS cluster
-resource "aws_iam_openid_connect_provider" "default" {
-  url = var.eks_openid_connect_provider_url
-
-  client_id_list = [
-    "sts.amazonaws.com",
-  ]
-
-  thumbprint_list = [data.tls_certificate.eks.certificates.0.sha1_fingerprint]
-}
 
 # define IAM role for the CSI Driver to utilise, including a trust relationship for the KAIM Server role
 resource "aws_iam_role" "csi_driver_role" {
@@ -192,7 +177,7 @@ resource "aws_iam_role" "csi_driver_role" {
       "Action": "sts:AssumeRoleWithWebIdentity",
       "Condition": {
         "StringEquals": {
-          "${trim(var.eks_openid_connect_provider_url,"https://")}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${trim(var.eks_openid_connect_provider_url,"https://")}:sub": "system:serviceaccount:kube-system:${var.csi_ebs_serviceaccount_name}"
         }
       }
     }
@@ -218,6 +203,10 @@ resource "helm_release" "aws-ebs-csi-driver" {
   namespace     = "kube-system"
   recreate_pods = true
   force_update  = false
+  values = [
+    templatefile("${path.module}/values/values.yaml", {
+      csi_ebs_serviceaccount_name = var.csi_ebs_serviceaccount_name
+  })]
 
   set {
     name = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
