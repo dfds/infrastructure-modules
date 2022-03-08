acf56b04e83e834e0a0f4814a5c68f099d900dbb
Audun Nes <aunes@dfds.com>
2022-03-08 14:34:11 +0100

    Preliminary stash of crossplane-operator Helm deploy stuff
    Signed-off-by: Audun Nes <aunes@dfds.com>

---
+++ _sub/compute/k8s-crossplane-operator-flux/main.tf
@@ -0,0 +1,26 @@
+# This module depends on you using Flux CD 2, and have added https://github.com/dfds/platform-apps in your
+# flux-system as instructed in https://github.com/dfds/platform-apps/blob/main/README.md
+
+# --------------------------------------------------
+# Create JSON files to be picked up by Flux CD
+# --------------------------------------------------
+resource "github_repository_file" "crossplane_operator_helm" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.cluster_repo_path}/${local.app_install_name}-helm.yaml"
+  content    = jsonencode(local.app_helm_path)
+}
+
+resource "github_repository_file" "crossplane_operator_helm_install" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.helm_repo_path}/kustomization.yaml"
+  content    = jsonencode(local.helm_install)
+}
+
+resource "github_repository_file" "crossplane_operator_helm_patch" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.helm_repo_path}/patch.yaml"
+  content    = jsonencode(local.helm_patch)
+}
