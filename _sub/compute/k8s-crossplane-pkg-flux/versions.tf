e0f365e43d68eb50e360552027843da1e96c8cf4
Audun Nes <aunes@dfds.com>
2022-03-08 14:35:47 +0100

    Remove empty lines

--- _sub/compute/k8s-crossplane-pkg-flux/versions.tf
+++ _sub/compute/k8s-crossplane-pkg-flux/versions.tf
@@ -6,13 +6,10 @@ terraform {
   Need to explicitly specific 3rd party Providers
   Version can still be controlled via main module
   */
-
   required_providers {
-
     github = {
       source = "integrations/github"
     }
-
   }
 
 }
