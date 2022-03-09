621ef7f751d9341aa84aad7e340d724b728304bb
Audun V. Nes <aunes@dfds.com>
2022-03-09 12:49:45 +0100

    Add sub module to generate Flux manifests to deploy Crossplane config… (#445)
    * Add sub module to generate Flux manifests to deploy Crossplane configuration package
    
    Signed-off-by: Audun Nes <aunes@dfds.com>
    
    * Remove empty lines
    
    * Refactor names
    
    Signed-off-by: Audun Nes <aunes@dfds.com>
    
    * Rename internal names
    
    Signed-off-by: Audun Nes <aunes@dfds.com>
    
    * Rename module
    
    Signed-off-by: Audun Nes <aunes@dfds.com>
    
    * Simplify naming and remove one variable
    
    Signed-off-by: Audun Nes <aunes@dfds.com>

--- compute/k8s-services/main.tf
+++ compute/k8s-services/main.tf
@@ -510,6 +510,22 @@ module "crossplane" {
   eks_openid_connect_provider_url   = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
 }
 
+module "crossplane_configuration_package" {
+  source       = "../../_sub/compute/k8s-crossplane-cfg-pkg"
+  count        = var.crossplane_cfg_pkg_deploy ? 1 : 0
+  name         = var.crossplane_cfg_pkg_name
+  package      = var.crossplane_cfg_pkg_docker_image
+  repo_owner   = var.crossplane_cfg_pkg_repo_owner != null ? var.crossplane_cfg_pkg_repo_owner : var.platform_fluxcd_github_owner
+  repo_name    = var.crossplane_cfg_pkg_repo_name != null ? var.crossplane_cfg_pkg_repo_name : var.platform_fluxcd_repo_name
+  repo_branch  = var.crossplane_cfg_pkg_repo_branch != null ? var.crossplane_cfg_pkg_repo_branch : var.platform_fluxcd_repo_branch
+  cluster_name = var.eks_cluster_name
+  providers = {
+    github = github.fluxcd
+  }
+
+  depends_on = [module.crossplane]
+}
+
 # --------------------------------------------------
 # Blackbox Exporter
 # --------------------------------------------------
