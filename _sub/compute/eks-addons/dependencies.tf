0c679b2b20d11f1b7ed5389860a3ffce61892d03
rifisdfds <40063756+rifisdfds@users.noreply.github.com>
2022-03-17 09:22:01 +0000

    update vpccni and coredns version maps for 1.21 (#450)

--- _sub/compute/eks-addons/dependencies.tf
+++ _sub/compute/eks-addons/dependencies.tf
@@ -11,13 +11,13 @@ locals {
   vpccni_version_map = {
     "1.19" = "v1.7.5-eksbuild.2"
     "1.20" = "v1.9.0-eksbuild.1"
-    "1.21" = "v1.9.0-eksbuild.1"
+    "1.21" = "v1.10.2-eksbuild.1"
   }
 
   coredns_version_map = {
     "1.19" = "v1.8.0-eksbuild.1"
     "1.20" = "v1.8.3-eksbuild.1"
-    "1.21" = "v1.8.3-eksbuild.1"
+    "1.21" = "v1.8.4-eksbuild.1"
   }
 
   kubeproxy_version_map = {
