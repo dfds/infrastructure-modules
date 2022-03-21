f02095e36441b7aeceb8a81f333efd2c780338da
Audun V. Nes <aunes@dfds.com>
2022-03-21 10:09:17 +0100

    Add encryption block to the bucket definition (#444)
    * Add encryption block to the bucket definition
    
    Signed-off-by: Audun Nes <aunes@dfds.com>
    
    * Add some ignores
    
    Signed-off-by: Audun Nes <aunes@dfds.com>

--- _sub/storage/s3-bucket-lifecycle/main.tf
+++ _sub/storage/s3-bucket-lifecycle/main.tf
@@ -1,13 +1,20 @@
-# tfsec:ignore:aws-s3-enable-versioning
+# tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-specify-public-access-block tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.name
-  acl    = var.acl
+  bucket        = var.name
+  acl           = var.acl
+  force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 
   tags = {
     "Managed by" = "Terraform"
   }
-
-  force_destroy = true
 
   lifecycle_rule {
     enabled                                = true
--- _sub/storage/s3-bucket/main.tf
+++ _sub/storage/s3-bucket/main.tf
@@ -1,12 +1,19 @@
-# tfsec:ignore:aws-s3-enable-versioning
+# tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-specify-public-access-block tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "bucket" {
-  count  = var.deploy ? 1 : 0
-  bucket = var.s3_bucket
-  acl    = var.acl
+  count         = var.deploy ? 1 : 0
+  bucket        = var.s3_bucket
+  acl           = var.acl
+  force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 
   tags = {
     "Managed by" = "Terraform"
   }
-
-  force_destroy = true
 }
