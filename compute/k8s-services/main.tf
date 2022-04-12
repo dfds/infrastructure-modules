8f3a4919b0125cfb042ec5842a03bc2c0f4ab0c4
Willi Carlsen <carlsenwilli@gmail.com>
2022-04-12 13:43:16 +0200

    Testing without count

--- compute/k8s-services/main.tf
+++ compute/k8s-services/main.tf
@@ -548,7 +548,7 @@ module "crossplane_configuration_package" {
 
 module "crossplane_provider_confluent_prereqs" {
   source = "../../_sub/compute/k8s-crossplane-provider-confluent"
-  count = contains(var.crossplane_providers, "dfdsdk/provider-confluent") ? 1 : 0
+  # count = contains(var.crossplane_providers, "dfdsdk/provider-confluent") ? 1 : 0
   namespace = var.crossplane_namespace
   email = var.crossplane_provider_confluent_email
   password = var.crossplane_provider_confluent_password
