46be0f705ce7566b1ab30b9d1fbada7ebd94dbd3
Willi Carlsen <carlsenwilli@gmail.com>
2022-04-12 14:17:18 +0200

    Added required providers

--- _sub/compute/k8s-crossplane-provider-confluent/versions.tf
+++ _sub/compute/k8s-crossplane-provider-confluent/versions.tf
@@ -1,3 +1,13 @@
 terraform {
   required_version = "~> 1.0"
+    required_providers {
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+    }
+
+    kubectl = {
+      source = "gavinbunney/kubectl"
+    }
+  }
 }
