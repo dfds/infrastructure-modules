53d2cd47b999758d9ab39e6b99b717774f82d236
Audun Nes <aunes@dfds.com>
2022-05-11 15:50:58 +0200

    Use the new S3 Terraform resources for Velero too
    Signed-off-by: Audun Nes <aunes@dfds.com>

--- storage/s3-velero-backup/main.tf
+++ storage/s3-velero-backup/main.tf
@@ -27,19 +27,10 @@ module "aws_iam_oidc_provider" {
 
 resource "aws_s3_bucket" "velero_storage" {
   bucket        = var.bucket_name
-  acl           = "private"
   force_destroy = var.force_bucket_destroy
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
-
-  versioning {
-    enabled = var.versioning
+  tags = {
+    "Managed by" = "Terraform"
   }
 }
 
@@ -124,4 +115,26 @@ resource "aws_iam_role" "velero_role" {
 resource "aws_iam_role_policy_attachment" "velero_policy_attach" {
   role       = aws_iam_role.velero_role.name
   policy_arn = aws_iam_policy.velero_policy.arn
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  bucket = aws_s3_bucket.velero_storage.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.velero_storage.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.velero_storage.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
