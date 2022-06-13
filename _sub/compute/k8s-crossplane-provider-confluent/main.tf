0e86c2a7f3cc2ff833ecc491a27dd69c9c65b5b1
samidbb <40062553+samidbb@users.noreply.github.com>
2022-06-13 10:44:33 +0200

    Configmaps with confluent cloud configurations in hellman (#501)

--- _sub/compute/k8s-crossplane-provider-confluent/main.tf
+++ _sub/compute/k8s-crossplane-provider-confluent/main.tf
@@ -28,3 +28,41 @@ spec:
       key: secret
 YAML
 }
+
+
+resource "github_repository_file" "crossplane_provider_confluent_path" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.cluster_repo_path}/${local.app_install_name}-config.yaml"
+  content    = jsonencode(local.app_config_path)
+}
+
+resource "github_repository_file" "crossplane_provider_confluent_init" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.config_repo_path}/kustomization.yaml"
+  content    = jsonencode(local.config_init)
+}
+
+resource "github_repository_file" "crossplane_provider_confluent_configmap_environments" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.config_repo_path}/configmap_environments.yaml"
+  content    = jsonencode(local.config_map_confluent_environments)
+}
+
+resource "github_repository_file" "crossplane_provider_confluent_configmap_clusters" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.config_repo_path}/configmap_clusters.yaml"
+  content    = jsonencode(local.config_map_confluent_clusters)
+}
+
+
+# resource "github_repository_file" "crossplane_provider_confluent_configmaps" {
+#   repository = var.repo_name
+#   branch     = local.repo_branch
+#   file       = "${local.config_repo_path}/configmaps.yaml"
+#   content    = local.yaml_body_config_map
+# }
+
