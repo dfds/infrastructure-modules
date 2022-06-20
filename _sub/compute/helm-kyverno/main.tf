b33e62dae2033a481e9b825730698592f2daf001
Willi Carlsen <carlsenwilli@gmail.com>
2022-06-20 19:33:10 +0200

    Increased too optimistic default resource limits for memory on Kyverno helm chart

--- _sub/compute/helm-kyverno/main.tf
+++ _sub/compute/helm-kyverno/main.tf
@@ -12,6 +12,11 @@ resource "helm_release" "kyverno" {
   namespace     = kubernetes_namespace.namespace.metadata[0].name
   recreate_pods = true
   force_update  = false
+
+  set {
+    name  = "resources.limits.memory"
+    value = "2048Mi"
+  }
 }
 
 resource "kubernetes_config_map" "configmap" {
@@ -41,7 +46,7 @@ metadata:
       any source. A NetworkPolicy cannot be used to control traffic to host ports.
       Although NodePort Services can be useful, their use must be limited to Services
       with additional upstream security checks. This policy validates that any new Services
-      do not use the `NodePort` type.      
+      do not use the `NodePort` type.
 spec:
   validationFailureAction: enforce
   background: true
@@ -87,7 +92,7 @@ metadata:
       any source. A NetworkPolicy cannot be used to control traffic to host ports.
       Although LoadBalancer Services can be useful, their use must be limited to Services
       with additional upstream security checks. This policy validates that any new Services
-      do not use the `LoadBalancer` type.      
+      do not use the `LoadBalancer` type.
 spec:
   validationFailureAction: enforce
   background: true
