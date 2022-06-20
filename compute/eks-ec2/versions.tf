9caa9c700f0d6a3a99d438c751a9d2708a8d5543
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2022-06-20 17:26:38 +0200

    Update Terraform aws to ~> 4.19.0 (#491)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- compute/eks-ec2/versions.tf
+++ compute/eks-ec2/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.15.0"
+      version = "~> 4.19.0"
     }
 
     kubernetes = {
--- security/org-account/main.tf
+++ security/org-account/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region  = var.aws_region
-  version = "~> 4.15.0"
+  version = "~> 4.19.0"
 }
 
 provider "aws" {
-  version = "~> 4.15.0"
+  version = "~> 4.19.0"
   region  = var.aws_region
 
   # Assume the Organizational role in Workload account
