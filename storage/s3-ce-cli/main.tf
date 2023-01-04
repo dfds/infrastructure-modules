4fbdea59b7c31e0fedafb6c98a1bbd597571a362
Aleksandra Korczak <61739075+aleksandra-korczak@users.noreply.github.com>
2023-01-04 13:55:07 +0100

    Remove template provider (#571)

--- storage/s3-ce-cli/main.tf
+++ storage/s3-ce-cli/main.tf
@@ -20,12 +20,12 @@ module "iam_inventory_role_policy" {
   source  = "../../_sub/storage/s3-bucket-object"
   bucket  = module.bucket.bucket_name
   key     = "aws/iam/inventory-role/policy.json"
-  content = data.template_file.iam_inventory_role_policy.rendered
+  content = data.aws_iam_policy_document.iam_inventory_role_policy.json
 }
 
 module "iam_inventory_role_trust" {
   source  = "../../_sub/storage/s3-bucket-object"
   bucket  = module.bucket.bucket_name
   key     = "aws/iam/inventory-role/trust.json"
-  content = data.template_file.iam_inventory_role_trust.rendered
+  content = data.aws_iam_policy_document.iam_inventory_role_trust.json
 }
