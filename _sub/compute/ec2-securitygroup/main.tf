2738485f7bdb7142cb96666bdbd55398fe11d3e7
Audun Nes <aunes@dfds.com>
2023-02-03 13:33:18 +0100

    Update AWS provider and handle tfsec issues

--- _sub/compute/ec2-securitygroup/main.tf
+++ _sub/compute/ec2-securitygroup/main.tf
@@ -1,4 +1,4 @@
-# tfsec:ignore:aws-vpc-add-description-to-security-group
+# tfsec:ignore:aws-vpc-add-description-to-security-group tfsec:ignore:aws-ec2-add-description-to-security-group-rule tfsec:ignore:aws-vpc-no-public-egress-sg tfsec:ignore:aws-ec2-no-public-egress-sgr
 resource "aws_security_group" "sg" {
   name        = var.name
   description = var.description
@@ -8,7 +8,7 @@ resource "aws_security_group" "sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sg
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
