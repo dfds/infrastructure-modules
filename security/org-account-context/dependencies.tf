2b1a49e731b4c4fb250a5944c9a23f4b2095b20d
Emil H. Clausen <sequoiia@hummel.yt>
2023-02-15 10:45:23 +0100

    Added new trust relationship to iam_role_shared

--- security/org-account-context/dependencies.tf
+++ security/org-account-context/dependencies.tf
@@ -43,3 +43,41 @@ data "aws_iam_policy_document" "assume_role_adfs_shared" {
   }
 
 }
+
+// Gives access to role through ADFS as well as the individual Capability account
+data "aws_iam_policy_document" "shared_role_adfs_cap_acc" {
+  statement {
+    actions = ["sts:AssumeRoleWithSAML"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [
+        "arn:aws:iam::${var.shared_account_id}:saml-provider/ADFS"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "SAML:aud"
+      values   = ["https://signin.aws.amazon.com/saml"]
+    }
+  }
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${module.org_account.id}:role/Capability"]
+    }
+  }
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${module.org_account.id}:role/AWSReservedSSO_CapabilityAccess_*"]
+    }
+  }
+}
