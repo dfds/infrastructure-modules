bc481ef9c333946a0c853066c44b54c056c8aa78
Emil Davtyan <emidavt@dfds.com>
2023-02-17 16:36:27 +0100

    test max pods limits

---
+++ test/integration/suite/node_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestNodeMaxPod(t *testing.T) {
+	t.Parallel()
+	clientset := NewK8sClientSet(t)
+
+	resp, err := clientset.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		t.Log(err.Error())
+		return
+	}
+
+	for _, node := range resp.Items {
+		assert.EqualValues(t, node.Status.Capacity.Pods().Value(),
+			// Asssuming a m5a.xlarge instance and prefix delegation enable.
+			898, "node %q pods limit does not match", node.Name)
+	}
+}
