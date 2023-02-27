d652cc7b43aaee5ae966b10a9b590b43027675ba
Audun Nes <aunes@dfds.com>
2023-02-27 12:56:52 +0100

    Fix tflint issues

--- security/iam-roles-master/vars.tf
+++ security/iam-roles-master/vars.tf
@@ -21,36 +21,3 @@ variable "create_org_account_iam_policy_name" {
   description = "Policy granting access to create new AWS Organization accounts"
   type        = string
 }
-
-# --------------------------------------------------
-# Unused variables - to provent TF warning/error:
-# Using a variables file to set an undeclared variable is deprecated and will
-# become an error in a future release. If you wish to provide certain "global"
-# settings to all configurations in your organization, use TF_VAR_...
-# environment variables to set these instead.
-# --------------------------------------------------
-
-variable "aws_account_name" {
-  type    = string
-  default = ""
-}
-
-variable "terraform_state_region" {
-  type    = string
-  default = ""
-}
-
-variable "cloudtrail_central_s3_bucket" {
-  type    = string
-  default = ""
-}
-
-variable "terraform_state_s3_bucket" {
-  type    = string
-  default = ""
-}
-
-variable "terraform_state_aws_region" {
-  type    = string
-  default = ""
-}
