09b220fe9336d33346286a3a9945e1025335f1e6
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-03-08 08:52:49 +0100

    Update Terraform aws to ~> 4.57.0 (#729)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/monitoring/fluentd-cloudwatch/versions.tf
+++ _sub/monitoring/fluentd-cloudwatch/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.56.0"
+      version = "~> 4.57.0"
     }
     github = {
       source  = "integrations/github"
