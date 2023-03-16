d37e0329f18d14e03639286e06298922aefcbcdd
Emil Davtyan <emil2k@gmail.com>
2023-03-16 13:28:06 +0100

    Enable general managed node group in QA. (#751)

--- test/integration/suite/vpc_cni_driver_test.go
+++ test/integration/suite/vpc_cni_driver_test.go
@@ -5,5 +5,5 @@ import "testing"
 func TestVpcCniDriverDaemonSet(t *testing.T) {
 	t.Parallel()
 	clientset := NewK8sClientSet(t)
-	AssertK8sDaemonSet(t, clientset, "kube-system", "aws-node", 4)
+	AssertK8sDaemonSet(t, clientset, "kube-system", "aws-node", 7)
 }
