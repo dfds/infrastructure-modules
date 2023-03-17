983da572f9112b335296fae77ff3726cee7f16de
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-03-17 12:49:39 +0100

    Update Terraform flux to ~> 0.25.0 (#740)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/k8s-fluxcd/versions.tf
+++ _sub/compute/k8s-fluxcd/versions.tf
@@ -19,7 +19,7 @@ terraform {
     }
     flux = {
       source  = "fluxcd/flux"
-      version = "~> 0.24.0"
+      version = "~> 0.25.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
