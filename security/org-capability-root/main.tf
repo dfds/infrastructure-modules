d42c5cee073bfa7c7ba54a49c3dc9d890d83fc99
Audun Nes <aunes@dfds.com>
2023-03-29 15:53:32 +0200

    Add support for the new service control policies without destroying the old ones yet.

--- security/org-capability-root/main.tf
+++ security/org-capability-root/main.tf
@@ -22,7 +22,7 @@ module "capability_ou" {
 }
 
 # --------------------------------------------------
-# Service Control (Organization) Policies
+# Service Control (Organization) Policies - legacy
 # --------------------------------------------------
 
 module "org_policy_denynoneuregions" {
@@ -57,3 +57,30 @@ module "org_policy_denyvpncreation" {
   attach_target_id = module.capability_ou.id
 }
 
+# --------------------------------------------------
+# Service Control (Organization) Policies - new
+# --------------------------------------------------
+
+module "org_policy_preventive_policy" {
+  source         = "../../_sub/security/org-service-control-policy"
+  name           = "PreventivePolicy"
+  description    = "Enables best practices for keeping accounts secure and limits when a breach occurs"
+  policy         = local.preventive_policy
+  attach_targets = var.ou_ids_for_preventive_policy
+}
+
+module "org_policy_integrity_policy" {
+  source         = "../../_sub/security/org-service-control-policy"
+  name           = "IntegrityPolicy"
+  description    = "Enables us to have better confidence in the information, logs and auditing provided by AWS"
+  policy         = local.integrity_policy
+  attach_targets = var.ou_ids_for_integrity_policy
+}
+
+module "org_policy_restrictive_policy" {
+  source         = "../../_sub/security/org-service-control-policy"
+  name           = "RestrictivePolicy"
+  description    = "Used to stop bad practices in the teams, either when there is an alternative or it has been judged to pose a risk"
+  policy         = local.restrictive_policy
+  attach_targets = var.ou_ids_for_restrictive_policy
+}
--- security/org-capability-root/vars.tf
+++ security/org-capability-root/vars.tf
@@ -5,3 +5,15 @@ variable "aws_region" {
 variable "capability_ou_name" {
   type = string
 }
+
+variable "ou_ids_for_preventive_policy" {
+  type = list(string)
+}
+
+variable "ou_ids_for_integrity_policy" {
+  type = list(string)
+}
+
+variable "ou_ids_for_restrictive_policy" {
+  type = list(string)
+}
