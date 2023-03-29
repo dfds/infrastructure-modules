8b32b4470a30881aaaa20edd02a5320697027b76
Audun Nes <aunes@dfds.com>
2023-03-29 17:09:49 +0200

    Refactor to become more separate from capability OU

--- security/org-capability-root/vars.tf
+++ security/org-capability-root/vars.tf
@@ -5,15 +5,3 @@ variable "aws_region" {
 variable "capability_ou_name" {
   type = string
 }
-
-variable "ou_ids_for_preventive_policy" {
-  type = list(string)
-}
-
-variable "ou_ids_for_integrity_policy" {
-  type = list(string)
-}
-
-variable "ou_ids_for_restrictive_policy" {
-  type = list(string)
-}
