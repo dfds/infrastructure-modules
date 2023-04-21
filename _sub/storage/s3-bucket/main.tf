066c26c27d6439880eee0c192d770a3309cec34b
Emil Davtyan <emil2k@gmail.com>
2023-04-21 15:44:56 +0200

    Handle new defaults for S3 ownership controls. (#824)
    Applies to new S3 buckets, announced in April 2023:
    https://aws.amazon.com/about-aws/whats-new/2022/12/amazon-s3-automatically-enable-block-public-access-disable-access-control-lists-buckets-april-2023/

--- _sub/storage/s3-bucket/main.tf
+++ _sub/storage/s3-bucket/main.tf
@@ -12,6 +12,36 @@ resource "aws_s3_bucket" "bucket" {
   )
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  count  = var.deploy && var.acl == "public-read" ? 1 : 0
+  bucket = aws_s3_bucket.bucket[count.index].id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  count  = var.deploy ? 1 : 0
+  bucket = aws_s3_bucket.bucket[count.index].id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# tfsec:ignore:aws-s3-no-public-access-with-acl
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  count  = var.deploy ? 1 : 0
+  bucket = aws_s3_bucket.bucket[count.index].id
+  acl    = var.acl
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.bucket,
+    aws_s3_bucket_ownership_controls.bucket,
+  ]
+}
+
 # tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
   count  = var.deploy && var.enable_server_side_encryption ? 1 : 0
@@ -22,11 +52,4 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption
       sse_algorithm = "aws:kms"
     }
   }
-}
-
-# tfsec:ignore:aws-s3-no-public-access-with-acl
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  count  = var.deploy ? 1 : 0
-  bucket = aws_s3_bucket.bucket[count.index].id
-  acl    = var.acl
 }
