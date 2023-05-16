72b515788e245599219b196befcba0f1add6a87a
Emil Davtyan <emidavt@dfds.com>
2023-05-16 16:35:55 +0200

    Add SupportAccess permission set.
    This is permission set will be used on hardened accounts to allow users
    to assume a limited role for handling support tickets.

--- _sub/security/iam-identity-center/main.tf
+++ _sub/security/iam-identity-center/main.tf
@@ -29,3 +29,16 @@ resource "aws_ssoadmin_permission_set_inline_policy" "capabilityaccess" {
   instance_arn       = aws_ssoadmin_permission_set.capabilityaccess.instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.capabilityaccess.arn
 }
+
+resource "aws_ssoadmin_permission_set" "supportaccess" {
+  name             = "SupportAccess"
+  description      = "The permission set for handling support cases within capabilities"
+  instance_arn     = tolist(data.aws_ssoadmin_instances.dfds.arns)[0]
+  session_duration = "PT8H"
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "supportaccess" {
+  instance_arn       = aws_ssoadmin_permission_set.supportaccess.instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.supportaccess.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AWSSupportAccess"
+}
