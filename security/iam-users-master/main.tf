c2e7dc4f47d9f4e822f2c2fcd55386c613cb18e2
Audun Nes <aunes@dfds.com>
2023-05-16 14:42:13 +0200

    Create a standalone policy before attaching it to a user

--- security/iam-users-master/main.tf
+++ security/iam-users-master/main.tf
@@ -29,13 +29,17 @@ resource "aws_iam_user_policy" "assume_noncore_accounts" {
 # Certero
 # ------------------------------------------------------------------------------
 
-# tfsec:ignore:aws-iam-no-user-attached-policies
+resource "aws_iam_policy" "certero_endpoint" {
+  name   = "CerteroEndpoint"
+  path   = "/"
+  policy = module.iam_policies.certero_endpoint_for_master_account
+}
+
 resource "aws_iam_user" "certero_endpoint_user" {
   name = "CerteroEndpointUser"
 }
 
-resource "aws_iam_user_policy" "certero_endpoint" {
-  name   = "CerteroEndpoint"
-  user   = aws_iam_user.certero_endpoint_user.name
-  policy = module.iam_policies.certero_endpoint_for_master_account
+resource "aws_iam_user_policy_attachment" "certero_endpoint" {
+  user       = aws_iam_user.certero_endpoint_user.name
+  policy_arn = aws_iam_policy.certero_endpoint.arn
 }
