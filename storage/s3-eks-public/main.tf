f74e4f56facd47d2af048627e45c5a80febe559b
Audun V. Nes <aunes@dfds.com>
2023-05-17 12:53:38 +0200

    Add support for S3 bucket for kubeconfig being private (#893)

--- storage/s3-eks-public/main.tf
+++ storage/s3-eks-public/main.tf
@@ -2,6 +2,12 @@
 # Init
 # --------------------------------------------------
 
+# Despite the main module name, it should only have public-read for production.
+# It is only for history reasons and an active integration with a 3rd party system
+# that "public-read" is still the default value for the ACL.
+# It is best practice to have a private ACL on S3 buckets.
+# Hence it should be set to private for most cluster deployments.
+
 terraform {
   backend "s3" {
   }
@@ -19,7 +25,7 @@ module "s3_bucket" {
   source                        = "../../_sub/storage/s3-bucket"
   deploy                        = length(var.eks_public_s3_bucket) >= 1 ? true : false
   s3_bucket                     = var.eks_public_s3_bucket
-  acl                           = "public-read"
+  acl                           = var.eks_is_sandbox ? "private" : var.acl
   enable_server_side_encryption = var.enable_server_side_encryption
   additional_tags               = var.additional_tags
 }
