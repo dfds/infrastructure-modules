65a9a1d57c75eb07a59836ee9426b8bcec8d732d
Emil Davtyan <emidavt@dfds.com>
2023-05-22 15:17:40 +0200

    Fix noisy drift notification.
    The AWS provider is unable to detect that this default configuration is
    not drift.

--- _sub/storage/s3-cloudtrail-bucket/main.tf
+++ _sub/storage/s3-cloudtrail-bucket/main.tf
@@ -40,6 +40,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
 
   rule {
     bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
