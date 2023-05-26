2012834033e4cd6fac6d9c3f7150c8e23d95442e
Audun Nes <aunes@dfds.com>
2023-05-26 07:47:36 +0200

    Releasing before_hook in services for new purposes (now that Flux is using new bootstrapping)

--- test/integration/hooks/custom_hook_01.sh
+++ test/integration/hooks/custom_hook_01.sh
@@ -19,33 +19,4 @@ echo "KUBECONFIG=$KUBECONFIG"
 
 cd "$PARENT_DIR/eu-west-1/k8s-qa/services" || return
 
-IS_FLUXCD_MIGRATED=$(terragrunt state list | grep -c module.platform_fluxcd.flux_bootstrap_git.this)
-
-if [[ $IS_FLUXCD_MIGRATED -eq 1 ]]; then
-	echo "Terraform state is already migrated for Flux..."
-else
-	echo "Migrating Terraform state for Flux CD bootstrap resources..."
-
-	terragrunt init -upgrade
-
-	terragrunt state rm module.platform_fluxcd[0].null_resource.flux_namespace
-	terragrunt state rm module.platform_fluxcd[0].kubectl_manifest.install
-	terragrunt state rm module.platform_fluxcd[0].kubectl_manifest.sync
-	terragrunt state rm module.platform_fluxcd[0].kubernetes_secret.main
-	terragrunt state rm module.platform_fluxcd[0].data.flux_install.main
-	terragrunt state rm module.platform_fluxcd[0].data.flux_sync.main
-	terragrunt state rm module.platform_fluxcd[0].data.kubectl_file_documents.install
-	terragrunt state rm module.platform_fluxcd[0].data.kubectl_file_documents.sync
-	terragrunt state rm module.platform_fluxcd[0].github_repository_file.install
-	terragrunt state rm module.platform_fluxcd[0].github_repository_file.kustomize
-	terragrunt state rm module.platform_fluxcd[0].github_repository_file.sync
-
-	terragrunt state mv module.platform_fluxcd[0].data.github_branch.flux_branch module.platform_fluxcd.data.github_branch.flux_branch
-	terragrunt state mv module.platform_fluxcd[0].data.github_repository.main module.platform_fluxcd.data.github_repository.main
-	terragrunt state mv module.platform_fluxcd[0].github_repository_deploy_key.main module.platform_fluxcd.github_repository_deploy_key.main
-	terragrunt state mv module.platform_fluxcd[0].github_repository_file.flux_monitoring_config_path module.platform_fluxcd.github_repository_file.flux_monitoring_config_path
-	terragrunt state mv module.platform_fluxcd[0].github_repository_file.platform_apps_init module.platform_fluxcd.github_repository_file.platform_apps_init
-	terragrunt state mv module.platform_fluxcd[0].tls_private_key.main module.platform_fluxcd.tls_private_key.main
-
-	terragrunt import module.platform_fluxcd.flux_bootstrap_git.this flux-system
-fi
+echo "This custom hook can be used for new purposes"
