c41c2c4b5974406582f064cd2cc12bcf849bd37c
samidbb <samdi@dfds.com>
2023-06-12 13:25:32 +0200

    fix aws providers auth. remove eu-west-1 resource explorer

--- security/org-account/main.tf
+++ security/org-account/main.tf
@@ -32,9 +32,6 @@ provider "aws" {
   region = "eu-west-1"
   alias  = "workload_eu-west-1"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
@@ -45,10 +42,6 @@ provider "aws" {
   region = "eu-west-2"
   alias  = "workload_eu-west-2"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
-
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
@@ -58,9 +51,6 @@ provider "aws" {
   region = "eu-west-3"
   alias  = "workload_eu-west-3"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
@@ -72,9 +62,6 @@ provider "aws" {
   region = "eu-north-1"
   alias  = "workload_eu-north-1"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
@@ -87,85 +74,66 @@ provider "aws" {
   region = "us-east-1"
   alias  = "workload_us-east-1"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
-
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 provider "aws" {
   region = "us-east-2"
   alias  = "workload_us-east-2"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
-
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 provider "aws" {
   region = "us-west-1"
   alias  = "workload_us-west-1"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
-
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 provider "aws" {
   region = "us-west-2"
   alias  = "workload_us-west-2"
 
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
-
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 # Asia Pacific
 provider "aws" {
   region = "ap-south-1"
   alias  = "workload_ap-south-1"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 provider "aws" {
   region = "ap-northeast-3"
   alias  = "workload_ap-northeast-3"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 provider "aws" {
   region = "ap-northeast-2"
   alias  = "workload_ap-northeast-2"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
@@ -176,21 +144,16 @@ provider "aws" {
 provider "aws" {
   region = "ap-southeast-1"
   alias  = "workload_ap-southeast-1"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 provider "aws" {
   region = "ap-southeast-2"
   alias  = "workload_ap-southeast-2"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
@@ -201,9 +164,6 @@ provider "aws" {
 provider "aws" {
   region = "ap-northeast-1"
   alias  = "workload_ap-northeast-1"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
@@ -215,22 +175,17 @@ provider "aws" {
 provider "aws" {
   region = "ca-central-1"
   alias  = "workload_ca-central-1"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
     role_arn = module.org_account.org_role_arn
   }
 }
+
 # South America
 provider "aws" {
   region = "sa-east-1"
   alias  = "workload_sa-east-1"
-  # Need explicit credentials in Master, to be able to assume Organizational Role in Workload account
-  access_key = var.access_key_master
-  secret_key = var.secret_key_master
 
   # Assume the Organizational role in Workload account
   assume_role {
@@ -383,11 +338,11 @@ resource "aws_resourceexplorer2_index" "ca-central-1" {
   provider = aws.workload_ca-central-1
 }
 
-resource "aws_resourceexplorer2_index" "eu-west-1" {
-  type = "LOCAL"
+# resource "aws_resourceexplorer2_index" "eu_west_1" {
+#   type = "LOCAL"
 
-  provider = aws.workload_eu-west-1
-}
+#   provider = aws.workload_2 # aws.workload_eu-west-1
+# }
 resource "aws_resourceexplorer2_index" "eu-west-2" {
   type = "LOCAL"
 
