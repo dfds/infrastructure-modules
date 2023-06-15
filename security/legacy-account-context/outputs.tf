fc2bd2f5dc0bb50e25577715476e595a36c9ca2d
Audun Nes <aunes@dfds.com>
2023-06-15 10:02:19 +0200

    Add module for handling legacy accounts

---
+++ security/legacy-account-context/outputs.tf
@@ -0,0 +1,20 @@
+output "email" {
+  value = module.org_account.email
+}
+
+output "id" {
+  value = module.org_account.id
+}
+
+output "name" {
+  value = module.org_account.name
+}
+
+output "org_role_name" {
+  value = module.org_account.org_role_name
+}
+
+output "org_role_arn" {
+  value = module.org_account.org_role_arn
+}
+
