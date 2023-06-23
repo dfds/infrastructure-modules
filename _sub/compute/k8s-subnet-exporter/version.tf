10acc357c96bf9a9364820c5089ef1719a02b809
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-06-23 09:32:29 +0200

    chore(deps): update terraform aws to ~> 5.5.0 (#1002)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/k8s-subnet-exporter/version.tf
+++ _sub/compute/k8s-subnet-exporter/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.4.0"
+      version = "~> 5.5.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
--- _sub/monitoring/datadog-integration-aws/versions.tf
+++ _sub/monitoring/datadog-integration-aws/versions.tf
@@ -9,7 +9,7 @@ terraform {
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.4.0"
+      version = "~> 5.5.0"
     }
   }
 }
