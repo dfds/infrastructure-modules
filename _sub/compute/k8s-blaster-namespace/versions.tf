ab54115ad9afa9c8a1d01b596ff86d172155cab9
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-06-30 09:49:04 +0200

    chore(deps): update terraform aws to ~> 5.6.0 (#1023)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/k8s-blaster-namespace/versions.tf
+++ _sub/compute/k8s-blaster-namespace/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.5.0"
+      version = "~> 5.6.0"
     }
     github = {
       source  = "integrations/github"
