95891371261893fdea0aa69128ed32ebd5ad6c4a
Aleksandra Korczak <61739075+aleksandra-korczak@users.noreply.github.com>
2023-07-03 10:37:56 +0200

    Add missing thumbprint (#1028)

--- _sub/security/iam-github-oidc-provider/main.tf
+++ _sub/security/iam-github-oidc-provider/main.tf
@@ -1,6 +1,9 @@
 resource "aws_iam_openid_connect_provider" "github" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
   url             = "https://token.actions.githubusercontent.com"
 }
 
