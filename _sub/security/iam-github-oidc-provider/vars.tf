549eac9aecf61af0021221f1d1d43dd1601d6d10
Aleksandra Korczak <alekorc@dfds.com>
2023-07-03 14:50:02 +0200

    Fix oidc-role trust policy

--- _sub/security/iam-github-oidc-provider/vars.tf
+++ _sub/security/iam-github-oidc-provider/vars.tf
@@ -1,9 +1,13 @@
 variable "repositories" {
   type = list(object({
     repository_name = string
-    branches        = list(string)
+    refs            = list(string)
   }))
-  description = "List of repositories to authenticate to AWS from. Each object contains repository name and list of branches that should be allowed to deploy from"
+  description = "List of repositories to authenticate to AWS from. Each object contains repository name and list of git refs that should be allowed to deploy from"
+  validation {
+    condition = alltrue([ for v in flatten(values({ for repo in var.repositories : repo.repository_name => repo.ref })) : startswith(v, "refs/heads/") || startswith(v, "refs/tags/") ])
+    error_message = "The ref needs to start with `refs/heads/` for branches and `refs/tags/` for tags."
+  }
 }
 
 variable "oidc_role_access" {
