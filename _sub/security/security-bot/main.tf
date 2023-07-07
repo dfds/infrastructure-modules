cb40aaa4db619429d1d233a8638fd11ecd059d19
Emil Davtyan <emidavt@dfds.com>
2023-07-07 13:40:41 +0200

    Deploy Security Bot from S3 with versioning. (#1040)

--- _sub/security/security-bot/main.tf
+++ _sub/security/security-bot/main.tf
@@ -294,17 +294,16 @@ resource "aws_iam_role_policy_attachment" "exec" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Source can be found at https://github.com/dfds/security-bot
 resource "aws_lambda_function" "bot" {
   count         = var.deploy ? 1 : 0
-  filename      = "${path.module}/lambda/security-bot.zip"
+  s3_bucket     = var.lambda_s3_bucket
+  s3_key        = "security-bot/security-bot-${var.lambda_version}.zip"
   function_name = aws_iam_role.lambda[0].name
   role          = aws_iam_role.lambda[0].arn
   handler       = "bootstrap"
   runtime       = "go1.x"
   timeout       = 10
-
-  # Source can be found at https://github.com/dfds/security-bot
-  source_code_hash = filebase64sha256("${path.module}/lambda/security-bot.zip")
 
   environment {
     variables = {
