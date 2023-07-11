e275eb2ddf6512836a8127e2036ec56ba615c054
Emil Davtyan <emidavt@dfds.com>
2023-07-11 09:54:21 +0200

    Ability to harden -based accounts.

--- security/legacy-account-context/vars.tf
+++ security/legacy-account-context/vars.tf
@@ -2,6 +2,11 @@ variable "aws_region" {
   type = string
 }
 
+variable "aws_region_2" {
+  type        = string
+  description = "Terraform has limitations that prevent us from dynamically creating AWS providers for each region, so instead of providing a list of regions we will specifiy an incremented set of variables to deploy resources across multiple regions."
+}
+
 variable "aws_session_name" {
   type        = string
   description = "An identifier for the AWS session name. This can be useful in identifiying which pipeline executed the Terraform."
@@ -50,4 +55,65 @@ variable "parent_id" {
   type        = string
   description = "The ID of the parent AWS Organization OU."
   default     = ""
+}
+
+variable "aws_region_sso" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "sso_support_permission_set_name" {
+  type    = string
+  default = null
+}
+
+variable "sso_support_group_name" {
+  type    = string
+  default = null
+}
+
+variable "harden" {
+  type    = bool
+  default = false
+}
+
+variable "security_bot_lambda_version" {
+  type = string
+}
+
+variable "security_bot_lambda_s3_bucket" {
+  type        = string
+  description = "The S3 bucket where the Security Bot Lambda package is stored."
+}
+
+variable "hardened_monitoring_email" {
+  type    = string
+  default = null
+}
+
+variable "hardened_monitoring_slack_channel" {
+  type    = string
+  default = null
+}
+
+variable "hardened_monitoring_slack_token" {
+  type      = string
+  sensitive = true
+  default   = null
+}
+
+variable "hardened_security_contact_name" {
+  type = string
+}
+
+variable "hardened_security_contact_title" {
+  type = string
+}
+
+variable "hardened_security_contact_email" {
+  type = string
+}
+
+variable "hardened_security_contact_phone_number" {
+  type = string
 }
