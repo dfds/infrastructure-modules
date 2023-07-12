4523127e03474f75d22bb49e9f62475b7bd24133
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-07-12 10:28:16 +0200

    chore(deps): update terraform github to ~> 5.30.0 (#1047)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/helm-kube-prometheus-stack/version.tf
+++ _sub/compute/helm-kube-prometheus-stack/version.tf
@@ -8,7 +8,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.29.0"
+      version = "~> 5.30.0"
     }
     helm = {
       source  = "hashicorp/helm"
