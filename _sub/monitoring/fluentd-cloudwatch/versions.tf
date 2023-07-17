558c1567289ae3b286f4d67a077578d856c9c0a4
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-07-17 10:17:23 +0200

    chore(deps): update terraform aws to ~> 5.8.0 (#1052)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/monitoring/fluentd-cloudwatch/versions.tf
+++ _sub/monitoring/fluentd-cloudwatch/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.7.0"
+      version = "~> 5.8.0"
     }
     github = {
       source  = "integrations/github"
--- storage/s3-velero-backup/versions.tf
+++ storage/s3-velero-backup/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.7.0"
+      version = "~> 5.8.0"
     }
     tls = {
       source  = "hashicorp/tls"
