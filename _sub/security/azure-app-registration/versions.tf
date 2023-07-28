c2e1344a2e09b3e0881bcf19fcea8fcb0a4f708b
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-07-28 11:46:30 +0200

    chore(deps): update terraform azuread to ~> 2.41.0 (#1067)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/security/azure-app-registration/versions.tf
+++ _sub/security/azure-app-registration/versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.40.0"
+      version = "~> 2.41.0"
     }
     random = {
       source  = "hashicorp/random"
