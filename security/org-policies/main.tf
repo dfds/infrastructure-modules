cf3c13b28c678e6dbd08d9f76743a5d3b40071a4
Silviu Calin <silviuandrei.calin@gmail.com>
2023-08-21 13:03:28 +0200

    Refactoring policies to terraform objects.

--- security/org-policies/main.tf
+++ security/org-policies/main.tf
@@ -6,7 +6,7 @@ module "org_policy_preventive_policy" {
   source         = "../../_sub/security/org-service-control-policy"
   name           = "PreventivePolicy"
   description    = "Enables best practices for keeping accounts secure and limits when a breach occurs"
-  policy         = jsonencode(jsondecode(file("${path.module}/policies/PreventivePolicy.json")))
+  policy         = "/policies/PreventivePolicy.tf"
   attach_targets = var.preventive_policy_attach_targets
 }
 
@@ -14,7 +14,7 @@ module "org_policy_integrity_policy" {
   source         = "../../_sub/security/org-service-control-policy"
   name           = "IntegrityPolicy"
   description    = "Enables us to have better confidence in the information, logs and auditing provided by AWS"
-  policy         = jsonencode(jsondecode(file("${path.module}/policies/IntegrityPolicy.json")))
+  policy         = "/policies/IntegrityPolicy.tf"
   attach_targets = var.integrity_policy_attach_targets
 }
 
@@ -22,14 +22,14 @@ module "org_policy_restrictive_policy" {
   source         = "../../_sub/security/org-service-control-policy"
   name           = "RestrictivePolicy"
   description    = "Used to stop bad practices in the teams, either when there is an alternative or it has been judged to pose a risk"
-  policy         = jsonencode(jsondecode(file("${path.module}/policies/RestrictivePolicy.json")))
-  attach_targets = var.restrictive_policy_attach_targets
+  policy         = "/policies/RestrictivePolicy.tf"
+  attach_targets = [var.restrictive_policy_attach_targets, var.resource_owner_tag_value]
 }
 
 module "org_policy_reservation_policy" {
   source         = "../../_sub/security/org-service-control-policy"
   name           = "ReservationPolicy"
   description    = "Enables us to limit teams from committing us to long term reservations"
-  policy         = jsonencode(jsondecode(file("${path.module}/policies/ReservationPolicy.json")))
+  policy         = "/policies/ReservationPolicy.tf"
   attach_targets = var.reservation_policy_attach_targets
 }
