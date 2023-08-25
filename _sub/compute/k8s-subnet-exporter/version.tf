3fe68c8d6bf36fda68b7f2b7d45bf01ad5c9c756
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-08-25 09:31:12 +0200

    chore(deps): update terraform aws to ~> 5.14.0 (#1104)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/k8s-subnet-exporter/version.tf
+++ _sub/compute/k8s-subnet-exporter/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.13.0"
+      version = "~> 5.14.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
