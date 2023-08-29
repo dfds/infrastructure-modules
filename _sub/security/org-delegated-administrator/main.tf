b1f9ca55d285cc9fe4f9d9e12961251a52b6f7d0
Aleksandra Korczak <alekorc@dfds.com>
2023-08-29 16:09:17 +0200

    Fix for_each

--- _sub/security/org-delegated-administrator/main.tf
+++ _sub/security/org-delegated-administrator/main.tf
@@ -1,5 +1,5 @@
 resource "aws_organizations_delegated_administrator" "this" {
-  for_each = { for x in var.delegated_administrators : format(x.account_id, x.service_principal) => x }
+  for_each = { for x in var.delegated_administrators : "${x.account_id}-${x.service_principal}" => x }
 
   account_id        = each.value.account_id
   service_principal = each.value.service_principal
