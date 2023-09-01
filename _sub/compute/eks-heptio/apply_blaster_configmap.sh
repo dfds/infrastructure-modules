b0c7a2d982aae6f7a7ddb3c8b236e131002b35ca
Audun Nes <aunes@dfds.com>
2023-09-01 14:32:37 +0200

    Add brackets around variables. Use index 0, because that is the only index being expanded anyway

--- _sub/compute/eks-heptio/apply_blaster_configmap.sh
+++ _sub/compute/eks-heptio/apply_blaster_configmap.sh
@@ -7,7 +7,7 @@ set -ex
 # [ "$DEBUG" == 'true' ] && set -x
 
 # Ensure at least two arguments were passed
-if [ -z "$2" ]; then
+if [[ -z $2 ]]; then
 	echo "Need at least two arguments"
 	exit
 fi
@@ -29,10 +29,10 @@ function SplitAssumedCreds() {
 }
 
 # Generate AWS CLI config files, if
-if [ -n "$4" ]; then
+if [[ -n $4 ]]; then
 	AWS_ASSUME_ARN=$6
 	AWS_ASSUMED_CREDS=($(aws --region "$REGION" sts assume-role \
-		--role-arn "$AWS_ASSUME_ARN" \
+		--role-arn "${AWS_ASSUME_ARN}" \
 		--role-session-name "ApplyBlasterConfigmap" \
 		--query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
 		--output text))
@@ -40,39 +40,35 @@ if [ -n "$4" ]; then
 fi
 
 # Determine if configmap file exists in S3
-if [ -n "$AWS_ASSUMED_CREDS" ]; then
+if [[ -n ${AWS_ASSUMED_CREDS[0]} ]]; then
 	AWS_ACCESS_KEY_ID=${AWS_ASSUMED_ACCESS_KEY_ID} \
 		AWS_SECRET_ACCESS_KEY=${AWS_ASSUMED_SECRET_ACCESS_KEY} \
 		AWS_SESSION_TOKEN=${AWS_ASSUMED_SESSION_TOKEN} \
-		aws --region "$REGION" s3 ls "$CONFIGMAP_PATH_S3" >/dev/null && APPLY_S3_CONFIGMAP=1
+		aws --region "${REGION}" s3 ls "${CONFIGMAP_PATH_S3}" >/dev/null && APPLY_S3_CONFIGMAP=1
 else
-	aws --region "$REGION" s3 ls "$CONFIGMAP_PATH_S3" >/dev/null && APPLY_S3_CONFIGMAP=1
+	aws --region "${REGION}" s3 ls "${CONFIGMAP_PATH_S3}" >/dev/null && APPLY_S3_CONFIGMAP=1
 fi
 
-# Output current configmap
-# echo "Current configmap:"
-# kubectl --kubeconfig $KUBE_CONFIG_PATH -n kube-system get configmap aws-auth -o yaml
-
 # Apply configmap
-if [ $APPLY_S3_CONFIGMAP -eq 1 ]; then
+if [[ ${APPLY_S3_CONFIGMAP} -eq 1 ]]; then
 
 	echo "Applying configmap from ${CONFIGMAP_PATH_S3}:"
 
-	if [ -n "$AWS_ASSUMED_CREDS" ]; then
+	if [[ -n ${AWS_ASSUMED_CREDS[0]} ]]; then
 		AWS_ACCESS_KEY_ID=${AWS_ASSUMED_ACCESS_KEY_ID} \
 			AWS_SECRET_ACCESS_KEY=${AWS_ASSUMED_SECRET_ACCESS_KEY} \
 			AWS_SESSION_TOKEN=${AWS_ASSUMED_SESSION_TOKEN} \
-			aws --region "$REGION" s3 cp "$CONFIGMAP_PATH_S3" "/tmp/${CONFIGMAP_KEY}"
+			aws --region "${REGION}" s3 cp "${CONFIGMAP_PATH_S3}" "/tmp/${CONFIGMAP_KEY}"
 	else
-		aws --region "$REGION" s3 cp "$CONFIGMAP_PATH_S3" "/tmp/${CONFIGMAP_KEY}"
+		aws --region "${REGION}" s3 cp "${CONFIGMAP_PATH_S3}" "/tmp/${CONFIGMAP_KEY}"
 	fi
 
 	# cat /tmp/${CONFIGMAP_KEY}
-	kubectl --kubeconfig "$KUBE_CONFIG_PATH" apply -f "/tmp/${CONFIGMAP_KEY}"
+	kubectl --kubeconfig "${KUBE_CONFIG_PATH}" apply -f "/tmp/${CONFIGMAP_KEY}"
 
 else
 
-	echo "No configmap found at \"$CONFIGMAP_PATH_S3\" or permission denied. Applying default configmap."
-	kubectl --kubeconfig "$KUBE_CONFIG_PATH" apply -f "$DEFAULT_CONFIGMAP_PATH"
+	echo "No configmap found at \"${CONFIGMAP_PATH_S3}\" or permission denied. Applying default configmap."
+	kubectl --kubeconfig "${KUBE_CONFIG_PATH}" apply -f "${DEFAULT_CONFIGMAP_PATH}"
 
 fi
