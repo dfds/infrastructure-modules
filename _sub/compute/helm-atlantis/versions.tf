717e1672a8707a9e217f588d721bdcd6dc97a4e0
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-09-08 09:04:54 +0200

    chore(deps): update terraform aws to ~> 5.16.0 (#1124)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/helm-atlantis/versions.tf
+++ _sub/compute/helm-atlantis/versions.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.15.0"
+      version = "~> 5.16.0"
     }
     github = {
       source  = "integrations/github"
