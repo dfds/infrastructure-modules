593ed1638a6555cb7720cd892ac02bfe66dfb621
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-09-12 08:35:23 +0200

    chore(deps): update terraform github to ~> 5.36.0 (#1126)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/k8s-blaster-namespace/versions.tf
+++ _sub/compute/k8s-blaster-namespace/versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.35.0"
+      version = "~> 5.36.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
