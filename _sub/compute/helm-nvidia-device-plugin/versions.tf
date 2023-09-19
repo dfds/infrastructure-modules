28eba45d559df6ad60051ebe85b5684577cf9298
Aleksandra Korczak <alekorc@dfds.com>
2023-09-19 12:54:00 +0200

    Bump aws provider

--- _sub/compute/helm-nvidia-device-plugin/versions.tf
+++ _sub/compute/helm-nvidia-device-plugin/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.16.0"
+      version = "~> 5.17.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
