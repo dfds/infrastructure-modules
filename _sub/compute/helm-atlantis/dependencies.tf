d3e297f31d18ae267088df7783c3746e6ce95471
Aleksandra Korczak <alekorc@dfds.com>
2023-09-20 11:55:27 +0200

    Fix atlantis

--- _sub/compute/helm-atlantis/dependencies.tf
+++ _sub/compute/helm-atlantis/dependencies.tf
@@ -4,4 +4,5 @@ locals {
   ingress_class           = "traefik"
   ingress_auth_type       = "basic"
   resources_limits_memory = var.resources_limits_memory != null ? var.resources_limits_memory : var.resources_requests_memory
+  resources_limits_cpu    = var.resources_limits_cpu != null ? var.resources_limits_cpu : var.resources_requests_cpu # null values are not legal in templatefile
 }
