b8f77cae0068658d618ec1d2377b96a3523de1d0
Aleksandra Korczak <61739075+aleksandra-korczak@users.noreply.github.com>
2023-09-21 15:12:33 +0200

    Feature/gpu workloads (#1142)
    * Add support for GPU based workloads
    
    * Refactor + fmt
    
    * Add gpu node group to QA
    
    * Bump aws provider
    
    * Fix tests
    
    * Update tests
    
    * WIP
    
    * WIP

---
+++ _sub/compute/helm-nvidia-device-plugin/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.3.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11.0"
+    }
+  }
+}
