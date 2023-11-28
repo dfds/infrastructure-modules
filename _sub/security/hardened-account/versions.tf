badbdf3a23745fc3e93a5c68ee6f1e383a3b1217
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-11-28 10:19:21 +0100

    chore(deps): update terraform aws to ~> 5.27.0 (#1211)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/security/hardened-account/versions.tf
+++ _sub/security/hardened-account/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5.26.0"
+      version               = "~> 5.27.0"
       configuration_aliases = [aws.workload, aws.workload_2, aws.sso]
     }
   }
