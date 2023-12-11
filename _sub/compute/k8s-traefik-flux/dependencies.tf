752c364baed589d2bf122418b3b178d555c521e1
Audun Nes <aunes@dfds.com>
2023-12-11 13:18:38 +0100

    Use the new traefik.io/v1alpha1 apiVersion

--- _sub/compute/k8s-traefik-flux/dependencies.tf
+++ _sub/compute/k8s-traefik-flux/dependencies.tf
@@ -108,7 +108,7 @@ locals {
   }
 
   config_dashboard_ingressroute = {
-    "apiVersion" = "traefik.containo.us/v1alpha1"
+    "apiVersion" = "traefik.io/v1alpha1"
     "kind"       = "IngressRoute"
     "metadata" = {
       "name"      = "${var.deploy_name}-dashboard"
