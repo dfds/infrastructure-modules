3420128ac99aeb49af2c52bf2b2e6c34c88d4d2f
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2023-12-15 09:31:44 +0100

    chore(deps): update terraform aws to ~> 5.31.0 (#1239)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/k8s-traefik-flux/versions.tf
+++ _sub/compute/k8s-traefik-flux/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.30.0"
+      version = "~> 5.31.0"
     }
     github = {
       source  = "integrations/github"
--- compute/k8s-services/versions.tf
+++ compute/k8s-services/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.30.0"
+      version = "~> 5.31.0"
     }
 
     kubernetes = {
