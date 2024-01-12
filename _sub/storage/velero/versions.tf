1fc836aaf6704f86b9ba5e6f76ce0c76b27e1393
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-01-12 13:33:31 +0100

    chore(deps): update terraform aws to ~> 5.32.0 (#1260)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/storage/velero/versions.tf
+++ _sub/storage/velero/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31.0"
+      version = "~> 5.32.0"
     }
     github = {
       source  = "integrations/github"
