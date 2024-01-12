66e1840bf086da55bf7e2dd7b6092448d87a5d81
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-01-12 14:40:49 +0100

    chore(deps): update terraform github to ~> 5.44.0 (#1259)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/monitoring/blackbox-exporter/versions.tf
+++ _sub/monitoring/blackbox-exporter/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.43.0"
+      version = "~> 5.44.0"
     }
   }
 }
--- _sub/storage/velero/versions.tf
+++ _sub/storage/velero/versions.tf
@@ -15,7 +15,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.43.0"
+      version = "~> 5.44.0"
     }
   }
 
