f0c5348a5f7040e502fc7ab7c77cf1ced9707de1
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-01-22 11:01:53 +0100

    chore(deps): update terraform github to ~> 5.45.0 (#1271)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/helm-atlantis/versions.tf
+++ _sub/compute/helm-atlantis/versions.tf
@@ -14,7 +14,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.44.0"
+      version = "~> 5.45.0"
     }
     htpasswd = {
       source  = "loafoe/htpasswd"
