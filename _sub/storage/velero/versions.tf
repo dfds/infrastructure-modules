776a9c350dd460809082770911744aa82481b1c9
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-02-23 09:08:24 +0100

    chore(deps): update terraform aws to ~> 5.38.0 (#1304)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/storage/velero/versions.tf
+++ _sub/storage/velero/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.37.0"
+      version = "~> 5.38.0"
     }
     github = {
       source  = "integrations/github"
