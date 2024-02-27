dff3e79a95d48e4082a988444b4b4903b9cbd97b
Audun V. Nes <aunes@dfds.com>
2024-02-27 11:55:36 +0100

    Deploy metrics-server with Flux instead of Terraform (#1308)
    * Move metrics-server from Helm deploy to Flux deploy
    
    * Always use latest version of metrics-server in QA
    
    * Use default value of empty string instead of null for metrics_server_helm_chart_version

--- _sub/monitoring/metrics-server/versions.tf
+++ _sub/monitoring/metrics-server/versions.tf
@@ -1,9 +1,10 @@
 terraform {
   required_version = ">= 1.3.0, < 1.6.0"
+
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.12.0"
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0.0"
     }
   }
 }
