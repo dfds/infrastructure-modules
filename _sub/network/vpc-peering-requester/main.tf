6b0f4d0519e2059716c60093160cfe2e6f20ee4e
rifisdfds <rifis@dfds.com>
2024-03-06 10:40:05 +0000

    feat: add requester internet gateway

--- _sub/network/vpc-peering-requester/main.tf
+++ _sub/network/vpc-peering-requester/main.tf
@@ -75,3 +75,17 @@ resource "aws_route" "capability_to_shared" {
   destination_cidr_block    = var.cidr_block_peer
   vpc_peering_connection_id = aws_vpc_peering_connection.capability.id
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.peering.id
+
+  tags = {
+    Name = "peering-igw"
+  }
+}
+
+resource "aws_route" "capability_to_shared" {
+  route_table_id            = aws_vpc.peering.main_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
