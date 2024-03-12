ce0c7f5f42458cd094c351d0f27868019fd5b920
Aleksandra Fromm <61739075+aleksandra-fromm@users.noreply.github.com>
2024-03-12 15:54:16 +0100

    Update yaml files in blackbox-exporter (#1328)
    * Update yaml files in blackbox-exporter
    
    * Fix file name
    
    * Fix error
    
    * Change var default
    
    * WIP

--- _sub/monitoring/blackbox-exporter/main.tf
+++ _sub/monitoring/blackbox-exporter/main.tf
@@ -2,25 +2,38 @@
 # Create JSON files to be picked up by Flux CD
 # --------------------------------------------------
 resource "github_repository_file" "blackbox_exporter_helm" {
-  repository          = var.repo_name
-  branch              = local.repo_branch
-  file                = "${local.cluster_repo_path}/${local.app_install_name}-helm.yaml"
-  content             = jsonencode(local.app_helm_path)
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.cluster_repo_path}/${local.app_install_name}-helm.yaml"
+  content = templatefile("${path.module}/values/app-config.yaml", {
+    app_install_name = local.app_install_name
+    helm_repo_path   = local.helm_repo_path
+    prune            = var.prune
+  })
   overwrite_on_create = var.overwrite_on_create
 }
 
 resource "github_repository_file" "blackbox_exporter_helm_install" {
-  repository          = var.repo_name
-  branch              = local.repo_branch
-  file                = "${local.helm_repo_path}/kustomization.yaml"
-  content             = jsonencode(local.helm_install)
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.helm_repo_path}/kustomization.yaml"
+  content = templatefile("${path.module}/values/kustomization.yaml", {
+    gitops_apps_repo_url    = var.gitops_apps_repo_url
+    deploy_name             = var.deploy_name
+    gitops_apps_repo_branch = var.gitops_apps_repo_branch
+  })
   overwrite_on_create = var.overwrite_on_create
 }
 
 resource "github_repository_file" "blackbox_exporter_helm_patch" {
-  repository          = var.repo_name
-  branch              = local.repo_branch
-  file                = "${local.helm_repo_path}/patch.yaml"
-  content             = jsonencode(local.helm_patch)
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.helm_repo_path}/patch.yaml"
+  content = templatefile("${path.module}/values/patch.yaml", {
+    namespace          = var.namespace
+    helm_chart_version = var.helm_chart_version
+    replicas           = var.replicas
+    monitoring_targets = var.monitoring_targets
+  })
   overwrite_on_create = var.overwrite_on_create
 }
