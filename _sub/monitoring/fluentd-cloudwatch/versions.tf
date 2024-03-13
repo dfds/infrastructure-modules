cc5c3ef18c1e7e61fcdc51e84946c70831f68cce
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-03-13 08:34:37 +0100

    chore(deps): update terraform github to ~> 6.1.0 (#1329)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/monitoring/fluentd-cloudwatch/versions.tf
+++ _sub/monitoring/fluentd-cloudwatch/versions.tf
@@ -15,7 +15,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 6.0.0"
+      version = "~> 6.1.0"
     }
     tls = {
       source  = "hashicorp/tls"
--- _sub/monitoring/helm-exporter/versions.tf
+++ _sub/monitoring/helm-exporter/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 6.0.0"
+      version = "~> 6.1.0"
     }
   }
 }
