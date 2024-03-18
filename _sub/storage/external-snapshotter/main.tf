0729767eae0803f0e56db8df18824c2b2c0d2ee2
Aleksandra Fromm <61739075+aleksandra-fromm@users.noreply.github.com>
2024-03-18 13:37:36 +0100

    Refactor external-snapshotter submodule (#1331)
    * Refactor external-snapshotter submodule
    
    * Fix error
    
    * Fix error
    
    * Fix error
    
    * Fix error
    
    ---------
    
    Co-authored-by: Audun V. Nes <aunes@dfds.com>

--- _sub/storage/external-snapshotter/main.tf
+++ _sub/storage/external-snapshotter/main.tf
@@ -11,18 +11,26 @@
 # --------------------------------------------------
 
 resource "github_repository_file" "external-snapshotter_crd_path" {
-  repository          = var.repo_name
-  branch              = data.github_branch.flux_branch.branch
-  file                = "${local.cluster_repo_path}/${var.deploy_name}-crd.yaml"
-  content             = jsonencode(local.app_crd_path)
+  repository = var.repo_name
+  branch     = data.github_branch.flux_branch.branch
+  file       = "${local.cluster_repo_path}/${var.deploy_name}-crd.yaml"
+  content = templatefile("${path.module}/values/crd.yaml", {
+    deploy_name   = var.deploy_name
+    crd_repo_path = local.crd_repo_path
+    prune         = var.prune
+  })
   overwrite_on_create = var.overwrite_on_create
 }
 
 resource "github_repository_file" "external-snapshotter_crd_init" {
-  repository          = var.repo_name
-  branch              = data.github_branch.flux_branch.branch
-  file                = "${local.crd_repo_path}/kustomization.yaml"
-  content             = jsonencode(local.crd_init)
+  repository = var.repo_name
+  branch     = data.github_branch.flux_branch.branch
+  file       = "${local.crd_repo_path}/kustomization.yaml"
+  content = templatefile("${path.module}/values/crd-kustomization.yaml", {
+    gitops_apps_repo_url    = var.gitops_apps_repo_url
+    deploy_name             = var.deploy_name
+    gitops_apps_repo_branch = var.gitops_apps_repo_branch
+  })
   overwrite_on_create = var.overwrite_on_create
 }
 
@@ -32,18 +40,26 @@ resource "github_repository_file" "external-snapshotter_crd_init" {
 # --------------------------------------------------
 
 resource "github_repository_file" "external-snapshotter_controller_path" {
-  repository          = var.repo_name
-  branch              = data.github_branch.flux_branch.branch
-  file                = "${local.cluster_repo_path}/${var.deploy_name}-controller.yaml"
-  content             = jsonencode(local.app_controller_path)
+  repository = var.repo_name
+  branch     = data.github_branch.flux_branch.branch
+  file       = "${local.cluster_repo_path}/${var.deploy_name}-controller.yaml"
+  content = templatefile("${path.module}/values/controller.yaml", {
+    deploy_name          = var.deploy_name
+    controller_repo_path = local.controller_repo_path
+    prune                = var.prune
+  })
   overwrite_on_create = var.overwrite_on_create
 }
 
 resource "github_repository_file" "external-snapshotter_controller_init" {
-  repository          = var.repo_name
-  branch              = data.github_branch.flux_branch.branch
-  file                = "${local.controller_repo_path}/kustomization.yaml"
-  content             = jsonencode(local.controller_init)
+  repository = var.repo_name
+  branch     = data.github_branch.flux_branch.branch
+  file       = "${local.controller_repo_path}/kustomization.yaml"
+  content = templatefile("${path.module}/values/controller-kustomization.yaml", {
+    gitops_apps_repo_url    = var.gitops_apps_repo_url
+    deploy_name             = var.deploy_name
+    gitops_apps_repo_branch = var.gitops_apps_repo_branch
+  })
   overwrite_on_create = var.overwrite_on_create
 }
 
@@ -53,17 +69,25 @@ resource "github_repository_file" "external-snapshotter_controller_init" {
 # --------------------------------------------------
 
 resource "github_repository_file" "external-snapshotter_config_path" {
-  repository          = var.repo_name
-  branch              = data.github_branch.flux_branch.branch
-  file                = "${local.cluster_repo_path}/${var.deploy_name}-config.yaml"
-  content             = jsonencode(local.app_config_path)
+  repository = var.repo_name
+  branch     = data.github_branch.flux_branch.branch
+  file       = "${local.cluster_repo_path}/${var.deploy_name}-config.yaml"
+  content = templatefile("${path.module}/values/config.yaml", {
+    deploy_name      = var.deploy_name
+    config_repo_path = local.config_repo_path
+    prune            = var.prune
+  })
   overwrite_on_create = var.overwrite_on_create
 }
 
 resource "github_repository_file" "external-snapshotter_config_init" {
-  repository          = var.repo_name
-  branch              = data.github_branch.flux_branch.branch
-  file                = "${local.config_repo_path}/kustomization.yaml"
-  content             = jsonencode(local.config_init)
+  repository = var.repo_name
+  branch     = data.github_branch.flux_branch.branch
+  file       = "${local.config_repo_path}/kustomization.yaml"
+  content = templatefile("${path.module}/values/config-kustomization.yaml", {
+    gitops_apps_repo_url    = var.gitops_apps_repo_url
+    deploy_name             = var.deploy_name
+    gitops_apps_repo_branch = var.gitops_apps_repo_branch
+  })
   overwrite_on_create = var.overwrite_on_create
 }
