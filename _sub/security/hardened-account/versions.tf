4eb7d6453155f0010ac90b606fa5aacd17c326ff
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-04-19 08:24:43 +0200

    chore(deps): update terraform aws to ~> 5.46.0 (#1375)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/security/hardened-account/versions.tf
+++ _sub/security/hardened-account/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5.45.0"
+      version               = "~> 5.46.0"
       configuration_aliases = [aws.workload, aws.workload_2, aws.sso]
     }
   }
