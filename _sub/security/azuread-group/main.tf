a86a5f2091bb1292f5b997be71c34fad39b1ba29
Aleksandra Fromm <alekorc@dfds.com>
2024-05-14 15:05:22 +0200

    WIP

--- _sub/security/azuread-group/main.tf
+++ _sub/security/azuread-group/main.tf
@@ -1,18 +1,18 @@
 data "azuread_client_config" "current" {}
 
-#data "azuread_user" "owner" {
-#  for_each            = toset(var.owners)
-#  user_principal_name = each.value
-#}
+data "azuread_user" "owner" {
+  for_each            = toset(var.owners)
+  user_principal_name = each.value
+}
 
-#locals {
-#  owners = concat([data.azuread_client_config.current.object_id], [for user in data.azuread_user.owner : user.object_id])
-#}
+locals {
+  owners = concat([data.azuread_client_config.current.object_id], [for user in data.azuread_user.owner : user.object_id])
+}
 
 resource "azuread_group" "this" {
   display_name     = var.display_name
   security_enabled = true
-#  owners           = [data.azuread_client_config.current.object_id]
+  owners           = [data.azuread_client_config.current.object_id]
   administrative_unit_ids = var.administrative_unit_ids
 #  prevent_duplicate_names = true
 }
