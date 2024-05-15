b788b029f63e8e73c834a35cd422ac420cbfe04c
Aleksandra Fromm <61739075+aleksandra-fromm@users.noreply.github.com>
2024-05-15 13:29:30 +0200

    Add azuread_group submodule (#1401)
    * Add azuread_group submodule
    
    * Fix locals
    
    * Add administrative_unit_ids
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP

---
+++ _sub/security/azuread-group/main.tf
@@ -0,0 +1,20 @@
+data "azuread_client_config" "current" {}
+
+data "azuread_user" "owner" {
+  for_each            = toset(var.owners)
+  user_principal_name = each.value
+}
+
+locals {
+  owners = concat([data.azuread_client_config.current.object_id], [for user in data.azuread_user.owner : user.object_id])
+}
+
+resource "azuread_group" "this" {
+  display_name     = var.display_name
+  security_enabled = true
+  owners           = local.owners
+  administrative_unit_ids = var.administrative_unit_ids
+  prevent_duplicate_names = true
+}
+
+
---
+++ _sub/security/azuread-group/vars.tf
@@ -0,0 +1,13 @@
+variable "owners" {
+  type = list(string)
+  description = "List of owners of the group"
+}
+
+variable "display_name" {
+  type = string
+  description = "The display name for the group"
+}
+
+variable "administrative_unit_ids" {
+  type = list(string)
+}
