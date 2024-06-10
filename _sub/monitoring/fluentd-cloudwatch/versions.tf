803a9e78fd14a3c785978d349655420d7bce81a9
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-06-10 11:35:57 +0200

    chore(deps): update terraform aws to ~> 5.53.0 (#1430)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/monitoring/fluentd-cloudwatch/versions.tf
+++ _sub/monitoring/fluentd-cloudwatch/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.51.0"
+      version = "~> 5.53.0"
     }
     github = {
       source  = "integrations/github"
--- compute/k8s-services/versions.tf
+++ compute/k8s-services/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.51.0"
+      version = "~> 5.53.0"
     }
 
     kubernetes = {
