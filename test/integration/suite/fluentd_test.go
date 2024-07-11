9b4f20c7c6cd29f02496e37e738546b4fb9cd0ef
Audun Nes <aunes@dfds.com>
2024-07-11 15:47:24 +0200

    Remove test case for fluentd

--- test/integration/suite/fluentd_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestFluentdDaemonSet(t *testing.T) {
-	t.Parallel()
-	clientset := NewK8sClientSet(t)
-	AssertK8sDaemonSet(t, clientset, "fluentd", "fluentd-cloudwatch", 5)
-}
