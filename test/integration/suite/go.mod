a23a683dc465b30df08b355180702b9e0490c48d
rifisdfds <rifis@dfds.com>
2024-07-23 15:38:24 +0100

    fix: qa traefik resource test

--- test/integration/suite/go.mod
+++ test/integration/suite/go.mod
@@ -14,7 +14,8 @@ require (
 
 require (
 	github.com/google/gnostic-models v0.6.8 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/traefik/traefik/v2 v2.11.6 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 )
 
@@ -30,23 +31,23 @@ require (
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/go-logr/logr v1.4.1 // indirect; indirect // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect; indirect // indirect
-	github.com/imdario/mergo v0.3.13 // indirect; indirect // indirect
+	github.com/imdario/mergo v0.3.16 // indirect; indirect // indirect
 	github.com/json-iterator/go v1.1.12 // indirect; indirect // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect; indirect // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/oauth2 v0.10.0 // indirect; indirect // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/term v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/time v0.3.0 // indirect; indirect // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect; indirect // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.5.0 // indirect; indirect // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect; indirect // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
--- test/integration/suite/traefik_test.go
+++ test/integration/suite/traefik_test.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 	"strings"
 	"testing"
+
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	intstr "k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,19 +26,193 @@ func TestTraefikDeployment(t *testing.T) {
 	AssertK8sDeployment(t, clientset, "traefik-blue-variant", "traefik-blue-variant", 3)
 }
 
-// The Grafana endpoint is not public any longer, but protected by SSO.
 func TestTraefikIngressRouteAndMiddleware(t *testing.T) {
 	clientset := NewK8sClientSet(t)
 	AssertFluxReconciliation(t, clientset)
 	AssertK8sDeployment(t, clientset, "traefik-blue-variant", "traefik-blue-variant", 3)
 
+	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	dynamicClient, err := dynamic.NewForConfig(clientset.RESTConfig())
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "nginx-test",
+			Namespace: "default",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: func() *int32 { i := int32(2); return &i }(),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "nginx-test",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "nginx-test",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "web",
+							Image: "nginx",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Create Deployment
+	fmt.Println("Creating deployment...")
+	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+
+	serviceClient := clientset.CoreV1().Services(apiv1.NamespaceDefault)
+
+	var service = &apiv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "nginx-test",
+			Namespace: "default",
+		},
+		Spec: apiv1.ServiceSpec{
+			Ports: []apiv1.ServicePort{apiv1.ServicePort{
+				Name:       "web",
+				Port:       int32(80),
+				TargetPort: intstr.IntOrString{IntVal: int32(80)},
+			}},
+			Selector: map[string]string{"app": "nginx-test"},
+		},
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Service",
+		},
+	}
+
+	// Create Service
+	fmt.Println("Creating service...")
+	serviceResult, err := serviceClient.Create(context.TODO(), service, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created service %q.\n", serviceResult.GetObjectMeta().GetName())
+
+	// Custom Resources
+
+	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+	cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatalf("Error building kubeconfig: %v", err)
+	}
+
+	// Create the Kubernetes clientset
+
+	customClientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Fatalf("Error creating Kubernetes clientset: %v", err)
+	}
+
+	_, err := customClientSet.Resource(ingressRouteGVR).Namespace("default").Create(context.TODO(), ingressRoute, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("error creating IngressRoute: %v", err)
+	}
+
+	// Create the controller-runtime client for Traefik CRDs
+	k8sClient, err := client.New(cfg, client.Options{})
+	if err != nil {
+		log.Fatalf("Error creating controller-runtime client: %v", err)
+	}
+
+	// Traefik Middleware
+
+	middleware := &traefikv1alpha1.Middleware{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "middleware-test",
+			Namespace: "default",
+		},
+		Spec: traefikv1alpha1.MiddlewareSpec{
+			StripPrefix: &traefikv1alpha1.StripPrefix{
+				Prefixes: []string{"/test"},
+			},
+		},
+	}
+
+	if err := k8sClient.Create(context.TODO(), middleware); err != nil {
+		return fmt.Errorf("error creating Middleware: %v", err)
+	}
+
+	// Traefik IngressRoute
+
+	ingressRoute := &traefikv1alpha1.IngressRoute{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "nginx-test",
+			Namespace: "default",
+		},
+		Spec: traefikv1alpha1.IngressRouteSpec{
+			Routes: []traefikv1alpha1.Route{
+				{
+					Match: "Host(`nginx-test.qa.qa.dfds.cloud`) && PathPrefix(`/test`)",
+					Kind:  "Rule",
+					Services: []traefikv1alpha1.Service{
+						{
+							Name: "nginx-test",
+							Port: 80,
+						},
+					},
+					Middlewares: []traefikv1alpha1.MiddlewareRef{
+						{
+							Name: "middleware-test",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := k8sClient.Create(context.TODO(), ingressRoute); err != nil {
+		return fmt.Errorf("error creating IngressRoute: %v", err)
+	}
+
+	// Delete resources
+
+	if err := k8sClient.Delete(context.TODO(), middleware); err != nil {
+		return fmt.Errorf("error deleting Middleware: %v", err)
+	}
+
+	if err := k8sClient.Delete(context.TODO(), ingressRoute); err != nil {
+		return fmt.Errorf("error deleting IngressRoute: %v", err)
+	}
+
+	deletePolicy := metav1.DeletePropagationForeground
+	if err := deploymentsClient.Delete(context.TODO(), name, metav1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	}); err != nil {
+		return fmt.Errorf("error deleting Deployment: %v", err)
+	}
+
+	servicesClient := clientset.CoreV1().Services(namespace)
+	if err := servicesClient.Delete(context.TODO(), name, metav1.DeleteOptions{}); err != nil {
+		return fmt.Errorf("error deleting Service: %v", err)
+	}
+
 	// The Grafana deployment utilizes the Traefik resources IngressRoute and
 	// Middleware to expose a public endpoint. This public endpoint will be
 	// used to check if Traefik is routing traffic correctly.
-	AssertK8sDeployment(t, clientset, "monitoring", "monitoring-grafana", 1)
+	AssertK8sDeployment(t, clientset, "default", "test-ingress-deployment", 1)
 
 	// Call the Grafana health endpoint and parse the response
-	resp, err := http.Get("https://grafana.qa.qa.dfds.cloud/infrastructure/api/health")
+	resp, err := http.Get("https://nginx-test.qa.qa.dfds.cloud/test")
 	if err != nil {
 		t.Fatal(err)
 	}
