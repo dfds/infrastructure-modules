9fe1c97b9bfe1f449f93a343967dc89a15ab8792
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-08-01 10:15:29 +0200

    chore(deps): update terraform aws to ~> 5.60.0 (#1492)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>
    Co-authored-by: rifisdfds <40063756+rifisdfds@users.noreply.github.com>

--- _sub/monitoring/fluentd-cloudwatch/versions.tf
+++ _sub/monitoring/fluentd-cloudwatch/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.59.0"
+      version = "~> 5.60.0"
     }
     github = {
       source  = "integrations/github"
