2b822e9a812d2455e21f7e6dae35fcb30690097c
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-09-05 12:54:00 +0200

    chore(deps): update terraform aws to ~> 5.65.0 (#1531)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- storage/s3-velero-backup/versions.tf
+++ storage/s3-velero-backup/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.64.0"
+      version = "~> 5.65.0"
     }
     tls = {
       source  = "hashicorp/tls"
