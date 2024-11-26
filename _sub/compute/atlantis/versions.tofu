a1be676e72e994bd5f1015e6073a68fc0079efc9
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2024-11-26 15:05:54 +0100

    chore(deps): update terraform github to ~> 6.4.0 (#1635)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/atlantis/versions.tofu
+++ _sub/compute/atlantis/versions.tofu
@@ -8,7 +8,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 6.3.0"
+      version = "~> 6.4.0"
     }
     htpasswd = {
       source  = "loafoe/htpasswd"
--- _sub/compute/k8s-traefik-flux/versions.tf
+++ _sub/compute/k8s-traefik-flux/versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 6.3.0"
+      version = "~> 6.4.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
