0f73766ff1985042fa27a3855dcd6f94a62e93f1
samidbb <samdi@dfds.com>
2025-01-08 09:50:11 +0100

    missing service account name for Kustomization

--- _sub/security/helm-1password-connect/dependencies.tf
+++ _sub/security/helm-1password-connect/dependencies.tf
@@ -12,12 +12,27 @@ locals {
 
 locals {
   app_helm_path = <<YAML
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: cluster-reconciler-${var.deploy_name}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: helm-controller
+  namespace: ${var.namespace}
+---
 apiVersion: kustomize.toolkit.fluxcd.io/v1
 kind: Kustomization
 metadata:
   name: "${local.app_install_name}-helm"
   namespace: "flux-system"
 spec:
+  serviceAccountName: kustomize-controller
   interval: 1m0s
   dependsOn:
     - name: "platform-apps-sources"
