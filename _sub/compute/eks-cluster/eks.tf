c2ce057d97fcbcfbfe5e5fdcd537258a60a74d85
Audun V. Nes <aunes@dfds.com>
2025-02-04 17:40:17 +0100

    Revert "Ensure cluster can be create from scratch without failure to setup EF…" (#1713)
    This reverts commit 8ddc023b2af5662484e91761e317e2a7d6b97e46.

--- _sub/compute/eks-cluster/eks.tf
+++ _sub/compute/eks-cluster/eks.tf
@@ -1,17 +1,11 @@
 #tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "eks" {
-  #checkov:skip=CKV_AWS_158: Ensure that CloudWatch Log Group is encrypted by KMS
-  #checkov:skip=CKV_AWS_338: Ensure CloudWatch log groups retains logs for at least 1 year
   name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = var.log_retention_days
 }
 
 #tfsec:ignore:aws-eks-no-public-cluster-access-to-cidr tfsec:ignore:aws-eks-no-public-cluster-access tfsec:ignore:aws-eks-encrypt-secrets tfsec:ignore:aws-eks-enable-control-plane-logging
 resource "aws_eks_cluster" "eks" {
-  #checkov:skip=CKV_AWS_37: Ensure Amazon EKS control plane logging is enabled for all log types
-  #checkov:skip=CKV_AWS_38: Ensure Amazon EKS public endpoint not accessible to 0.0.0.0/0
-  #checkov:skip=CKV_AWS_39: Ensure Amazon EKS public endpoint disabled
-  #checkov:skip=CKV_AWS_58: Ensure EKS Cluster has Secrets Encryption Enabled
   name     = var.cluster_name
   role_arn = aws_iam_role.eks.arn
   version  = var.cluster_version
