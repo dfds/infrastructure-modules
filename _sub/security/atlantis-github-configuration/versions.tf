5288de6a4443b59b30471ae929ddbcc91ceb8940
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2025-02-18 10:29:51 +0100

    chore(deps): update terraform aws to ~> 5.87.0 (#1724)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/security/atlantis-github-configuration/versions.tf
+++ _sub/security/atlantis-github-configuration/versions.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.86.0"
+      version = "~> 5.87.0"
     }
     github = {
       source  = "integrations/github"
