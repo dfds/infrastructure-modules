62432da5c7956a3613885878d3653a574b2b8960
samidbb <samdi@dfds.com>
2025-03-04 08:45:02 +0100

    velero fsb as variable for testing

--- compute/k8s-services/main.tf
+++ compute/k8s-services/main.tf
@@ -656,6 +656,7 @@ module "velero" {
   image_tag                           = var.velero_image_tag
   plugin_for_aws_version              = var.velero_plugin_for_aws_version
   snapshots_enabled                   = var.velero_snapshots_enabled
+  filesystem_backup_enabled           = var.velero_filesystem_backup_enabled
   overwrite_on_create                 = var.fluxcd_bootstrap_overwrite_on_create
   gitops_apps_repo_url                = local.fluxcd_apps_repo_url
   gitops_apps_repo_branch             = var.fluxcd_apps_repo_branch
