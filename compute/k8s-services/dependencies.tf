108fcd3de8760f51091e9a3640461ad0b5e1a839
Audun Nes <aunes@dfds.com>
2025-03-04 16:43:50 +0100

    Add a new variable to control the port number that blackbox exporter should contact the Traefik endpoint on

--- compute/k8s-services/dependencies.tf
+++ compute/k8s-services/dependencies.tf
@@ -225,13 +225,13 @@ locals {
 
   blackbox_exporter_monitoring_traefik_blue_variant = var.traefik_blue_variant_deploy ? [{
     "name"   = "traefik-blue-variant"
-    "url"    = "http://traefik-blue-variant.traefik-blue-variant:9000/ping"
+    "url"    = "http://traefik-blue-variant.traefik-blue-variant:${var.blackbox_exporter_monitoring_traefik_blue_variant_port}/ping"
     "module" = "http_2xx"
   }] : []
 
   blackbox_exporter_monitoring_traefik_green_variant = var.traefik_green_variant_deploy ? [{
     "name"   = "traefik-green-variant"
-    "url"    = "http://traefik-green-variant.traefik-green-variant:9000/ping"
+    "url"    = "http://traefik-green-variant.traefik-green-variant:${var.blackbox_exporter_monitoring_traefik_green_variant_port}/ping"
     "module" = "http_2xx"
   }] : []
 
