126960407a05d805427543d6192d690dd5fadcaf
samidbb <samdi@dfds.com>
2025-03-17 13:11:59 +0100

    c

--- security/org-policies/restrictive.tf
+++ security/org-policies/restrictive.tf
@@ -210,7 +210,8 @@ data "aws_iam_policy_document" "restrictive" {
       "s3:GetMultiRegionAccessPoint",
       "s3:DescribeMultiRegionAccessPointOperation",
       "trustedadvisor:DescribeAccount*",
-      "trustedadvisor:DescribeChecks"
+      "trustedadvisor:DescribeChecks",
+      "trustedadvisor:DescribeRisk*"
     ]
     resources = ["*"]
     condition {
