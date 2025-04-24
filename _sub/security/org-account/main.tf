c1aa3861e5930b385ab8b1b5a96d303e8f05b64c
Audun Nes <aunes@dfds.com>
2025-04-24 11:51:45 +0200

    Fixed what was broken.

--- _sub/security/org-account/main.tf
+++ _sub/security/org-account/main.tf
@@ -1,11 +1,9 @@
-data "aws_organizations_organization" "org" {}
-
 resource "aws_organizations_account" "org_account" {
   name                       = lower(var.name)
   email                      = var.email
   iam_user_access_to_billing = "ALLOW"
   role_name                  = var.org_role_name
-  parent_id                  = var.parent_id != "" ? var.parent_id : data.aws_organizations_organization.org.roots[0].id
+  parent_id                  = var.parent_id
 
   provisioner "local-exec" {
     command = "sleep ${var.sleep_after}"
