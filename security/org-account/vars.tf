7400df0416387287355e718ed8400cf94b896419
Audun Nes <aunes@dfds.com>
2025-04-24 11:55:03 +0200

    Suppply default value for parent_id if missing

--- security/org-account/vars.tf
+++ security/org-account/vars.tf
@@ -24,8 +24,8 @@ variable "email" {
 
 variable "parent_id" {
   type        = string
-  description = "The ID of the parent AWS Organization OU."
-  default     = ""
+  description = "The ID of the parent AWS Organization OU. Defaults to the root."
+  default     = "r-65k1" # TODO: Get from data source, once supported
 }
 
 variable "cloudtrail_local_s3_bucket" {
