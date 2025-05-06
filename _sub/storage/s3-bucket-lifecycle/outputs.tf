6ab4ba9d98643eb3e401c08b0bd801e6f843fbcf
Audun Nes <aunes@dfds.com>
2025-05-06 12:28:01 +0200

    Add outputs to velero and s3-lifecycle modules

--- _sub/storage/s3-bucket-lifecycle/outputs.tf
+++ _sub/storage/s3-bucket-lifecycle/outputs.tf
@@ -5,3 +5,10 @@ output "name" {
 output "arn" {
   value = aws_s3_bucket.bucket.arn
 }
+
+output "replication_role_arn" {
+  value = try(element([
+    for role in aws_iam_role.replication_role : role.arn
+    if role != null
+  ], 0), "")
+}
--- storage/s3-velero-backup/outputs.tf
+++ storage/s3-velero-backup/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_name" {
 output "bucket_arn" {
   value = module.velero_storage.arn
 }
+
+output "bucket_replication_role_arn" {
+  value = try(module.velero_storage.replication_role_arn, "")
+}
