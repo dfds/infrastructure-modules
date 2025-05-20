214e3e20382b3c01ba55b75634c74294b102398b
Dev-Excellence Service Account <65592364+devex-sa@users.noreply.github.com>
2025-05-20 13:40:23 +0200

    chore(deps): update terraform azuread to v3.4.0 (#1820)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- _sub/compute/helm-kube-prometheus-stack/versions.tofu
+++ _sub/compute/helm-kube-prometheus-stack/versions.tofu
@@ -20,7 +20,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 3.3.0"
+      version = "~> 3.4.0"
     }
   }
 }
--- _sub/security/azuread-group/versions.tf
+++ _sub/security/azuread-group/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "3.3.0"
+      version = "3.4.0"
     }
   }
 }
