536e386794a1a06423f1820c747b2f9403dd0eab
rifisdfds <rifis@dfds.com>
2025-06-04 14:36:58 +0100

    fix: replaces noisy local_file resources with null_resource

--- _sub/compute/eks-heptio/main.tf
+++ _sub/compute/eks-heptio/main.tf
@@ -2,18 +2,23 @@
 # Kubeconfig
 # --------------------------------------------------
 
-resource "local_file" "kubeconfig_admin" {
-  content  = local.kubeconfig_admin_template
-  filename = local.temp_kubeconfig_path
+resource "null_resource" "kubeconfig_admin" {
+  triggers = {
+    content_hash = sha1(local.kubeconfig_admin_template)
+  }
 
-  # The path ${var.kubeconfig_path} is OS and user context-depdendent. This causes problems e.g. when executed locally.
+  # The path ${var.kubeconfig_path} is OS and user context-dependent. This causes problems e.g. when executed locally.
   # The path to the config file might be different than in the state, causing Terraform to fail refreshing state for the KUBECONFIG file.
   # The current workaround is to generate the file in a relative but non-expanded path, and move it using a script.
 
   provisioner "local-exec" {
-    command = "bash -c '${path.module}/move_kubeconfig.sh ${local.temp_kubeconfig_path} ${var.kubeconfig_path}'"
+    command = <<EOT
+      echo "${local.kubeconfig_admin_template}" > ${local.temp_kubeconfig_path}
+      bash -c '${path.module}/move_kubeconfig.sh ${local.temp_kubeconfig_path} ${var.kubeconfig_path}'
+    EOT
   }
 }
+
 
 
 # --------------------------------------------------
@@ -24,23 +29,25 @@ locals {
   path_default_configmap = "${path.cwd}/default-auth-cm.yaml"
 }
 
-resource "local_file" "default-configmap" {
-  content  = local.default_auth_cm_template
-  filename = local.path_default_configmap
-}
-
-resource "null_resource" "enable-workers-default" {
+resource "null_resource" "apply_default_configmap" {
   count = var.blaster_configmap_apply ? 0 : 1
 
+  triggers = {
+    content_hash = sha1(local.default_auth_cm_template)
+  }
+
   provisioner "local-exec" {
-    command = "kubectl --kubeconfig ${var.kubeconfig_path} apply -f ${local.path_default_configmap}"
+    command = <<EOT
+      echo '${local.default_auth_cm_template}' > ${local.path_default_configmap}
+      kubectl --kubeconfig ${var.kubeconfig_path} apply -f ${local.path_default_configmap}
+    EOT
   }
 
   depends_on = [
-    local_file.kubeconfig_admin,
-    local_file.default-configmap,
+    null_resource.kubeconfig_admin
   ]
 }
+
 
 resource "null_resource" "enable-workers-from-s3" {
   count = var.blaster_configmap_apply ? 1 : 0
@@ -56,7 +63,7 @@ resource "null_resource" "enable-workers-from-s3" {
   }
 
   depends_on = [
-    local_file.kubeconfig_admin,
-    local_file.default-configmap,
+    null_resource.kubeconfig_admin,
+    null_resource.apply_default_configmap,
   ]
 }
