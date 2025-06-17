bd4ab442d46436428a06df69646a4aae6f76b43a
Sami <40062553+samidbb@users.noreply.github.com>
2025-06-17 09:32:58 +0200

    Test/test (#1863)

--- security/ebs-tag-shared-snapshots/main.tf
+++ security/ebs-tag-shared-snapshots/main.tf
@@ -3,13 +3,12 @@ module "lambda" {
   source = "../../_sub/compute/lambda/lambda-from-archive"
   filename_out = "${path.module}/lambda.zip"
   path_to_index_file = "${path.module}/src/index.js"
-  name = "ebs-snapshot-share"
+  name = "ebs-snapshot-tag"
   function_environment_variables = {
     RUN_AWS_REGION = var.aws_region
-    # DESTINATION_ACCOUNTS = join(",", var.snapshot_share_destination_accounts)
   }
 }
-
+# test
 resource "aws_cloudwatch_event_rule" "this" {
   name        = "capture-snapshot-shared-event"
   description = "Capture each EBS Snapshot Shared"
@@ -17,7 +16,12 @@ resource "aws_cloudwatch_event_rule" "this" {
   event_pattern = <<PATTERN
 {
   "source": ["aws.ec2"],
-  "detail-type": ["EBS Snapshot Notification"]
+  "detail-type": ["EBS Snapshot Notification"],
+  "detail": {
+    "result": ["succeeded"],
+    "event": ["shareSnapshot"],
+    "source": ["${var.shared_snapshot_source_account}"]
+  }
 }
 PATTERN
 }
@@ -36,7 +40,7 @@ module "trigger_lambda_permission" {
 }
 
 resource "aws_iam_role_policy" "ebs_snapshots" {
-  name = "ebs-snapshot-share"
+  name = "ebs-snapshot-tag"
   role = module.lambda.role_name
   policy = data.aws_iam_policy_document.ebs_snapshots.json
 }
