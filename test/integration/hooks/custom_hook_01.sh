f424812da54bc7dc041c36f2c9373c40fe7e2c36
Audun Nes <aunes@dfds.com>
2025-08-14 12:58:29 +0200

    feat(external-secrets): add helm chart version input and update custom hook for ESO installation

--- test/integration/hooks/custom_hook_01.sh
+++ test/integration/hooks/custom_hook_01.sh
@@ -19,7 +19,7 @@ echo "KUBECONFIG=${KUBECONFIG}"
 
 cd "${PARENT_DIR}/eu-west-1/k8s-qa/services" || return
 
-# Check if namespace exists
+# Check if Grafana namespace exists
 NS=$(kubectl get namespace --no-headers | grep -w grafana | wc -l | tr -d ' ')
 if [[ ${NS} -eq 1 ]]; then
 	PVC=$(kubectl get pvc -n grafana --no-headers | wc -l | tr -d ' ')
@@ -33,7 +33,18 @@ if [[ ${NS} -eq 1 ]]; then
 	fi
 fi
 
+# Check if ESO is installed
+ESO=$(kubectl get helmrelease -n external-secrets -o custom-columns=VERSION:.spec.chart.spec.version --no-headers | wc -l | tr -d ' ')
+if [[ ${ESO} -eq 1 ]]; then
+	# Find desired helm chart version
+	HELMRELEASE=$(grep external_secrets_helm_chart_version terragrunt.hcl | cut -d '"' -f2)
+	if [[ "x${HELMRELEASE}x" != "xx" ]]; then
+		echo "Applying/Upgrading CRDs for ESO"
+		kubectl apply --server-side -f https://raw.githubusercontent.com/external-secrets/external-secrets/refs/tags/v${HELMRELEASE}/deploy/crds/bundle.yaml
+	fi
+fi
+
 TENANT_NS=$(kubectl get namespace --no-headers | grep -w flux-tenant-test | wc -l | tr -d ' ')
 if [[ ${TENANT_NS} -eq 0 ]]; then
-  kubectl create namespace flux-tenant-test
+	kubectl create namespace flux-tenant-test
 fi
