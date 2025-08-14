f00601a9ee6fcced826afeb8aafdb0c2a29315de
Willi Carlsen <carlsenwilli@gmail.com>
2025-08-14 14:43:15 +0200

    Refactor priorityclass (#1925)
    * Refactor priorityclass
    
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>
    
    * Deleted vars
    
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>
    
    ---------
    
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- _sub/compute/k8s-priority-class/main.tf
+++ _sub/compute/k8s-priority-class/main.tf
@@ -1,10 +1,41 @@
+locals {
+  priority_classes = [
+    {
+      "name"        = "service-critical"
+      "description" = "Used for service critical pods, e.g. ingress controllers."
+      "priority"    = 1000000
+    },
+    {
+      "name"        = "cluster-monitoring"
+      "description" = "Used for pods responsible for cluster-wide monitoring, alerting, and logging."
+      "priority"    = 100000
+    },
+    {
+      "name"        = "selfservice"
+      "description" = "Used for pods necessary to support the DevEx self-service system."
+      "priority"    = 10000
+    },
+    {
+      "name"        = "high"
+      "description" = "Used for production-grade workloads."
+      "priority"    = 1000
+    },
+    {
+      "name"        = "low"
+      "description" = "The default priority class. Used for pods that are less important, e.g. dev and test pods."
+      "priority"    = 10
+    }
+  ]
+  global_default = "low"
+}
+
 resource "kubernetes_priority_class" "class" {
-  count = length(var.priority_class)
+  for_each = {for pc in local.priority_classes : pc.name => pc}
   metadata {
-    name = var.priority_class[count.index].name
+    name = each.value.name
   }
 
-  description    = var.priority_class[count.index].description
-  value          = var.priority_class[count.index].priority
-  global_default = try(var.priority_class[count.index].default, false)
+  description    = each.value.description
+  value          = each.value.priority
+  global_default = local.global_default == each.value.name
 }
