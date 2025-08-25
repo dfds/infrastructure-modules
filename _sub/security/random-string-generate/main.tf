997f376e0c525e78445809d9b77495930c5f2c85
Audun Nes <aunes@dfds.com>
2025-08-25 11:09:41 +0200

    chore: remove unused random string generation Terraform configuration files

--- _sub/security/random-string-generate/main.tf
+++
@@ -1,6 +0,0 @@
-resource "random_string" "password" {
-  count            = var.deploy ? 1 : 0
-  special          = var.special_character_enabled
-  override_special = "{}[]!"
-  length           = 24
-}
