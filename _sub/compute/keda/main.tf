7292df5a3868b89b4cd29f2b44e770dcd1cdadc1
Audun V. Nes <aunes@dfds.com>
2025-10-06 15:03:42 +0200

    refactor: streamline KEDA deployment configuration and remove unused variables (#2029)
    * refactor: streamline KEDA deployment configuration and remove unused variables
    
    * feat: enable KEDA deployment by default

--- _sub/compute/keda/main.tf
+++ _sub/compute/keda/main.tf
@@ -9,11 +9,10 @@ resource "github_repository_file" "helm" {
   content = templatefile("${path.module}/values/app.yaml", {
     app_install_name = local.app_install_name
     helm_repo_path   = local.helm_repo_path
-    deploy_name      = var.deploy_name
-    namespace        = var.namespace
+    deploy_name      = local.deploy_name
     prune            = var.prune
   })
-  overwrite_on_create = var.overwrite_on_create
+  overwrite_on_create = true
 }
 
 resource "github_repository_file" "helm_install" {
@@ -22,20 +21,9 @@ resource "github_repository_file" "helm_install" {
   file       = "${local.helm_repo_path}/kustomization.yaml"
   content = templatefile("${path.module}/values/kustomization.yaml", {
     gitops_apps_repo_url    = var.gitops_apps_repo_url
-    deploy_name             = var.deploy_name
+    deploy_name             = local.deploy_name
     gitops_apps_repo_branch = var.gitops_apps_repo_branch
+    chart_version           = var.chart_version
   })
-  overwrite_on_create = var.overwrite_on_create
-}
-
-resource "github_repository_file" "helm_patch" {
-  repository = var.repo_name
-  branch     = local.repo_branch
-  file       = "${local.helm_repo_path}/patch.yaml"
-  content = templatefile("${path.module}/values/patch.yaml", {
-    namespace                 = var.namespace
-    chart_version             = var.chart_version
-    deploy_name               = var.deploy_name
-  })
-  overwrite_on_create = var.overwrite_on_create
+  overwrite_on_create = true
 }
