ef36445e5a30a6121a17366f7ae748d1d4340de8
Willi Carlsen <carlsenwilli@gmail.com>
2025-10-27 18:28:45 +0100

    Provisioning of new clusters blocked by removal of bootstrap_cluster_creator_admin_permission feature
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- _sub/compute/eks-cluster/eks.tf
+++ _sub/compute/eks-cluster/eks.tf
@@ -25,7 +25,7 @@ resource "aws_eks_cluster" "eks" {
 
   access_config {
     authentication_mode                         = "API_AND_CONFIG_MAP"
-    bootstrap_cluster_creator_admin_permissions = false
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   vpc_config {
