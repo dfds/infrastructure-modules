1ab824eb8a3a5b0a5bf0783257f22edb152078ef
Mik-Nord <miknord@dfds.com>
2025-11-24 08:28:07 +0100

    Save state

---
+++ _sub/compute/kyverno/main.tf
@@ -0,0 +1,29 @@
+# --------------------------------------------------
+# Create files to be picked up by Flux CD
+# --------------------------------------------------
+
+resource "github_repository_file" "helm" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.cluster_repo_path}/${local.app_install_name}-helm.yaml"
+  content = templatefile("${path.module}/values/app.yaml", {
+    app_install_name = local.app_install_name
+    helm_repo_path   = local.helm_repo_path
+    deploy_name      = local.deploy_name
+    prune            = var.prune
+  })
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "helm_install" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.helm_repo_path}/kustomization.yaml"
+  content = templatefile("${path.module}/values/kustomization.yaml", {
+    gitops_apps_repo_url    = var.gitops_apps_repo_url
+    deploy_name             = local.deploy_name
+    gitops_apps_repo_branch = var.gitops_apps_repo_branch
+    chart_version           = var.chart_version
+  })
+  overwrite_on_create = true
+}
