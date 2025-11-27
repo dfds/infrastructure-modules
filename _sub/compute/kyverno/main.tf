be82fe47db1713d58e26832bb6d0ce4ac8d58a1f
Mik-Nord <miknord@dfds.com>
2025-11-27 21:54:25 +0100

    Removed tests

--- _sub/compute/kyverno/main.tf
+++ _sub/compute/kyverno/main.tf
@@ -22,8 +22,7 @@ resource "github_repository_file" "helm_install" {
   content = templatefile("${path.module}/values/kustomization.yaml", {
     gitops_apps_repo_url    = var.gitops_apps_repo_url
     deploy_name             = local.deploy_name
-    # gitops_apps_repo_ref    = var.gitops_apps_repo_ref
-    gitops_apps_repo_ref    = "feature/kyverno-two-stages"
+    gitops_apps_repo_ref    = var.gitops_apps_repo_ref
   })
   overwrite_on_create = true
 }
@@ -47,8 +46,7 @@ resource "github_repository_file" "policies_kustomization" {
   content = templatefile("${path.module}/values/policies-kustomization.yaml", {
     gitops_apps_repo_url = var.gitops_apps_repo_url
     deploy_name          = local.deploy_name
-    # gitops_apps_repo_ref = var.gitops_apps_repo_ref
-    gitops_apps_repo_ref = "feature/kyverno-two-stages"
+    gitops_apps_repo_ref = var.gitops_apps_repo_ref
   })
   overwrite_on_create = true
 }
