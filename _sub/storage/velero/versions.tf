d257c9ec2b0ec6972d696fb2f55b078570e91b8d
Audun Nes <aunes@dfds.com>
2025-12-05 14:32:44 +0100

    Update AWS provider version to 6.23.0 across multiple modules for improved compatibility and features

--- _sub/storage/velero/versions.tf
+++ _sub/storage/velero/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.0"
+      version = ">= 6.23.0"
     }
     github = {
       source  = "integrations/github"
