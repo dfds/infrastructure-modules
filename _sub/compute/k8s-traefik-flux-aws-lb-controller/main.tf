bfc25979753fb7b9425026bd1cd18277d72a004b
samidbb <samdi@dfds.com>
2025-12-18 15:48:49 +0100

    nlb with aws lb controller working config

---
+++ _sub/compute/k8s-traefik-flux-aws-lb-controller/main.tf
@@ -0,0 +1,30 @@
+# --------------------------------------------------
+# Create JSON files to be picked up by Flux CD
+# --------------------------------------------------
+resource "github_repository_file" "traefik_helm" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.cluster_repo_path}/${local.app_install_name}-helm.yaml"
+  content = templatefile("${path.module}/values/app-helm.yaml", {
+    app_install_name = local.app_install_name
+    deploy_name      = var.deploy_name
+    namespace        = var.namespace
+    helm_repo_path   = local.helm_repo_path
+    prune            = var.prune
+    dashboard_ingress_host = var.dashboard_ingress_host
+  })
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "traefik_helm_install" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.helm_repo_path}/kustomization.yaml"
+  content = templatefile("${path.module}/values/helm-install.yaml", {
+    gitops_apps_repo_url = var.gitops_apps_repo_url
+    deploy_name          = var.deploy_name
+    gitops_apps_repo_ref = var.gitops_apps_repo_ref
+    namespace            = var.namespace
+  })
+  overwrite_on_create = true
+}
---
+++ _sub/compute/k8s-traefik-flux-aws-lb-controller/vars.tf
@@ -0,0 +1,54 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "deploy_name" {
+  type        = string
+  description = "Unique identifier of the deployment, only needs override if deploying multiple instances"
+  default     = "traefik"
+}
+
+variable "namespace" {
+  type        = string
+  description = "The namespace in which to deploy Helm resources"
+  default     = "traefik"
+}
+
+variable "github_owner" {
+  type        = string
+  description = "Name of the Github owner (previously: organization)"
+}
+
+variable "repo_name" {
+  type        = string
+  description = "Name of the Github repo to store the manifests in"
+}
+
+variable "repo_branch" {
+  type        = string
+  description = "Override the default branch of the repo (optional)"
+  default     = null
+}
+
+variable "dashboard_ingress_host" {
+  type        = string
+  description = "The alb auth dns name for accessing Traefik."
+}
+
+variable "gitops_apps_repo_url" {
+  type        = string
+  default     = ""
+  description = "The https url for your GitOps manifests"
+}
+
+variable "gitops_apps_repo_ref" {
+  type        = string
+  default     = "main"
+  description = "The default branch or tag for your GitOps manifests"
+}
+
+variable "prune" {
+  type        = bool
+  default     = true
+  description = "Enable Garbage collection"
+}
