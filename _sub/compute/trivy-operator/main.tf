64822056e1e45abdfef53445bff00af3d2ba2f9a
Audun Nes <aunes@dfds.com>
2026-01-06 16:15:24 +0100

    feat: add IAM roles and policies for Trivy Operator, update Helm chart configuration

--- _sub/compute/trivy-operator/main.tf
+++ _sub/compute/trivy-operator/main.tf
@@ -6,10 +6,16 @@ resource "github_repository_file" "helm" {
   branch     = local.repo_branch
   file       = "${local.cluster_repo_path}/${local.app_install_name}-helm.yaml"
   content = templatefile("${path.module}/values/app-config.yaml", {
-    app_install_name = local.app_install_name
-    helm_repo_path   = local.helm_repo_path
-    deploy_name      = local.deploy_name
-    prune            = var.prune
+    app_install_name               = local.app_install_name
+    helm_repo_path                 = local.helm_repo_path
+    deploy_name                    = local.deploy_name
+    prune                          = var.prune
+    resources_requests_cpu         = var.resources_requests_cpu
+    resources_requests_memory      = var.resources_requests_memory
+    scan_resources_requests_cpu    = var.scan_resources_requests_cpu
+    scan_resources_requests_memory = var.scan_resources_requests_memory
+    workload_account_id            = data.aws_caller_identity.this.account_id
+    cluster_name                   = var.cluster_name
   })
   overwrite_on_create = true
 }
@@ -26,17 +32,15 @@ resource "github_repository_file" "helm_install" {
   overwrite_on_create = true
 }
 
-resource "github_repository_file" "helm_patch" {
-  repository = var.repo_name
-  branch     = local.repo_branch
-  file       = "${local.helm_repo_path}/patch.yaml"
-  content = templatefile("${path.module}/values/patch.yaml", {
-    deploy_name                    = local.deploy_name
-    github_token                   = var.github_token
-    resources_requests_cpu         = var.resources_requests_cpu
-    resources_requests_memory      = var.resources_requests_memory
-    scan_resources_requests_cpu    = var.scan_resources_requests_cpu
-    scan_resources_requests_memory = var.scan_resources_requests_memory
-  })
-  overwrite_on_create = true
+resource "aws_ssm_parameter" "this" {
+  name        = "/eks/${var.cluster_name}/${local.deploy_name}-github-token"
+  description = "GitHub Token for the Trivy Operator"
+  type        = "SecureString"
+  value       = var.github_token
+
+  lifecycle {
+    ignore_changes = [
+      overwrite,
+    ]
+  }
 }
