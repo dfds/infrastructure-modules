b170d75ef01eaf5276ec8bc96cd17a39d21e367d
Mik-Nord <121795624+Mik-Nord@users.noreply.github.com>
2026-01-12 14:18:25 +0100

    Added policy reporter (#2168)

--- _sub/compute/kyverno/main.tf
+++ _sub/compute/kyverno/main.tf
@@ -48,3 +48,27 @@ resource "github_repository_file" "policies_kustomization" {
   })
   overwrite_on_create = true
 }
+
+resource "github_repository_file" "policy_reporter_helm" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.cluster_repo_path}/${local.app_install_name}-policy-reporter-helm.yaml"
+  content = templatefile("${path.module}/values/policy-reporter.yaml", {
+    app_install_name    = local.app_install_name
+    reporter_repo_path  = local.reporter_repo_path
+    deploy_name         = local.deploy_name
+  })
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "policy_reporter_kustomization" {
+  repository = var.repo_name
+  branch     = local.repo_branch
+  file       = "${local.reporter_repo_path}/kustomization.yaml"
+  content = templatefile("${path.module}/values/policy-reporter-kustomization.yaml", {
+    gitops_apps_repo_url = var.gitops_apps_repo_url
+    deploy_name          = local.deploy_name
+    gitops_apps_repo_ref = var.gitops_apps_repo_ref
+  })
+  overwrite_on_create = true
+}
