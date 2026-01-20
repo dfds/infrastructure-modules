820955d99fa33fce1afa02b6f1c5ecbd36366ab6
Willi Carlsen <carlsenwilli@gmail.com>
2026-01-20 10:41:16 +0100

    terraform-aws-iam module for eks service accounts require AWS provider >=6.28.0 (#2181)
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

--- compute/k8s-services/versions.tf
+++ compute/k8s-services/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.23.0"
+      version = ">= 6.28.0"
     }
 
     kubernetes = {
@@ -38,5 +38,4 @@ terraform {
       version = ">= 3.7.0"
     }
   }
-
 }
