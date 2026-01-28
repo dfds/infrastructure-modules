48fb2739aaa5d0e2b50392abb89aa78aedb4a92c
samidbb <samdi@dfds.com>
2026-01-28 14:59:15 +0100

    sending iam role arns to external-dns flux app again

--- _sub/network/external-dns/versions.tf
+++ _sub/network/external-dns/versions.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.6.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.23.0"
-    }
     github = {
       source  = "integrations/github"
       version = ">= 6.8.0,!=6.8.2"
