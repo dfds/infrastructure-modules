5f861bd7d05783654dc4ec3696013f0fed83f002
Aleksandra Fromm <alekorc@dfds.com>
2026-01-28 14:25:15 +0100

    Remove locals

--- security/org-policies/preventive.tf
+++ security/org-policies/preventive.tf
@@ -290,7 +290,13 @@ data "aws_iam_policy_document" "preventive" {
 
     condition {
       test = "ArnNotLike"
-      values = local.bedrock_exempted_principal_arns
+      values = concat(
+      [
+        "arn:aws:iam::*:role/OrgRole",
+        "arn:aws:iam::*:role/aws-config-recorder*"
+      ],
+      formatlist("arn:aws:iam::%s:role/*", var.bedrock_exempted_accounts)
+    )
       variable = "aws:PrincipalArn"
     }
   }
