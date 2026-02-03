d59d5b0dafe250b4b18179793d06d1ef97dcf6d7
Aleksandra Fromm <alekorc@dfds.com>
2026-02-03 11:03:20 +0100

    Move validation to variable

--- _sub/security/org-service-control-policy/main.tf
+++ _sub/security/org-service-control-policy/main.tf
@@ -3,13 +3,6 @@ resource "aws_organizations_policy" "policy" {
   name        = var.name
   description = var.description
   content     = var.policy
-
-  lifecycle {
-    precondition {
-      condition = length(replace(replace(var.policy, " ", ""), "\n", "")) < 6144
-      error_message = "Length of the policy is more than 6144 symbols, current length is: ${length(replace(replace(var.policy, " ", ""), "\n", ""))}"
-    }
-  }
 }
 
 locals {
