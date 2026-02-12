7cf5e05646a587f9c4141a154190c89ff1d6815f
rifisdfds <40063756+rifisdfds@users.noreply.github.com>
2026-02-12 07:32:54 +0000

    feat: remove cloudtrail from hardened account feature (#2178)
    * feat: remove cloudtrail from hardened account feature
    
    * fix: remove unrequired cis and cloudtrail bits

--- _sub/security/security-bot/vars.tf
+++ _sub/security/security-bot/vars.tf
@@ -29,21 +29,6 @@ variable "slack_channel" {
   type = string
 }
 
-variable "cloudwatch_logs_group_name" {
-  type        = string
-  description = "The name of the CloudWatch log group containing the CloudTrail events triggering the alarms."
-}
-
-variable "cloudwatch_logs_group_arn" {
-  type        = string
-  description = "The arn of the CloudWatch log group containing the CloudTrail events triggering the alarms."
-}
-
-variable "sns_topic_arn_cis_controls" {
-  type        = string
-  description = "The SNS topic where alerts are published related to the CIS controls."
-}
-
 variable "sns_topic_arn_compliance_changes" {
   type        = string
   description = "The SNS topic where changes to AWS Config resource compliance are published."
