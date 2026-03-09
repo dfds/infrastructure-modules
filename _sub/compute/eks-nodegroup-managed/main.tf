764677f5dbe7dc56c03fe299a61bb38e4cb7a594
Willi Carlsen <carlsenwilli@gmail.com>
2026-03-09 14:54:24 +0100

    Revert "Add default value for hop limit, to enable rollback (#2261)" (#2262)
    This reverts commit 066edccf28082872586c16832641598fb12d7684.

--- _sub/compute/eks-nodegroup-managed/main.tf
+++ _sub/compute/eks-nodegroup-managed/main.tf
@@ -31,9 +31,8 @@ resource "aws_launch_template" "eks" {
   }
 
   metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 2
+    http_endpoint = "enabled"
+    http_tokens   = "required"
   }
 
   block_device_mappings {
