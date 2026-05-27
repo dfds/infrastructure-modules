5f41047e95d84c4eb8b74c55441e85739815c318
Audun V. Nes <aunes@dfds.com>
2026-05-27 14:23:42 +0200

    revert deletion: add S3 replication destination module with necessary configurations and variables (#2358)

---
+++ storage/s3-replication-destination/main.tf
@@ -0,0 +1,43 @@
+locals {
+  bucket_arn = format("arn:aws:s3:::%s", var.bucket_name)
+}
+
+module "destination" {
+  source             = "../../_sub/storage/s3-bucket-lifecycle"
+  bucket_name        = var.bucket_name
+  bucket_policy      = data.aws_iam_policy_document.policy.json
+  sse_algorithm      = var.sse_algorithm
+  retention_days     = var.retention_days
+  versioning_enabled = true
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.replication_role_arn]
+    }
+
+    sid    = "ReplicationPermissions"
+    effect = "Allow"
+    actions = [
+      "s3:ReplicateDelete",
+      "s3:ReplicateObject",
+      "s3:ObjectOwnerOverrideToBucketOwner",
+      "s3:GetBucketVersioning",
+      "s3:PutBucketVersioning"
+    ]
+
+    resources = [local.bucket_arn, format("%s/*", local.bucket_arn)]
+
+  }
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = var.bucket_name
+  policy = data.aws_iam_policy_document.policy.json
+
+  depends_on = [
+    module.destination
+  ]
+}
