9e10d6912bce3ecc542404dd700cd08db6947813
Audun Nes <aunes@dfds.com>
2026-05-27 13:50:14 +0200

    revert deletion: add S3 replication destination module with necessary configurations and variables

---
+++ storage/s3-replication-destination/vars.tf
@@ -0,0 +1,49 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_assume_role_arn" {
+  type = string
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Replication destination bucket name"
+}
+
+variable "replication_role_arn" {
+  description = "Which role arn can write to the bucket?"
+  type        = string
+}
+
+variable "retention_days" {
+  type        = number
+  description = "Retention days set on bucket."
+  default     = 15
+  validation {
+    condition     = var.retention_days > 0
+    error_message = "Retention days must be greater than 0."
+  }
+}
+
+variable "sse_algorithm" {
+  type        = string
+  description = "The server-side encryption algorithm to use."
+  default     = "AES256"
+  validation {
+    condition     = contains(["aws:kms", "aws:kms:dsse", "AES256"], var.sse_algorithm)
+    error_message = "SSE algorithm must be either 'aws:kms', 'aws:kms:dsse' or 'AES256'."
+  }
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags to apply to all the resources deployed by the module"
+  default     = {}
+}
+
+variable "data_tags" {
+  type        = map(string)
+  description = "A map of tags to apply to all the data and/or storage deployed by the module"
+  default     = {}
+}
