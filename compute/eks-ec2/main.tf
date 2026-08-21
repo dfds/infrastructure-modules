8955bf1c4baf57753753f8a7ffb4ab7dd4433cfc
devex-sa <github.devex@dfds.com>
2026-08-21 21:33:18 +0000

    Update Terraform terraform-aws-modules/eks/aws to v21.25.0

--- compute/eks-ec2/main.tf
+++ compute/eks-ec2/main.tf
@@ -462,7 +462,7 @@ module "eks_version_endpoint" {
 # --------------------------------------------------
 module "karpenter" {
   source                        = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version                       = "21.24.2"
+  version                       = "21.25.0"
   create                        = true
   cluster_name                  = module.eks_cluster.name
   create_access_entry           = true
